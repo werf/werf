3697bbf26623818bbba1a410517df0edc36b4831
Dmitry Stolyarov <dmitry.stolyarov@flant.com>
2016-04-04 16:00:46 +0300

    Refactoring: rename local var in CascadeTagging

--- lib/dapp/builder/cascade_tagging.rb
+++ lib/dapp/builder/cascade_tagging.rb
@@ -10,37 +10,37 @@ module Dapp
 
         opts[:build_history_length] ||= 10
 
-        i = {
+        spec = {
           name: name,
           tag: home_branch,
           registry: opts[:docker_registry]
         }
 
         # return if nothing changed
-        return if image_id == docker.image_id(i)
+        return if image_id == docker.image_id(spec)
 
         # remove excess tags
-        tags_to_remove = docker.images(name: i[:name], registry: i[:registry])
-                               .map { |image| image[:tag] }
-                               .select { |tag| tag.start_with?("#{i[:tag]}_") && tag.sub(/^#{i[:tag]}_/, '').to_i >= opts[:build_history_length] }
+        tags_to_remove = docker.images(name: spec[:name], registry: spec[:registry])
+        tags_to_remove.map! { |image| image[:tag] }
+        tags_to_remove.select! { |tag| tag.start_with?("#{spec[:tag]}_") && tag.sub(/^#{spec[:tag]}_/, '').to_i >= opts[:build_history_length] }
         tags_to_remove.each do |tag_to_remove|
-          docker.rmi i.merge(tag: tag_to_remove)
+          docker.rmi spec.merge(tag: tag_to_remove)
         end
 
         # shift old images: 1 -> 2, 2 -> 3, ..., n -> n+1
         (opts[:build_history_length] - 1).downto(1).each do |n|
-          origin = i.merge(tag: "#{i[:tag]}_#{n}")
+          origin = spec.merge(tag: "#{spec[:tag]}_#{n}")
 
           if docker.image_exist?(**origin)
-            docker.tag origin, i.merge(tag: "#{i[:tag]}_#{n + 1}")
+            docker.tag origin, spec.merge(tag: "#{spec[:tag]}_#{n + 1}")
           end
         end
 
         # shift top -> 1
-        docker.tag i, i.merge(tag: "#{i[:tag]}_1") if docker.image_exist?(**i)
+        docker.tag spec, spec.merge(tag: "#{spec[:tag]}_1") if docker.image_exist?(**spec)
 
         # tag top
-        docker.tag image_id, i
+        docker.tag image_id, spec
       end
       # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
     end
