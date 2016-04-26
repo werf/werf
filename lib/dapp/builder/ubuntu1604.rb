0a57babfa916f9b8afab9017223148343f6fe20e
Dmitry Stolyarov <dmitry.stolyarov@flant.com>
2016-04-26 20:38:48 +0300

    Add ubuntu 16.04 support

---
+++ lib/dapp/builder/ubuntu1604.rb
@@ -0,0 +1,19 @@
+module Dapp
+  class Builder
+    # Ubuntu 16.04 support
+    module Ubuntu1604
+      def from_ubuntu1604
+        # use ubuntu 16.04
+        docker.from 'ubuntu:16.04'
+        docker.run(
+          'apt-get update',
+          'apt-get dist-upgrade',
+          step: :begining
+        )
+
+        # TERM & utf8
+        docker.env TERM: 'xterm', LANG: 'C.UTF-8', step: :begining
+      end
+    end
+  end
+end
