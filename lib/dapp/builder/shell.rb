7d1696b934add9ac87feea0cfc15827cf3f17b3d
Alexey igrychev <alexey.igrychev@flant.com>
2016-05-25 15:05:22 +0300

    modified Builder::Shell

--- lib/dapp/builder/shell.rb
+++ lib/dapp/builder/shell.rb
@@ -1,6 +1,19 @@
 module Dapp
   module Builder
     class Shell < Base
+      [:infra_install, :infra_setup, :app_install, :app_setup].each do |m|
+        define_method :"#{m}_commands" do
+          config[m]
+        end
+
+        define_method m do
+          send(:"#{m}_commands")
+        end
+
+        define_method :"#{m}_key" do
+          sha256([super, sha256(send(:"#{m}_commands"))])
+        end
+      end
     end
   end
 end
