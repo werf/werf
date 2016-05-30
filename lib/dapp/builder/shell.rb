96a7f8e7a9dad684f5b0b200372817fe7138b534
Alexey igrychev <alexey.igrychev@flant.com>
2016-05-30 12:45:23 +0300

    modified Builder::Shell

--- lib/dapp/builder/shell.rb
+++ lib/dapp/builder/shell.rb
@@ -2,25 +2,20 @@ module Dapp
   module Builder
     class Shell < Base
       [:infra_install, :infra_setup, :app_install, :app_setup].each do |m|
-        define_method(:"#{m}_commands") { config[m] }
-        define_method(m) { send(:"#{m}_commands") }
-        define_method(:"#{m}_key") { sha256([super, send(:"#{m}_commands")]) }
+        define_method(:"#{m}_commands") { conf[m] }
+        define_method(m) do
+          cmds = send(:"#{m}_commands") || []
+          Image.new(from: send(:"#{m}_from")).tap { |image| image.build_cmd!(*cmds) }
+        end
+        define_method(:"#{m}_key") { sha256([super(), send(:"#{m}_commands")]) }
       end
 
       def app_install_key
-        if dependency_file?
-          sha256([super, app_install_commands])
-        else
-          super
-        end
+        sha256([super(), app_install_commands])
       end
 
       def app_setup_key
-        if app_setup_file?
-          sha256([super, app_setup_commands])
-        else
-          super
-        end
+        sha256([super(), app_setup_commands])
       end
     end
   end
