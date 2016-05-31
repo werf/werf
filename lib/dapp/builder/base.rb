1ebd3df1409b2ac1733a7a3613a5635d5c45dd4c
Alexey igrychev <alexey.igrychev@flant.com>
2016-05-31 18:03:33 +0300

    added Builder#chef_path

--- lib/dapp/builder/base.rb
+++ lib/dapp/builder/base.rb
@@ -227,6 +227,16 @@ module Dapp
         path.compact.inject(Pathname.new('/.build'), &:+).expand_path
       end
 
+      def chef_path(*path)
+        path.compact.inject(build_path('chef'), &:+).expand_path.tap do |p|
+          FileUtils.mkdir_p p.parent
+        end
+      end
+
+      def container_chef_path(*path)
+        path.compact.inject(container_build_path('chef'), &:+).expand_path
+      end
+
 
       def sources_1_image
         @sources_1_image ||= Image.new(from: sources_1_from)
