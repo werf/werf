48cd0c2b31c7d3299e5f46eb62fe98512b1af0c6
Timofey Kirillov <distorhead@gmail.com>
2016-06-03 18:17:39 +0300

    Stage::Base++

--- lib/dapp/stage/base.rb
+++ lib/dapp/stage/base.rb
@@ -5,10 +5,44 @@ module Dapp
 
       attr_accessor :prev, :next
 
+      def initialize(from_image_name: nil)
+        @from_image_name = from_image_name
+      end
+
       def build
         return if image_exist?
         prev.build if prev
         build_image!
+      end
+
+      def image_exist?
+        docker.image_exist? image_name
+      end
+
+      def build_image!
+        docker.build_image! image: image, name: image_name
+      end
+
+      def from_image_name
+        @from_image_name || (prev.image_name if prev) || begin
+          raise "missing from_image_name"
+        end
+      end
+
+      def signature
+        raise
+      end
+
+      def image_name
+        "dapp:#{signature}"
+      end
+
+      def image
+        @image ||= begin
+          Image.new(from: from_image_name).tap do |image|
+            yield image if block_given?
+          end
+        end
       end
     end # Base
   end # Stage
