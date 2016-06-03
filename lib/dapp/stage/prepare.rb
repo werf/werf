5beed41e02bdcfbd28c5f914a9ac9f0bdf0c9fe8
Timofey Kirillov <distorhead@gmail.com>
2016-06-03 18:18:54 +0300

    Prepare stage image creation example

--- lib/dapp/stage/prepare.rb
+++ lib/dapp/stage/prepare.rb
@@ -4,6 +4,12 @@ module Dapp
       include Dapp::Stage::Mod::Centos7
       include Dapp::Stage::Mod::Ubuntu1404
       include Dapp::Stage::Mod::Ubuntu1604
+
+      def image
+        super do |image|
+          image.build_cmd! 'ololo', 'trololo'
+        end
+      end
     end # Prepare
   end # Builder
 end # Dapp
