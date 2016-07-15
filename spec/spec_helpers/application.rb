f6217b11499e4eefedd54d37cef4d3f3945be325
Alexey Igrychev <pes.sutuliy@gmail.com>
2016-07-15 16:14:35 +0300

    +

--- spec/spec_helpers/application.rb
+++ spec/spec_helpers/application.rb
@@ -32,7 +32,7 @@ module SpecHelpers
     def default_config
       {
         _name: 'test',
-        _chef: {},
+        _chef: { _modules: [] },
         _shell: { _infra_install: [], _infra_setup: [], _app_install: [], _app_setup: []},
         _docker: { _from: :'ubuntu:14.04', _expose: [] },
         _git_artifact: { _local: [], _remote: [] }
