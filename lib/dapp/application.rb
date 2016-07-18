a56e26c43f1fdb19366969809af7fe483a179b5a
Alexey Igrychev <pes.sutuliy@gmail.com>
2016-07-18 15:50:42 +0300

    refactored

--- lib/dapp/application.rb
+++ lib/dapp/application.rb
@@ -54,25 +54,21 @@ module Dapp
     end
 
     def build_cache_path(*path)
-      path.compact.map(&:to_s).inject(Pathname.new(@build_cache_path), &:+).expand_path.tap do |p|
+      make_path(@build_cache_path, *path).expand_path.tap do |p|
         FileUtils.mkdir_p p.parent
       end
     end
 
     def home_path(*path)
-      path.compact.map(&:to_s).inject(Pathname.new(config._home_path), &:+).expand_path
+      make_path(config._home_path, *path).expand_path
     end
 
     def build_path(*path)
-      path
-        .compact
-        .map(&:to_s)
-        .inject(Pathname.new(@build_path), &:+)
-        .expand_path.tap { |p| FileUtils.mkdir_p p.parent }
+      make_path(@build_path, *path).expand_path.tap { |p| FileUtils.mkdir_p p.parent }
     end
 
     def container_build_path(*path)
-      path.compact.map(&:to_s).inject(Pathname.new('/.build'), &:+)
+      make_path('/.build', *path)
     end
 
     def builder
@@ -157,6 +153,12 @@ module Dapp
       end
 
       [branch, tag].compact
+    end
+
+    private
+
+    def make_path(base, *path)
+      path.compact.map(&:to_s).inject(Pathname.new(base), &:+)
     end
   end # Application
 end # Dapp
