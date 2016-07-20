bed3e02bfaa9f06a503ab08b6d4069d53b2915ce
Timofey Kirillov <distorhead@gmail.com>
2016-07-20 18:19:04 +0300

    Shellout without ::Bundler

--- lib/dapp/helper/shellout.rb
+++ lib/dapp/helper/shellout.rb
@@ -3,11 +3,17 @@ module Dapp
     # Shellout
     module Shellout
       def shellout(*args, log_verbose: false, **kwargs)
-        Bundler.with_clean_env do
+        do_shellout = proc do
           log_verbose = (log_verbose && cli_options[:log_verbose]) if defined? cli_options
           kwargs[:live_stream] = STDOUT if log_verbose
           Mixlib::ShellOut.new(*args, timeout: 3600, **kwargs).run_command
-        end # with_clean_env
+        end
+
+        if defined? ::Bundler
+          ::Bundler.with_clean_env { do_shellout.call }
+        else
+          do_shellout.call
+        end
       end
 
       def shellout!(*args, **kwargs)
