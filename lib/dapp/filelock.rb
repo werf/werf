e10d8f4af0b84994a8990289fcf3f8622500ae30
Alexey Igrychev <alexey.igrychev@flant.com>
2016-08-01 21:21:25 +0300

    codeclimate

--- lib/dapp/filelock.rb
+++ lib/dapp/filelock.rb
@@ -28,7 +28,7 @@ module Dapp
     protected
 
     def filelock_lockfile(filelock, error_message: 'Already in use!', timeout: 10)
-      File.open(tmp_path(filelock), File::RDWR | File::CREAT, 0o644) do |file| # TODO: storage path
+      File.open(home_path(filelock), File::RDWR | File::CREAT, 0o644) do |file|
         Timeout.timeout(timeout) do
           file.flock(File::LOCK_EX)
         end
