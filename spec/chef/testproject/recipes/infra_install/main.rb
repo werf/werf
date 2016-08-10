92016d992b827ffb1efeae6f896410746b374232
Timofey Kirillov <distorhead@gmail.com>
2016-08-10 15:51:57 +0300

    Chef: attributes/common path copy support

--- spec/chef/testproject/recipes/infra_install/main.rb
+++ spec/chef/testproject/recipes/infra_install/main.rb
@@ -1,6 +1,10 @@
 include_recipe 'apt' if node[:platform_family].to_s == 'debian'
 
 log "TEST MESSAGE" do
+  message "node['test']['common_attr'] = #{node['test']['common_attr']}"
+end
+
+log "TEST MESSAGE" do
   message "node['test']['hello'] = #{node['test']['hello']}"
 end
 
