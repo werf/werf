12e6c61f6af885b6d6258413f27b775615c9dd4e
Timofey Kirillov <distorhead@gmail.com>
2016-08-16 14:30:07 +0300

    Shared lock 'images' for app build & export

--- lib/dapp/application.rb
+++ lib/dapp/application.rb
@@ -31,8 +31,10 @@ module Dapp
 
     def build!
       with_introspection do
-        last_stage.build!
-        last_stage.save_in_cache!
+        lock('images', shared: true) do
+          last_stage.build!
+          last_stage.save_in_cache!
+        end
       end
     ensure
       FileUtils.rm_rf(tmp_path)
@@ -41,13 +43,15 @@ module Dapp
     def export!(repo, format:)
       raise Error::Application, code: :application_not_built unless last_stage.image.tagged? || dry_run?
 
-      tags.each do |tag|
-        image_name = format % { repo: repo, app_name: config._name, tag: tag }
-        if dry_run?
-          log_state(image_name, state: t(code: 'state.push'), styles: { status: :success })
-        else
-          log_process(image_name, process: t(code: 'status.process.pushing')) do
-            last_stage.image.export!(image_name, log_verbose: log_verbose?, log_time: log_time?, force: cli_options[:force])
+      lock('images', shared: true) do
+        tags.each do |tag|
+          image_name = format % { repo: repo, app_name: config._name, tag: tag }
+          if dry_run?
+            log_state(image_name, state: t(code: 'state.push'), styles: { status: :success })
+          else
+            log_process(image_name, process: t(code: 'status.process.pushing')) do
+              last_stage.image.export!(image_name, log_verbose: log_verbose?, log_time: log_time?, force: cli_options[:force])
+            end
           end
         end
       end
--- lib/dapp/application/lock.rb
+++ lib/dapp/application/lock.rb
@@ -3,20 +3,23 @@ module Dapp
   class Application
     # Lock
     module Lock
-      def lock(name, *args, default_timeout: 60, &blk)
+      def lock(name, *args, default_timeout: 60, **kwargs, &blk)
         timeout = @cli_options[:lock_timeout] || default_timeout
 
         ::Dapp::Lock::File.new(
-          self.lock_path, name,
+          lock_path, name,
           timeout: timeout,
           on_wait: ->(&blk) {
             log_secondary_process(
-              self.t(code: 'process.waiting_resouce_lock', data: { name: name }),
+              self.t(code: 'process.waiting_resouce_lock', data: { basename: home_path.basename,
+                                                                   name: name }),
               short: true,
               &blk
             )
           }
-        ).synchronize(*args, &blk)
+        ).synchronize(*args, **kwargs, &blk)
+      rescue Dapp::Lock::Error::Timeout => e
+        raise Dapp::Lock::Error::Timeout, e.net_status.tap { |err| err[:data][:basename] = home_path.basename }
       end
     end # Lock
   end # Application
