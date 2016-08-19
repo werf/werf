a58ead1079e9cf96decf7e4b5359c9f1f94a10e6
Timofey Kirillov <distorhead@gmail.com>
2016-08-19 17:14:55 +0300

    Spec project stub ++

--- spec/spec_helper/application.rb
+++ spec/spec_helper/application.rb
@@ -92,6 +92,8 @@ module SpecHelper
         allow(instance).to receive(:name) { 'test_project' }
         allow(instance).to receive(:path) { Dir.pwd }
         allow(instance).to receive(:lock) { |&blk| blk.call }
+        allow(instance).to receive(:build_path) { @build_path ||= Pathname("/tmp/dapps-build-#{SecureRandom.uuid}") }
+        allow(instance).to receive(:log_secondary_process) { |*args, &blk| blk.call(*args) if blk }
       end
     end
   end
