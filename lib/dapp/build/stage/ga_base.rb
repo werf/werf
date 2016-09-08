8baa73975efb57e9cb01a56afe8fc580474bd8cf
Timofey Kirillov <distorhead@gmail.com>
2016-09-08 15:47:44 +0300

    Forgotten g_a_container rm from build stage obj

--- lib/dapp/build/stage/ga_base.rb
+++ lib/dapp/build/stage/ga_base.rb
@@ -5,8 +5,6 @@ module Dapp
       class GABase < Base
         attr_accessor :prev_g_a_stage, :next_g_a_stage
 
-        GITARTIFACT_IMAGE = 'dappdeps/gitartifact:0.1.5'.freeze
-
         def prev_g_a_stage
           dependencies_stage.prev_stage.prev_stage
         end
@@ -21,7 +19,7 @@ module Dapp
 
         def prepare_image
           super
-          image.add_volumes_from g_a_container
+          image.add_volumes_from application.project.gitartifact_container
           image.add_command 'export PATH=/.dapp/deps/gitartifact/bin:$PATH'
 
           application.git_artifacts.each do |git_artifact|
@@ -45,24 +43,6 @@ module Dapp
         end
 
         protected
-
-        def g_a_container_name # FIXME: hashsum(image) or dockersafe()
-          GITARTIFACT_IMAGE.tr('/', '_').tr(':', '_')
-        end
-
-        def g_a_container
-          @gitartifact_container ||= begin
-            if application.project.shellout("docker inspect #{g_a_container_name}").exitstatus.nonzero?
-              application.project.log_secondary_process(application.project.t(code: 'process.git_artifact_loading'), short: true) do
-                application.project.shellout ['docker create',
-                                              "--name #{g_a_container_name}",
-                                              "--volume /.dapp/deps/gitartifact #{GITARTIFACT_IMAGE}"].join(' ')
-              end
-            end
-
-            g_a_container_name
-          end
-        end
 
         def should_not_be_detailed?
           true
