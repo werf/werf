679f5a2644808714871089bf1870619d5e259d7a
Alexey Igrychev <alexey.igrychev@flant.com>
2016-09-13 19:25:08 +0300

    fixed

--- spec/unit/git_artifact_spec.rb
+++ spec/unit/git_artifact_spec.rb
@@ -42,6 +42,7 @@ describe Dapp::GitArtifact do
   def project
     super do
       allow_any_instance_of(Dapp::Project).to receive(:git_path) { 'git' }
+      allow_any_instance_of(Dapp::Project).to receive(:tar_path) { 'tar' }
       allow_any_instance_of(Dapp::Project).to receive(:sudo_path) { 'sudo' }
     end
   end
