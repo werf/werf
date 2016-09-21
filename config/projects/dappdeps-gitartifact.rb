5530a471aab821c45fed895e77305524534b3ad3
Timofey Kirillov <distorhead@gmail.com>
2016-09-21 17:17:20 +0300

    Remove sudo

--- config/projects/dappdeps-gitartifact.rb
+++ config/projects/dappdeps-gitartifact.rb
@@ -7,7 +7,7 @@ license_file 'LICENSE.txt'
 
 install_dir '/.dapp/deps/gitartifact'
 
-build_version '0.1.5'
+build_version '0.1.6'
 build_iteration 1
 
 dependency 'dappdeps-gitartifact'
--- config/software/dappdeps-gitartifact.rb
+++ config/software/dappdeps-gitartifact.rb
@@ -4,7 +4,6 @@ license 'MIT'
 license_file 'https://github.com/flant/dappdeps-gitartifact/blob/master/LICENSE.txt'
 
 dependency 'git'
-dependency 'sudo'
 
 build do
   link "#{install_dir}/embedded/bin", "#{install_dir}/bin"
