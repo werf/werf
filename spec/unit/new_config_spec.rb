dc93d0cf60acb9571b30df134aa93221bd37eee2
Alexey Igrychev <alexey.igrychev@flant.com>
2016-10-31 19:33:39 +0300

    added mount specs

--- spec/unit/new_config_spec.rb
+++ spec/unit/new_config_spec.rb
@@ -470,4 +470,42 @@ describe Dapp::Config::DimgGroupMain do
       end
     end
   end
+
+  context 'mount' do
+    def dappfile_dimg_mount(type, to)
+      dappfile do
+        dimg do
+          mount to do
+            from type
+          end
+        end
+      end
+    end
+
+    context 'base' do
+      [:tmp_dir, :build_dir].each do |type|
+        it type do
+          dappfile_dimg_mount(type, '/to')
+          expect(dimg.public_send("_#{type}_mount").size).to eq 1
+        end
+      end
+
+      it 'custom' do
+        dappfile_dimg_mount('/from', '/to')
+        expect(dimg.public_send('_custom_mount').size).to eq 1
+      end
+    end
+
+    context 'negative' do
+      it 'absolute path required (1)' do
+        dappfile_dimg_mount('from', '/to')
+        expect { dimgs }.to raise_error RuntimeError
+      end
+
+      it 'absolute path required (2)' do
+        dappfile_dimg_mount('/from', 'to')
+        expect { dimgs }.to raise_error RuntimeError
+      end
+    end
+  end
 end
