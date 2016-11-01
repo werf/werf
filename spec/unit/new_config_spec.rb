93333a1d33fe145700a054abe0f710daf78f32c5
Alexey igrychev <alexey.igrychev@flant.com>
2016-11-01 18:13:44 +0300

    added validation specs

--- spec/unit/new_config_spec.rb
+++ spec/unit/new_config_spec.rb
@@ -310,7 +310,7 @@ describe Dapp::Config::DimgGroupMain do
           line("attributes['k1']['#{key}'] = 'k1#{key}value'")
         end
 
-        expect(dimg._chef.send("_#{key}_attributes")).to eq({ 'k1' => { key.to_s => "k1#{key}value" } })
+        expect(dimg._chef.send("__#{key}_attributes")).to eq({ 'k1' => { key.to_s => "k1#{key}value" } })
       end
     end
   end
@@ -734,6 +734,253 @@ describe Dapp::Config::DimgGroupMain do
         end
 
         expect { dimg }.to output("wrong_using_base_directive\n").to_stdout_from_any_process
+      end
+    end
+  end
+
+  context 'validate' do
+    context 'git_artifact' do
+      def dappfile_dimg_group_git_artifact(&blk)
+        dappfile do
+          dimg_group do
+            docker do
+              from 'image:tag'
+            end
+
+            git_artifact :local do
+              instance_eval(&blk) if block_given?
+            end
+
+            dimg
+          end
+        end
+      end
+
+      it 'to required' do
+        dappfile_dimg_group_git_artifact do
+          export '/cwd'
+        end
+        expect_exception_code(:export_to_required) { dimg.send(:validate!) }
+      end
+    end
+
+    context 'artifacts' do
+      def dappfile_dimg_group_artifact(&blk)
+        dappfile do
+          dimg_group do
+            docker do
+              from 'image:tag'
+            end
+
+            artifact do
+              instance_eval(&blk) if block_given?
+            end
+
+            dimg
+          end
+        end
+      end
+
+      context 'artifact' do
+        it 'to required' do
+          dappfile_dimg_group_artifact do
+            export '/cwd' do
+              before :setup
+            end
+          end
+          expect_exception_code(:export_to_required) { dimg.send(:validate!) }
+        end
+
+        context 'scratch' do
+          it 'associated (:scratch_artifact_associated)' do
+            dappfile do
+              dimg_group do
+                artifact do
+                  docker do
+                    from 'image:tag'
+                  end
+
+                  export '/cwd' do
+                    before :setup
+                    to '/to'
+                  end
+                end
+
+                dimg
+              end
+            end
+            expect_exception_code(:scratch_artifact_associated) { dimg.send(:validate!) }
+          end
+        end
+
+        context 'stage_artifact_not_associated' do
+          it 'not associated (:stage_artifact_not_associated)' do
+            dappfile do
+              dimg_group do
+                docker do
+                  from 'image:tag'
+                end
+
+                artifact do
+                  export '/cwd'
+                end
+
+                dimg
+              end
+            end
+            expect_exception_code(:stage_artifact_not_associated) { dimg.send(:validate!) }
+          end
+        end
+      end
+
+      context 'positive' do
+        it 'different where_to_add' do
+          dappfile_dimg_group_artifact do
+            export '/cwd' do
+              before :setup
+              to '/to1'
+            end
+
+            export '/cwd' do
+              before :setup
+              to '/to2'
+            end
+          end
+          expect { dimg.send(:validate!) }.to_not raise_error
+        end
+
+        it 'different paths' do
+          dappfile_dimg_group_artifact do
+            export '/cwd' do
+              before :setup
+              include_paths 'c'
+              to '/to1'
+            end
+
+            export '/cwd' do
+              before :setup
+              include_paths 'd'
+              to '/to1'
+            end
+          end
+          expect { dimg.send(:validate!) }.to_not raise_error
+        end
+
+        it 'paths with same exclude_paths' do
+          dappfile_dimg_group_artifact do
+            export '/cwd' do
+              before :setup
+              include_paths 'c'
+              to '/to1'
+            end
+
+            export '/cwd' do
+              before :setup
+              exclude_paths 'c'
+              to '/to1'
+            end
+          end
+          expect { dimg.send(:validate!) }.to_not raise_error
+        end
+
+        it 'paths with exclude_paths' do
+          dappfile_dimg_group_artifact do
+            export '/cwd' do
+              before :setup
+              include_paths 'c/d/e'
+              to '/to1'
+            end
+
+            export '/cwd' do
+              before :setup
+              include_paths 'c'
+              exclude_paths 'c/d'
+              to '/to1'
+            end
+          end
+          expect { dimg.send(:validate!) }.to_not raise_error
+        end
+
+        it 'where_to_add with paths' do
+          dappfile_dimg_group_artifact do
+            export '/cwd' do
+              before :setup
+              include_paths 'c'
+              to '/to'
+            end
+
+            export '/cwd' do
+              before :setup
+              to '/to/path'
+            end
+          end
+          expect { dimg.send(:validate!) }.to_not raise_error
+        end
+
+        it 'where_to_add with exclude_paths' do
+          dappfile_dimg_group_artifact do
+            export '/cwd' do
+              before :setup
+              exclude_paths 'path'
+              to '/to'
+            end
+
+            export '/cwd' do
+              before :setup
+              to '/to/path'
+            end
+          end
+          expect { dimg.send(:validate!) }.to_not raise_error
+        end
+      end
+
+      context 'negative' do
+        it 'same where_to_add' do
+          dappfile_dimg_group_artifact do
+            export '/cwd' do
+              before :setup
+              to '/to'
+            end
+
+            export '/cwd' do
+              before :setup
+              to '/to'
+            end
+          end
+          expect_exception_code(:artifact_conflict) { dimg.send(:validate!) }
+        end
+
+        it 'conflict between where_to_add' do
+          dappfile_dimg_group_artifact do
+            export '/cwd' do
+              before :setup
+              to '/to'
+            end
+
+            export '/cwd' do
+              before :setup
+              to '/to/path'
+            end
+          end
+          expect_exception_code(:artifact_conflict) { dimg.send(:validate!) }
+        end
+
+        it 'conflict between paths and exclude_paths' do
+          dappfile_dimg_group_artifact do
+            export '/cwd' do
+              before :setup
+              include_paths 'c'
+              to '/to'
+            end
+
+            export '/cwd' do
+              before :setup
+              exclude_paths 'd'
+              to '/to'
+            end
+          end
+          expect_exception_code(:artifact_conflict) { dimg.send(:validate!) }
+        end
       end
     end
   end
