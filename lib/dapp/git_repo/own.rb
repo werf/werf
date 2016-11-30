fa2ba1b04cdc8148703535d2e6c8904a62b45a8d
Alexey Igrychev <alexey.igrychev@flant.ru>
2016-11-30 15:30:42 +0300

    fixed GitRepo#path

--- lib/dapp/git_repo/own.rb
+++ lib/dapp/git_repo/own.rb
@@ -11,7 +11,14 @@ module Dapp
       end
 
       def path
-        @path ||= Pathname(git("-C #{dimg.home_path} rev-parse --git-dir").stdout.strip).expand_path
+        @path ||= begin
+          git_repo_path = Pathname(git("-C #{dimg.home_path} rev-parse --git-dir").stdout.strip)
+          if git_repo_path.relative?
+            File.join(dimg.home_path, git_repo_path)
+          else
+            git_repo_path
+          end
+        end
       end
 
       def latest_commit(branch = nil)
