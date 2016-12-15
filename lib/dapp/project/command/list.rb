a514bd8f2f7cdb748888b3efccd591b65cfdad56
Alexey Igrychev <alexey.igrychev@flant.ru>
2016-12-15 15:17:10 +0300

    modified dapp list

--- lib/dapp/project/command/list.rb
+++ lib/dapp/project/command/list.rb
@@ -8,7 +8,7 @@ module Dapp
         def list
           build_configs.each do |config|
             if config._name.nil?
-              log_warning("Project '#{name}' with nameless dimg!")
+              log_warning('Dappfile has unnamed dimg!') if log_verbose?
             else
               puts config._name
             end
