313e06a836d0685a2e21112620da26ef00c7a238
Alexey Igrychev <alexey.igrychev@flant.ru>
2017-03-02 12:16:39 +0300

    ~

--- lib/dapp/dimg/error/shellout.rb
+++ lib/dapp/dimg/error/shellout.rb
@@ -2,7 +2,7 @@ module Dapp
   module Dimg
     module Error
       # Shellout
-      class Shellout < ::Dapp::Error::Shellout; end
+      class Shellout < ::Dapp::Dapp::Error::Shellout; end
     end
   end
 end
