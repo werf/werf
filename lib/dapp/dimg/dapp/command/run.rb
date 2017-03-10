3b825ec1789d972bae8ca82556c693af9b1816ce
Alexey Igrychev <alexey.igrychev@flant.ru>
2017-03-10 10:17:36 +0300

    Добавлена опция --ssh-key в команду dimg run resolved #44

--- lib/dapp/dimg/dapp/command/run.rb
+++ lib/dapp/dimg/dapp/command/run.rb
@@ -5,6 +5,7 @@ module Dapp
         module Run
           def run(docker_options, command)
             one_dimg!
+            setup_ssh_agent
             Dimg.new(config: build_configs.first, dapp: self, ignore_git_fetch: true, should_be_built: true).run(docker_options, command)
           end
         end
