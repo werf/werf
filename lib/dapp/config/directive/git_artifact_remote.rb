665600dd220cdc06da33c414b4ddcc90f10f2d76
Alexey igrychev <alexey.igrychev@flant.com>
2017-03-30 23:00:59 +0300

    Доработка git-артефактов * отлажена работа с пользовательскими опциями `branch` и `commit`.

--- lib/dapp/config/directive/git_artifact_remote.rb
+++ lib/dapp/config/directive/git_artifact_remote.rb
@@ -28,7 +28,7 @@ module Dapp
           attr_accessor :_url, :_name, :_branch, :_commit
 
           def _artifact_options
-            super.merge(name: _name, branch: _branch)
+            super.merge(name: _name, branch: _branch, commit: _commit)
           end
 
           protected
@@ -39,6 +39,11 @@ module Dapp
 
           def commit(value)
             @_commit = value
+          end
+
+          def validate!
+            super
+            raise Error::Config, code: :git_artifact_remote_branch_with_commit if !_branch.nil? && !_commit.nil?
           end
         end
 
--- lib/dapp/git_repo/remote.rb
+++ lib/dapp/git_repo/remote.rb
@@ -10,9 +10,7 @@ module Dapp
         dimg.project.log_secondary_process(dimg.project.t(code: 'process.git_artifact_clone', data: { name: name }), short: true) do
           begin
             Rugged::Repository.clone_at(url, path, bare: true)
-          rescue Rugged::NetworkError => e
-            raise Error::Rugged, code: :rugged_remote_error, data: { message: e.message, url: url }
-          rescue Rugged::SslError => e
+          rescue Rugged::NetworkError, Rugged::SslError => e
             raise Error::Rugged, code: :rugged_remote_error, data: { message: e.message, url: url }
           end
         end unless File.directory?(path)
@@ -22,7 +20,16 @@ module Dapp
         branch ||= self.branch
         dimg.project.log_secondary_process(dimg.project.t(code: 'process.git_artifact_fetch', data: { name: name }), short: true) do
           git_bare.fetch('origin', [branch])
+          raise Error::Rugged, code: :branch_not_exist_in_remote_git_repository, data: { branch: branch, url: url } unless branch_exist?(branch)
         end unless dimg.ignore_git_fetch || dimg.project.dry_run?
+      end
+
+      def branch_exist?(name)
+        git_bare.branches.exist?(branch_format(name))
+      end
+
+      def latest_commit(name)
+        git_bare.ref("refs/remotes/#{branch_format(name)}").target_id
       end
 
       def cleanup!
@@ -32,13 +39,19 @@ module Dapp
 
       def lookup_commit(commit)
         super
-      rescue Rugged::OdbError => _e
+      rescue Rugged::OdbError, TypeError => _e
         raise Error::Rugged, code: :commit_not_found_in_remote_git_repository, data: { commit: commit, url: url }
       end
 
       protected
 
       attr_reader :url
+
+      private
+
+      def branch_format(name)
+        "origin/#{name.reverse.chomp('origin/'.reverse).reverse}"
+      end
     end
   end
 end
