b65516273b1932b4cf7542eb34a55917244d758a
Alexey igrychev <alexey.igrychev@flant.com>
2017-04-26 13:27:08 +0300

    Наследование dimg для job-ов * если в job-е не определён dimg, он берётся из родительской диретивы (deployment, dimg).

--- lib/dapp/deployment/config/config.rb
+++ lib/dapp/deployment/config/config.rb
@@ -15,21 +15,26 @@ module Dapp
         protected
 
         def deployment_config_validate!
-          [:bootstrap, :before_apply_job].each do |job|
-            next if (directive_config = _deployment.public_send("_#{job}")).empty?
-            if directive_config._dimg.nil? && !_dimg.map(&:_name).compact.empty?
-              raise Error::Config, code: :"deployment_#{job}_dimg_not_defined"
-            end
-            unless _dimg.map(&:_name).include?(directive_config._dimg)
-              raise Error::Config, code: :"deployment_#{job}_dimg_not_found", data: { dimg: directive_config._dimg }
-            end
-          end
-
           raise Error::Config, code: :app_name_required if _app.any? { |app| app._name.nil? } && _app.size > 1
           _app.each do |app|
             raise Error::Config, code: :app_dimg_not_defined, data: { app: app._name } if app._dimg.nil? && !_dimg.map(&:_name).compact.empty?
             unless _dimg.map(&:_name).include?(app._dimg)
               raise Error::Config, code: :app_dimg_not_found, data: { app: app._name, dimg: app._dimg }
+            end
+          end
+
+          [:deployment, :app].each do |directive|
+            Array(public_send(:"_#{directive}")).each do |obj|
+              [:bootstrap, :before_apply_job].each do |job|
+                next if (job_config = obj.public_send("_#{job}")).empty?
+                job_dimg = job_config._dimg || obj._dimg
+                if job_dimg.nil? && !_dimg.map(&:_name).compact.empty?
+                  raise Error::Config, code: :"#{directive}_#{job}_dimg_not_defined"
+                end
+                unless _dimg.map(&:_name).include?(job_dimg)
+                  raise Error::Config, code: :"#{directive}_#{job}_dimg_not_found", data: { dimg: job_dimg }
+                end
+              end
             end
           end
         end
