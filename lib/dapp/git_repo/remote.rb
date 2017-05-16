0a922b0953288302cf4729b134bf10d5270a19b5
Timofey Kirillov <timofey.kirillov@flant.com>
2017-05-16 16:31:32 +0300

    Поддержка ssh-url для git-артефактов
    * Поддержка одновременной работы нескольких git-артефактов с разными урлами и одним именем
    * Определение имени пользователя из git ssh-url

--- lib/dapp/git_repo/remote.rb
+++ lib/dapp/git_repo/remote.rb
@@ -9,27 +9,42 @@ module Dapp
 
         dimg.project.log_secondary_process(dimg.project.t(code: 'process.git_artifact_clone', data: { name: name }), short: true) do
           begin
-            Rugged::Repository.clone_at(
-              url, path,
-              bare: true,
-              credentials: proc do |_, username|
-                @_rugged_credentials = Rugged::Credentials::SshKeyFromAgent.new(username: username)
-              end
-            )
+            Rugged::Repository.clone_at(url, path, bare: true, credentials: _rugged_credentials)
           rescue Rugged::NetworkError, Rugged::SslError => e
             raise Error::Rugged, code: :rugged_remote_error, data: { message: e.message, url: url }
           end
         end unless File.directory?(path)
       end
 
+      def _rugged_credentials
+        @_rugged_credentials ||= begin
+          ssh_url = begin
+            URI.parse(@url)
+            false
+          rescue URI::InvalidURIError
+            true
+          end
+
+          if ssh_url
+            host_with_user = @url.split(':', 2).first
+            username = host_with_user.split('@', 2).reverse.last
+            Rugged::Credentials::SshKeyFromAgent.new(username: username)
+          end
+        end
+      end
+
+      def path
+        dimg.build_path('git_repo_remote', name, Digest::MD5.hexdigest(@url)).to_s
+      end
+
       def git_bare
-        @git_bare ||= Rugged::Repository.new(path, bare: true, credentials: @_rugged_credentials)
+        @git_bare ||= Rugged::Repository.new(path, bare: true, credentials: _rugged_credentials)
       end
 
       def fetch!(branch = nil)
         branch ||= self.branch
         dimg.project.log_secondary_process(dimg.project.t(code: 'process.git_artifact_fetch', data: { name: name }), short: true) do
-          git_bare.fetch('origin', [branch], credentials: @_rugged_credentials)
+          git_bare.fetch('origin', [branch], credentials: _rugged_credentials)
           raise Error::Rugged, code: :branch_not_exist_in_remote_git_repository, data: { branch: branch, url: url } unless branch_exist?(branch)
         end unless dimg.ignore_git_fetch || dimg.project.dry_run?
       end
