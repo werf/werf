92406ae785d21f95165ae646247a3a391c6b8bdc
Timofey Kirillov <timofey.kirillov@flant.com>
2017-05-16 13:43:01 +0300

    Использование ssh-agent при работе с rugged

--- lib/dapp/git_repo/remote.rb
+++ lib/dapp/git_repo/remote.rb
@@ -9,17 +9,27 @@ module Dapp
 
         dimg.project.log_secondary_process(dimg.project.t(code: 'process.git_artifact_clone', data: { name: name }), short: true) do
           begin
-            Rugged::Repository.clone_at(url, path, bare: true)
+            Rugged::Repository.clone_at(
+              url, path,
+              bare: true,
+              credentials: proc do |_, username|
+                @_rugged_credentials = Rugged::Credentials::SshKeyFromAgent.new(username: username)
+              end
+            )
           rescue Rugged::NetworkError, Rugged::SslError => e
             raise Error::Rugged, code: :rugged_remote_error, data: { message: e.message, url: url }
           end
         end unless File.directory?(path)
       end
 
+      def git_bare
+        @git_bare ||= Rugged::Repository.new(path, bare: true, credentials: @_rugged_credentials)
+      end
+
       def fetch!(branch = nil)
         branch ||= self.branch
         dimg.project.log_secondary_process(dimg.project.t(code: 'process.git_artifact_fetch', data: { name: name }), short: true) do
-          git_bare.fetch('origin', [branch])
+          git_bare.fetch('origin', [branch], credentials: @_rugged_credentials)
           raise Error::Rugged, code: :branch_not_exist_in_remote_git_repository, data: { branch: branch, url: url } unless branch_exist?(branch)
         end unless dimg.ignore_git_fetch || dimg.project.dry_run?
       end
