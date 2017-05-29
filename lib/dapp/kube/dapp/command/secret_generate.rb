e3b904653490ea9149354bd71fbb461bb9631825
Alexey Igrychev <alexey.igrychev@flant.com>
2017-05-29 12:40:10 +0200

    kube secret generate command: hidden user typing

--- lib/dapp/kube/dapp/command/secret_generate.rb
+++ lib/dapp/kube/dapp/command/secret_generate.rb
@@ -5,7 +5,7 @@ module Dapp
         module SecretGenerate
           def kube_secret_generate
             raise Error::Command, code: :secret_key_not_found if secret.nil?
-            unless (data = $stdin.gets(nil)).nil?
+            unless (data = $stdin.noecho { |s| s.gets(nil) }).nil?
               puts unless data.end_with?("\n")
               puts secret.generate(data)
             end
