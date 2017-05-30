aa450b9896513a533011a51343a5a53b1a5752c2
Timofey Kirillov <timofey.kirillov@flant.com>
2017-05-30 18:04:46 +0300

    Экспорт файлов артефакта без изменения owner/group
    Те owner/group, которые были проставлены при создании файлов артефакта
    будут использованы и при импорте артефакта в образ в случае
    если owner и group не указаны явно в Dappfile.
    
    Очистка tmp-dir сделана через docker-контейнер под пользователем root,
    т.к. в этой директории могут быть произвольные владельцы и права
    после копирования файлов из артефакта.
    
    Refs #244

--- spec/chef/testproject/.dapp_chef/recipes/build_artifact/myartifact.rb
+++ spec/chef/testproject/.dapp_chef/recipes/build_artifact/myartifact.rb
@@ -1,13 +1,13 @@
 directory '/myartifact' do
-  owner 'root'
-  group 'root'
+  owner 'www-data'
+  group 'www-data'
   mode '0755'
   action :create
 end
 
 file "/myartifact/#{node.read('mdapp-testartifact', 'target_filename')}" do
-  owner 'root'
-  group 'root'
+  owner 'www-data'
+  group 'www-data'
   mode '0777'
   content ::File.open("/testartifact/#{node.read('mdapp-testartifact', 'target_filename')}").read
   action :create
