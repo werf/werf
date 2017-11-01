07f3e6dda7b4f494a26d9934a01335ede5cc1c80
Alexey igrychev <alexey.igrychev@flant.com>
2017-11-01 18:39:29 +0000

    dapp update command: update only if newest beta version exists

--- lib/dapp/cli/command/update.rb
+++ lib/dapp/cli/command/update.rb
@@ -6,14 +6,25 @@ module Dapp
           spec = Gem::Specification.find do |s|
             File.fnmatch(File.join(s.full_gem_path, '*'), __FILE__)
           end
-          Gem.install(spec.name, approximate_recommendation(spec.version))
+          unless (latest_version = latest_beta_version(spec)).nil?
+            Gem.install(spec.name, latest_version)
+          end
         rescue Gem::FilePermissionError => e
           raise Errno::EACCES, e.message
         end
 
-        # get latest beta-version
-        def approximate_recommendation(version)
-          [version.approximate_recommendation, 0].join('.')
+        def latest_beta_version(current_spec)
+          minor_version = current_spec.version.approximate_recommendation
+          url = "https://rubygems.org/api/v1/versions/#{current_spec.name}.json"
+          response = Excon.get(url)
+          if response.status == 200
+            JSON.parse(response.body)
+              .reduce([]) { |versions, spec| versions << Gem::Version.new(spec['number']) }
+              .reject { |spec_version| minor_version != spec_version.approximate_recommendation || current_spec.version >= spec_version }
+              .first
+          else
+            raise ::Dapp::Error::Base, message: "Cannot get `#{url}`: got bad http status `#{response.status}`!"
+          end
         end
       end
     end
