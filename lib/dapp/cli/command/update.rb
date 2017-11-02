ebf32abcb6e934ff4d7d257547ba5fb487f0827f
Alexey igrychev <alexey.igrychev@flant.com>
2017-11-02 09:43:10 +0000

    Implement dapp update locking

--- lib/dapp/cli/command/update.rb
+++ lib/dapp/cli/command/update.rb
@@ -7,7 +7,9 @@ module Dapp
             File.fnmatch(File.join(s.full_gem_path, '*'), __FILE__)
           end
           unless (latest_version = latest_beta_version(spec)).nil?
-            Gem.install(spec.name, latest_version)
+            with_lock do
+              Gem.install(spec.name, latest_version)
+            end
           end
         rescue Gem::FilePermissionError => e
           raise Errno::EACCES, e.message
@@ -23,7 +25,28 @@ module Dapp
               .reject { |spec_version| minor_version != spec_version.approximate_recommendation || current_spec.version >= spec_version }
               .first
           else
-            raise ::Dapp::Error::Base, message: "Cannot get `#{url}`: got bad http status `#{response.status}`!"
+            puts "Cannot get `#{url}`: got bad http status `#{response.status}`"
+          end
+        end
+
+        def with_lock
+          old_umask = File.umask(0)
+          file = nil
+
+          begin
+            begin
+              file = ::File.open('/tmp/dapp-update-running.lock', ::File::RDWR | ::File::CREAT, 0777)
+            ensure
+              File.umask(old_umask)
+            end
+
+            if file.flock(::File::LOCK_EX | ::File::LOCK_NB)
+              yield
+            else
+              puts 'There are other active dapp processes, exiting without update'
+            end
+          ensure
+            file.close unless file.nil?
           end
         end
       end
