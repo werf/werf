e62b9fcb0e951bf2fa236fd1a2cc9840db6c6df9
Alexey igrychev <alexey.igrychev@flant.com>
2017-12-20 21:20:43 +0300

    Разделение ошибок на пользовательские и системные

--- lib/dapp/error/default.rb
+++ lib/dapp/error/default.rb
@@ -1,6 +1,8 @@
 module Dapp
   module Error
     class Default < Base
+      include Mod::User
+
       def initialize(net_status = {})
         super({ context: self.class.to_s.split('::').last.downcase }.merge(net_status))
       end
