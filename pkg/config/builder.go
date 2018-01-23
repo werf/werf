825ac709aafccde77301f34aefe60ced235b12fe
Alexey igrychev <alexey.igrychev@flant.com>
2018-01-23 21:42:07 +0300

    Config: yaml loader

--- pkg/config/builder.go
+++ pkg/config/builder.go
@@ -1,16 +1,27 @@
 package config
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
+	"github.com/romana/rlog"
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
 )
 
-func LoadConfig(DappfilePath string) (interface{}, error) {
-	rand.Seed(time.Now().UTC().UnixNano())
-	if rand.Float64() > 0.5 {
-		return nil, fmt.Errorf("BADBADBAD")
-	} else {
-		return map[string]map[string]string{"hello": map[string]string{"world": "74"}}, nil
+func LoadConfig(dappfilePath string) (interface{}, error) {
+	rlog.Debugf("Parsing file `%s` ...", dappfilePath)
+	data, err := ioutil.ReadFile(dappfilePath)
+	if err != nil {
+		rlog.Errorf("Error reading file `%s`: %v", dappfilePath, err)
+		return nil, err
 	}
+
+	config := &Config{}
+	err = yaml.Unmarshal([]byte(data), &config)
+	if err != nil {
+		rlog.Errorf("Error parsing yaml from `%s`: %v", dappfilePath, err)
+		return nil, err
+	}
+
+	rlog.Infof("Config `%s` loaded", dappfilePath)
+
+	return config, nil
 }
