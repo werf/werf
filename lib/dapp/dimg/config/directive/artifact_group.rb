e69c845a0dd3441ad1a59f91bc1496604e93d904
Alexey igrychev <alexey.igrychev@flant.com>
2018-01-23 16:57:48 +0300

    Dappfile: поддержка директивы import для работы с артефактами

--- lib/dapp/dimg/config/directive/artifact_group.rb
+++ lib/dapp/dimg/config/directive/artifact_group.rb
@@ -3,8 +3,23 @@ module Dapp
     module Config
       module Directive
         class ArtifactGroup < DimgGroup
+          attr_reader :_name
+
+          def initialize(name = nil, dapp:)
+            super(dapp: dapp)
+            @_name = name || SecureRandom.hex(2)
+          end
+
           def export(*args, &blk)
-            artifact_config = pass_to(ArtifactDimg.new("artifact-#{SecureRandom.hex(2)}", dapp: dapp))
+            _artifact_export(_artifact_config, *args, &blk)
+          end
+
+          def _artifact_config
+            artifact_config_name = "artifact-#{[_name, SecureRandom.hex(2)].join('-')}"
+            pass_to(ArtifactDimg.new(artifact_config_name, dapp: dapp))
+          end
+
+          def _artifact_export(artifact_config, *args, &blk)
             artifact = Artifact.new(dapp: dapp, config: artifact_config)
             artifact.export(*args, &blk).tap do
               _export.concat artifact._export
