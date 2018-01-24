acc60013c3d04c521d3f32b038dcb7283e47ed60
Timofey Kirillov <timofey.kirillov@flant.com>
2018-01-24 16:33:21 +0300

    (yaml) call dappfile-yml from ruby
    * Changed dapp.path determination algorithm:
        * pwd by default.
        * --dir if specified.
    * Dappfiles will be searched in dapp.path:
        * Priority: dappfile.yml > dappfile.yaml > Dappfile.
        * In the case yaml dappfile found dappfile-yml binary dependency will be called to construct Dapp::config.
            * In that case dappfile-yml binary required to exist and work properly.
        * In the case old Dappfile found and none of yaml ones dapp will work.
        * In the case no dappfiles found dapp will fail with old error dappfile_not_found.
    * If DAPP_BIN_DAPPFILE_YML path to dappfile-yml is specified (from go-env), then it will be used to construct Dapp::Config from yaml.
    * Dapp will download dappfile-yml into ~/.dapp/bin/<dapp-version>/dappfile-yml cache otherwise.
        * For now the link is not correct, WIP.

---
+++ lib/dapp/downloader.rb
@@ -0,0 +1,101 @@
+module Dapp
+  module Downloader
+    class BytesCount
+      attr_reader :bytes, :total_bytes_count
+
+      def initialize(bytes, total_bytes_count: nil)
+        @bytes = bytes.to_f
+        @total_bytes_count = total_bytes_count
+      end
+
+      def to_s(*a)
+        max_bytes = @total_bytes_count || self
+        width = sprintf("%.2f", max_bytes.bytes/1024/1024).bytesize
+        sprintf("%#{width}.2f", @bytes/1024/1024)
+      end
+
+      [:+, :-, :*, :/].each do |method|
+        define_method(method) do |arg|
+          res = case arg
+          when BytesCount
+            @bytes.send(method, arg.bytes)
+          else
+            @bytes.send(method, arg)
+          end
+          self.class.new(res, total_bytes_count: total_bytes_count)
+        end
+      end
+
+      [:>, :<].each do |method|
+        define_method(method) do |arg|
+          case arg
+          when BytesCount
+            @bytes.send(method, arg.bytes)
+          else
+            @bytes.send(method, arg)
+          end
+        end
+      end
+
+      def method_missing(method, *args, &blk)
+        case method
+        when :to_f, :to_i
+          @bytes.send(method, *args, &blk)
+        else
+          raise
+        end
+      end
+    end # BytesCount
+
+    class << self
+      def download(url, destination, show_progress: false, progress_titile: nil)
+        resp = nil
+        downloaded = false
+        location = URI(url)
+
+        while !downloaded do
+          Net::HTTP.start(location.host, location.port, use_ssl: true) do |http|
+            req = Net::HTTP::Get.new location
+
+            http.request req do |resp|
+              case resp
+              when Net::HTTPRedirection
+                location = URI(resp["location"])
+                next
+              when Net::HTTPSuccess
+                File.open(destination, "w") do |file|
+                  file_size_mb = nil
+                  file_size_mb = BytesCount.new(resp.to_hash["content-length"].first.to_f) if show_progress
+
+                  if show_progress
+                    old_DEFAULT_BEGINNING_POSITION = ProgressBar::Progress.send(:remove_const, :DEFAULT_BEGINNING_POSITION)
+                    ProgressBar::Progress.send(:const_set, :DEFAULT_BEGINNING_POSITION, BytesCount.new(0, total_bytes_count: file_size_mb))
+                  end
+
+                  begin
+                    progressbar = nil
+                    progressbar = ProgressBar.create(format: "   %cMB / %CMB   %B.  %t", starting_at: BytesCount.new(0, total_bytes_count: file_size_mb), total: file_size_mb, progress_mark: "#", remainder_mark: ".", title: progress_titile, length: 100) if show_progress
+
+                    resp.read_body do |segment|
+                      progressbar.progress = progressbar.progress + segment.bytesize if show_progress
+                      file.write segment
+                    end
+                  ensure
+                    if show_progress
+                      ProgressBar::Progress.send(:remove_const, :DEFAULT_BEGINNING_POSITION)
+                      ProgressBar::Progress.send(:const_set, :DEFAULT_BEGINNING_POSITION, old_DEFAULT_BEGINNING_POSITION)
+                    end
+                  end
+                end
+
+                downloaded = true
+              else
+                resp.value
+              end
+            end # http.request
+          end # Net::HTTP.start
+        end # loop
+      end
+    end
+  end # Downloader
+end # Dapp
