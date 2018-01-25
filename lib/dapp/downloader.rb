1f8012e9fbccccf355e8fce5ee767535cb788d52
Timofey Kirillov <timofey.kirillov@flant.com>
2018-01-25 15:26:23 +0300

    (yaml) Fix dappfile-yml downloader
    * Normal progressbar
    * Working link on bintray

--- lib/dapp/downloader.rb
+++ lib/dapp/downloader.rb
@@ -1,5 +1,10 @@
 module Dapp
   module Downloader
+    module Error
+      class DownloadFailed < ::Exception
+      end
+    end
+
     class BytesCount
       attr_reader :bytes, :total_bytes_count
 
@@ -26,7 +31,7 @@ module Dapp
         end
       end
 
-      [:>, :<].each do |method|
+      [:>, :<, :==].each do |method|
         define_method(method) do |arg|
           case arg
           when BytesCount
@@ -50,20 +55,20 @@ module Dapp
     class << self
       def download(url, destination, show_progress: false, progress_titile: nil)
         resp = nil
-        downloaded = false
         location = URI(url)
+        done = false
+        state = {}
 
-        while !downloaded do
+        loop do
           Net::HTTP.start(location.host, location.port, use_ssl: true) do |http|
             req = Net::HTTP::Get.new location
-
             http.request req do |resp|
               case resp
               when Net::HTTPRedirection
                 location = URI(resp["location"])
                 next
               when Net::HTTPSuccess
-                File.open(destination, "w") do |file|
+                File.open(destination, "wb") do |file|
                   file_size_mb = nil
                   file_size_mb = BytesCount.new(resp.to_hash["content-length"].first.to_f) if show_progress
 
@@ -74,7 +79,7 @@ module Dapp
 
                   begin
                     progressbar = nil
-                    progressbar = ProgressBar.create(format: "   %cMB / %CMB   %B.  %t", starting_at: BytesCount.new(0, total_bytes_count: file_size_mb), total: file_size_mb, progress_mark: "#", remainder_mark: ".", title: progress_titile, length: 100) if show_progress
+                    progressbar = ProgressBar.create(format: "   %cMB / %CMB   %B  %t", starting_at: BytesCount.new(0, total_bytes_count: file_size_mb), total: file_size_mb, progress_mark: "#", remainder_mark: ".", title: progress_titile, length: 100, autofinish: true) if show_progress
 
                     resp.read_body do |segment|
                       progressbar.progress = progressbar.progress + segment.bytesize if show_progress
@@ -86,14 +91,16 @@ module Dapp
                       ProgressBar::Progress.send(:const_set, :DEFAULT_BEGINNING_POSITION, old_DEFAULT_BEGINNING_POSITION)
                     end
                   end
-                end
+                end # File.open
 
-                downloaded = true
+                done = true
               else
-                resp.value
-              end
+                raise Error::DownloadFailed, "Failed to download #{url}: #{resp.code} #{resp.message}"
+              end # when
             end # http.request
           end # Net::HTTP.start
+
+          break if done
         end # loop
       end
     end
