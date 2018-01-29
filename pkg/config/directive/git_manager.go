c2eb19c12d798754890709e3afa2408207438dc3
Alexey igrychev <alexey.igrychev@flant.com>
2018-01-29 18:58:58 +0300

    Config: GitManager.ToRuby()

--- pkg/config/directive/git_manager.go
+++ pkg/config/directive/git_manager.go
@@ -8,18 +8,23 @@ type GitManager struct {
 }
 
 func (c *GitManager) ToRuby() ruby_marshal_config.GitArtifact {
-	rubyGitArtifactLocal := ruby_marshal_config.GitArtifactLocal{}
-	for _, local := range c.Local {
-		rubyGitArtifactLocal.Export = append(rubyGitArtifactLocal.Export, local.ToRuby())
+	gitArtifact := &ruby_marshal_config.GitArtifact{}
+
+	if len(c.Local) != 0 {
+		rubyGitArtifactLocal := ruby_marshal_config.GitArtifactLocal{}
+		for _, local := range c.Local {
+			rubyGitArtifactLocal.Export = append(rubyGitArtifactLocal.Export, local.ToRuby())
+		}
+		gitArtifact.Local = []ruby_marshal_config.GitArtifactLocal{rubyGitArtifactLocal}
 	}
 
-	rubyGitArtifactRemote := ruby_marshal_config.GitArtifactRemote{}
-	for _, remote := range c.Remote {
-		rubyGitArtifactRemote.Export = append(rubyGitArtifactRemote.Export, remote.ToRuby())
+	if len(c.Remote) != 0 {
+		rubyGitArtifactRemote := ruby_marshal_config.GitArtifactRemote{}
+		for _, remote := range c.Remote {
+			rubyGitArtifactRemote.Export = append(rubyGitArtifactRemote.Export, remote.ToRuby())
+		}
+		gitArtifact.Remote = []ruby_marshal_config.GitArtifactRemote{rubyGitArtifactRemote}
 	}
 
-	return ruby_marshal_config.GitArtifact{
-		Local:  []ruby_marshal_config.GitArtifactLocal{rubyGitArtifactLocal},
-		Remote: []ruby_marshal_config.GitArtifactRemote{rubyGitArtifactRemote},
-	}
+	return *gitArtifact
 }
