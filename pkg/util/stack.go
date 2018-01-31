d726ed4d7b7fbc4d0727e9a95425d35fdfb99c1c
Ivan Mikheykin <ivan.mikheykin@flant.com>
2018-01-31 18:37:50 +0300

    go fmt stack.go

--- pkg/util/stack.go
+++ pkg/util/stack.go
@@ -2,22 +2,25 @@ package util
 
 type (
 	Stack struct {
-		top *node
+		top    *node
 		length int
 	}
 	node struct {
 		value interface{}
-		prev *node
+		prev  *node
 	}
 )
+
 // Create a new stack
 func NewStack() *Stack {
-	return &Stack{nil,0}
+	return &Stack{nil, 0}
 }
+
 // Return the number of items in the stack
 func (this *Stack) Len() int {
 	return this.length
 }
+
 // View the top item on the stack
 func (this *Stack) Peek() interface{} {
 	if this.length == 0 {
@@ -25,6 +28,7 @@ func (this *Stack) Peek() interface{} {
 	}
 	return this.top.value
 }
+
 // Pop the top item of the stack and return it
 func (this *Stack) Pop() interface{} {
 	if this.length == 0 {
@@ -36,9 +40,10 @@ func (this *Stack) Pop() interface{} {
 	this.length--
 	return n.value
 }
+
 // Push a value onto the top of the stack
 func (this *Stack) Push(value interface{}) {
-	n := &node{value,this.top}
+	n := &node{value, this.top}
 	this.top = n
 	this.length++
 }
