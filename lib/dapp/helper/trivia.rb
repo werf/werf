c2a8a412b0a59dcef8e8d06fd1b76b837623b24b
Alexey igrychev <alexey.igrychev@flant.com>
2018-02-21 13:09:53 +0300

    Поддержка скрытых файлов при сопоставлении путей

--- lib/dapp/helper/trivia.rb
+++ lib/dapp/helper/trivia.rb
@@ -28,6 +28,25 @@ module Dapp
         Pathname.new(File.join(base.to_s, *path.compact.map(&:to_s)))
       end
 
+      def check_path?(path, format)
+        path_checker(path) { |checking_path| File.fnmatch(format, checking_path, File::FNM_PATHNAME|File::FNM_DOTMATCH) }
+      end
+
+      def check_subpath?(path, format)
+        path_checker(format) { |checking_path| File.fnmatch(checking_path, path, File::FNM_PATHNAME|File::FNM_DOTMATCH) }
+      end
+
+      def path_checker(path)
+        path_parts = path.split('/')
+        checking_path = nil
+
+        until path_parts.empty?
+          checking_path = [checking_path, path_parts.shift].compact.join('/')
+          return true if yield checking_path
+        end
+        false
+      end
+
       def self.class_to_lowercase(class_name = self)
         class_name.to_s.split('::').last.split(/(?=[[:upper:]]|[0-9])/).join('_').downcase.to_s
       end
--- lib/dapp/version.rb
+++ lib/dapp/version.rb
@@ -1,4 +1,4 @@
 module Dapp
   VERSION = "0.23.7"
-  BUILD_CACHE_VERSION = 26
+  BUILD_CACHE_VERSION = 26.1
 end
