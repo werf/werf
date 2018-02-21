e3a4df0435eac643fe71187d6881c6fd6fee765a
Alexey igrychev <alexey.igrychev@flant.com>
2018-02-21 14:44:40 +0300

    Fix: валидация путей артефактов

--- lib/dapp/helper/trivia.rb
+++ lib/dapp/helper/trivia.rb
@@ -28,6 +28,22 @@ module Dapp
         Pathname.new(File.join(base.to_s, *path.compact.map(&:to_s)))
       end
 
+      def ignore_path?(path, paths: [], exclude_paths: [])
+        ignore_path_base(path, exclude_paths: exclude_paths) do
+          paths.empty? ||
+            paths.any? do |p|
+              File.fnmatch?(p, path, File::FNM_PATHNAME|File::FNM_DOTMATCH) ||
+                File.fnmatch?(File.join(p, '**', '*'), path, File::FNM_PATHNAME|File::FNM_DOTMATCH)
+            end
+        end
+      end
+
+      def ignore_path_base(path, exclude_paths: [])
+        is_exclude_path = exclude_paths.any? { |p| check_path?(path, p) }
+        is_include_path = yield
+        is_exclude_path || !is_include_path
+      end
+
       def check_path?(path, format)
         path_checker(path) { |checking_path| File.fnmatch(format, checking_path, File::FNM_PATHNAME|File::FNM_DOTMATCH) }
       end
