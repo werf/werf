0fd458b0fb5c68305b5a060ad0c202967254e41c
Ivan Mikheykin <ivan.mikheykin@flant.com>
2018-02-27 23:03:54 +0300

    Change go-get.sh to support many dirs in GOPATH

--- go-get.sh
+++ go-get.sh
@@ -2,7 +2,11 @@
 
 set -e
 
+# pin go.uuid because sprig builds with error
+# github.com/Masterminds/sprig/crypto.go:35: multiple-value uuid.NewV4() in single-value context
 go get -v github.com/satori/go.uuid
-git -C ../../satori/go.uuid/ checkout v1.2.0
+path=${GOPATH%%:*}/src
+git -C $path/github.com/satori/go.uuid checkout v1.2.0
 
 go get -v github.com/flant/dapp/...
+
