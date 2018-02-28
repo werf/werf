61f2ccdfbebc37b1d8c385cc53664d7659533928
Alexey igrychev <alexey.igrychev@flant.com>
2018-02-28 22:00:33 +0300

    dapp kube lint command: корректная работа при отсутствие values.yaml

--- lib/dapp/kube/dapp/command/lint.rb
+++ lib/dapp/kube/dapp/command/lint.rb
@@ -4,11 +4,10 @@ module Dapp
       module Command
         module Lint
           def kube_chart_name
-            chart_yaml_path = kube_chart_path.join("Chart.yaml")
-            chart_spec = yaml_load_file(chart_yaml_path)
+            chart_spec = yaml_load_file(kube_chart_yaml_path)
 
             if chart_spec["name"].nil? || chart_spec["name"].empty?
-              raise ::Dapp::Error::Command, code: :no_helm_chart_spec_name, data: { name: chart_spec["name"], path: chart_yaml_path, raw_spec: chart_yaml_path.read.strip }
+              raise ::Dapp::Error::Command, code: :no_helm_chart_spec_name, data: { name: chart_spec["name"], path: kube_chart_yaml_path, raw_spec: kube_chart_yaml_path.read.strip }
             end
 
             chart_spec["name"]
@@ -29,6 +28,7 @@ module Dapp
 
           def kube_lint
             kube_check_helm_chart!
+            kube_check_helm_chart_yaml!
 
             repo = option_repo
 
@@ -41,7 +41,7 @@ module Dapp
 
               all_values = {}
               [kube_chart_path('values.yaml').expand_path, *kube_values_paths, *kube_tmp_chart_secret_values_paths].each do |values_path|
-                all_values = all_values.in_depth_merge(yaml_load_file(values_path))
+                all_values = all_values.in_depth_merge(yaml_load_file(values_path)) if File.file?(values_path)
               end
 
               options[:helm_set_options].each do |opt_spec|
@@ -68,6 +68,13 @@ module Dapp
             end
           end
 
+          def kube_check_helm_chart_yaml!
+            raise ::Dapp::Error::Command, code: :chart_yaml_not_found, data: { path: kube_chart_yaml_path } unless kube_chart_yaml_path.exist?
+          end
+
+          def kube_chart_yaml_path
+            kube_chart_path.join("Chart.yaml")
+          end
         end
       end
     end
