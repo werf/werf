bc7f21f40037e13a6cad22d8a5ad4b17efb8158a
Timofey Kirillov <timofey.kirillov@flant.com>
2018-03-16 12:28:07 +0300

    (sentry) capture more data for sentry
    * All CI-env variables extra data.
    * Dapp version tags data.
    * Docker version tags data.
    * Additional capture for deploy jobs-watcher Default exceptions from parallel thread.
      * Because these exceptions will not pass through bin/dapp main exception capture point.

--- lib/dapp/kube/dapp/command/deploy.rb
+++ lib/dapp/kube/dapp/command/deploy.rb
@@ -71,7 +71,14 @@ module Dapp
               watch_hooks_thr = nil
               unless dry_run?
                 watch_hooks_thr = Thread.new do
-                  watch_hooks.each {|job| Kubernetes::Manager::Job.new(self, job.name).watch_till_done!}
+                  watch_hooks.each {|job|
+                    begin
+                      Kubernetes::Manager::Job.new(self, job.name).watch_till_done!
+                    rescue ::Exception => e
+                      sentry_exception(e, extra: {"job-spec" => job.spec})
+                      raise
+                    end
+                  }
                 end
               end
 
