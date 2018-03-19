ddefc1edac75d637207ec4dd8d6f7b8506a39457
Timofey Kirillov <timofey.kirillov@flant.com>
2018-03-19 13:58:34 +0300

    (way2go) builder, git-artifact, image cmd stubs

--- go-build.sh
+++ go-build.sh
@@ -3,3 +3,6 @@
 set -e
 
 go install github.com/flant/dapp/cmd/dappfile-yml
+go install github.com/flant/dapp/cmd/git-artifact
+go install github.com/flant/dapp/cmd/image
+go install github.com/flant/dapp/cmd/builder
