d0c856fe40a5ce9e93d71e96c7bac12a972e4db5
Alexey igrychev <alexey.igrychev@flant.com>
2018-04-25 19:22:22 +0300

    dapp dimg run: ignore signature calculation logging

--- lib/dapp/dimg/dapp/command/run.rb
+++ lib/dapp/dimg/dapp/command/run.rb
@@ -6,7 +6,7 @@ module Dapp
           def run(stage_name, docker_options, command)
             one_dimg!
             setup_ssh_agent
-            dimg(config: build_configs.first, ignore_git_fetch: true, should_be_built: stage_name.nil?)
+            dimg(config: build_configs.first, ignore_git_fetch: true, should_be_built: stage_name.nil?, ignore_signature_auto_calculation: true)
               .run_stage(stage_name, docker_options, command)
           end
         end
