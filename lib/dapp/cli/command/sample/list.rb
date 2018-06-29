da703a8523b258ff3d4e2757300a853eb0961955
Alexey igrychev <alexey.igrychev@flant.com>
2018-06-29 15:06:40 +0300

    dapp sample list: hide running time log

--- lib/dapp/cli/command/sample/list.rb
+++ lib/dapp/cli/command/sample/list.rb
@@ -14,6 +14,10 @@ BANNER
             self.class.parse_options(self, argv)
             run_dapp_command(run_method, options: cli_options)
           end
+
+          def log_running_time
+            false
+          end
         end
       end
     end
