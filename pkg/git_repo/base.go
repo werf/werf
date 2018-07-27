4010ddad7f56eaad1b83e3ab4819bcde3372575e
Alexey igrychev <alexey.igrychev@flant.com>
2018-07-27 14:35:04 +0300

    way2go git artifact: up latest commit logic * remote git artifact uses latest origin/branch commit if branch/tag/commit isn't specified

--- pkg/git_repo/base.go
+++ pkg/git_repo/base.go
@@ -3,6 +3,8 @@ package git_repo
 import (
 	"fmt"
 	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"strings"
 )
 
 type Base struct {
@@ -10,14 +12,7 @@ type Base struct {
 }
 
 func (repo *Base) getHeadCommitForRepo(repoPath string) (string, error) {
-	var err error
-
-	repository, err := git.PlainOpen(repoPath)
-	if err != nil {
-		return "", fmt.Errorf("cannot open repo: %s", err)
-	}
-
-	ref, err := repository.Head()
+	ref, err := repo.getReferenceForRepo(repoPath)
 	if err != nil {
 		return "", fmt.Errorf("cannot get repo head: %s", err)
 	}
@@ -25,11 +20,40 @@ func (repo *Base) getHeadCommitForRepo(repoPath string) (string, error) {
 	return fmt.Sprintf("%s", ref.Hash()), nil
 }
 
+func (repo *Base) getHeadBranchNameForRepo(repoPath string) (string, error) {
+	ref, err := repo.getReferenceForRepo(repoPath)
+	if err != nil {
+		return "", fmt.Errorf("cannot get repo head: %s", err)
+	}
+
+	if ref.Name().IsBranch() {
+		branchRef := ref.Name()
+		return strings.Split(string(branchRef), "refs/heads/")[1], nil
+	} else {
+		return "", fmt.Errorf("cannot get branch name: HEAD refers to a specific revision that is not associated with a branch name")
+	}
+}
+
+func (repo *Base) getReferenceForRepo(repoPath string) (*plumbing.Reference, error) {
+	var err error
+
+	repository, err := git.PlainOpen(repoPath)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open repo: %s", err)
+	}
+
+	return repository.Head()
+}
+
 func (repo *Base) String() string {
 	return repo.Name
 }
 
 func (repo *Base) HeadCommit() (string, error) {
+	return "", fmt.Errorf("not implemented")
+}
+
+func (repo *Base) HeadBranchName() (string, error) {
 	return "", fmt.Errorf("not implemented")
 }
 
