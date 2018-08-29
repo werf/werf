98436e821693e2427283432088e95b7cd382fc93
Alexey igrychev <alexey.igrychev@flant.com>
2018-08-29 15:34:44 +0300

    dapp dimg build: reject saving cache by default

--- lib/dapp/dimg/cli/command/dimg/bp.rb
+++ lib/dapp/dimg/cli/command/dimg/bp.rb
@@ -19,22 +19,27 @@ BANNER
         extend ::Dapp::CLI::Options::Ssh
 
         option :tmp_dir_prefix,
-              long: '--tmp-dir-prefix PREFIX',
-              description: 'Tmp directory prefix (/tmp by default). Used for build process service directories.'
+               long: '--tmp-dir-prefix PREFIX',
+               description: 'Tmp directory prefix (/tmp by default). Used for build process service directories.'
 
         option :lock_timeout,
-              long: '--lock-timeout TIMEOUT',
-              description: 'Redefine resource locking timeout (in seconds)',
-              proc: ->(v) { v.to_i }
+               long: '--lock-timeout TIMEOUT',
+               description: 'Redefine resource locking timeout (in seconds)',
+               proc: ->(v) { v.to_i }
 
         option :build_context_directory,
-              long: '--build-context-directory DIR_PATH',
-              default: nil
+               long: '--build-context-directory DIR_PATH',
+               default: nil
 
         option :use_system_tar,
-              long: '--use-system-tar',
-              boolean: true,
-              default: false
+               long: '--use-system-tar',
+               boolean: true,
+               default: false
+
+        option :force_save_cache,
+               long: '--force-save-cache',
+               boolean: true,
+               default: false
 
         # push options
 
