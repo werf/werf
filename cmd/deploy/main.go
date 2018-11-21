9d5f17865cc677c45a97d119426f51e6de36dda6
Timofey Kirillov <timofey.kirillov@flant.com>
2018-11-21 21:47:09 +0300

    fix master build

--- cmd/deploy/main.go
+++ cmd/deploy/main.go
@@ -103,11 +103,6 @@ func main() {
 				}
 			}
 
-			err = lock.Init()
-			if err != nil {
-				return nil, err
-			}
-
 			kubeContext := os.Getenv("KUBECONTEXT")
 			if kubeContext == "" {
 				kubeContext = rubyCliOptions.Context
@@ -122,31 +117,20 @@ func main() {
 				return nil, err
 			}
 
-			value, hasKey := args["projectDir"]
-			if !hasKey {
-				return nil, fmt.Errorf("projectDir argument required!")
+			projectDir, err := ruby2go.StringOptionFromArgs("projectDir", args)
+			if err != nil {
+				return nil, err
 			}
-			projectDir := value.(string)
-
-			value, hasKey = args["releaseName"]
-			if !hasKey {
-				return nil, fmt.Errorf("releaseName argument required!")
+			releaseName, err := ruby2go.StringOptionFromArgs("releaseName", args)
+			if err != nil {
+				return nil, err
 			}
-			releaseName := value.(string)
-
-			value, hasKey = args["tag"]
-			if !hasKey {
-				return nil, fmt.Errorf("tag argument required!")
+			tag, err := ruby2go.StringOptionFromArgs("tag", args)
+			if err != nil {
+				return nil, err
 			}
-			tag := value.(string)
 
-			value, hasKey = args["repo"]
-			if !hasKey {
-				return nil, fmt.Errorf("repo argument required!")
-			}
-			repo := value.(string)
-
-			return nil, runDeploy(projectDir, releaseName, tag, kubeContext, repo, rubyCliOptions)
+			return nil, runDeploy(projectDir, releaseName, tag, kubeContext, rubyCliOptions)
 
 		default:
 			return nil, fmt.Errorf("command `%s` isn't supported", cmd)
