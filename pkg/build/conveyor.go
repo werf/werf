2b6b2ae0ffdf8e64ad99baa7c3943f2f32ea3b76
Timofey Kirillov <timofey.kirillov@flant.com>
2018-11-27 16:46:20 +0300

    Update conveyor.go

--- pkg/build/conveyor.go
+++ pkg/build/conveyor.go
@@ -14,5 +14,23 @@ type Conveyor struct {
 }
 
 type Phase interface {
-  Run(*Conveyor)
+	Run(*Conveyor) error
+}
+
+func (c *Conveyor) Build() error {
+	phases := []Phase{}
+	phases = append(phases, NewInitilizationPhase())
+	phases = append(phases, NewSignaturesPhase())
+	phases = append(phases, NewSetCachedStatePhase()) // Определение состояния кеша (есть в кеше / нет в кеше)
+	phases = append(phases, NewRenewPhase()) // Сброс кеша отсутствующих коммитов из-за rebase
+	phases = append(phases, NewBuildPhase()) // Определение состояния кеша (есть в кеше / нет в кеше)
+
+	for _, phase := range phases {
+		err := phase.Run(c)
+		if err != nil {
+			return err	
+		}
+	}
+	
+	return nil
 }
