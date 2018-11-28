f334d4fcb5543f87833c3fbc94788c8fcb0a9889
Timofey Kirillov <timofey.kirillov@flant.com>
2018-11-28 21:54:52 +0300

    [go build] image ReadDockerState

--- pkg/build/docker_state_phase.go
+++ pkg/build/docker_state_phase.go
@@ -12,5 +12,15 @@ func (p *DockerStatePhase) Run(c *Conveyor) error {
 	if debug() {
 		fmt.Printf("DockerStatePhase.Run\n")
 	}
+
+	for _, dimg := range c.GetDimgsInOrder() {
+		for _, stage := range dimg.GetStages() {
+			err := stage.GetImage().ReadDockerState()
+			if err != nil {
+				return fmt.Errorf("error reading docker state of stage %s: %s", stage.Name(), err)
+			}
+		}
+	}
+
 	return nil
 }
