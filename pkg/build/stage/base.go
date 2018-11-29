5a37866d7c0c5bb0b645f8c675d1a0616ad4828f
Timofey Kirillov <timofey.kirillov@flant.com>
2018-11-29 22:00:30 +0300

    [go build] complete mounts preparement in prepare image

--- pkg/build/stage/base.go
+++ pkg/build/stage/base.go
@@ -44,6 +44,7 @@ type BaseStage struct {
 	gitArtifacts []*GitArtifact
 	dimgConfig   *config.Dimg
 	tmpDir       string
+	buildDir     string
 }
 
 func (s *BaseStage) Name() StageName {
@@ -86,7 +87,8 @@ func (s *BaseStage) addServiceMounts(image Image, prevImage Image) error {
 	mountpointsByType := map[string][]string{}
 
 	for _, mountCfg := range s.dimgConfig.Mount {
-		mountpointsByType[mountCfg.Type] = append(mountpointsByType[mountCfg.Type], mountCfg.To)
+		mountpoint := filepath.Clean(mountCfg.To)
+		mountpointsByType[mountCfg.Type] = append(mountpointsByType[mountCfg.Type], mountpoint)
 	}
 
 	var labels map[string]string
@@ -98,34 +100,49 @@ func (s *BaseStage) addServiceMounts(image Image, prevImage Image) error {
 		struct{ Label, MountType string }{"dapp-mount-tmp-dir", "tmp_dir"},
 		struct{ Label, MountType string }{"dapp-mount-build-dir", "build_dir"},
 	} {
-		value, hasKey := labels[labelMountType.Label]
+		v, hasKey := labels[labelMountType.Label]
 		if !hasKey {
 			continue
 		}
 
-		mountpoints := strings.Split(value, ";")
-		for _, mountpoint := range mountpoints {
-			if mountpoint == "" {
-				continue
-			}
-
-			mountpointsByType[labelMountType.MountType] = append(mountpointsByType[labelMountType.MountType], mountpoint)
-		}
+		mountpoints := util.RejectEmptyStrings(util.UniqStrings(strings.Split(v, ";")))
+		mountpointsByType[labelMountType.MountType] = mountpoints
 	}
 
 	for mountType, mountpoints := range mountpointsByType {
-		_ = mountType // todo
 		for _, mountpoint := range mountpoints {
-			absolutePath := util.ExpandPath(filepath.Join("/", mountpoint))
-			tmpPath := filepath.Join(s.tmpDir, slug.Slug(absolutePath))
+			absoluteMountpoint := filepath.Join("/", mountpoint)
 
-			err := os.MkdirAll(tmpPath, os.ModePerm)
-			if err != nil {
-				return fmt.Errorf("error creating tmp path %s for mount: %s", tmpPath, err)
+			var absoluteFrom string
+			switch mountType {
+			case "tmp_dir":
+				absoluteFrom = filepath.Join(s.tmpDir, "mount", slug.Slug(absoluteMountpoint))
+			case "build_dir":
+				absoluteFrom = filepath.Join(s.buildDir, "mount", slug.Slug(absoluteMountpoint))
+			default:
+				panic(fmt.Sprintf("unknown mount type %s", mountType))
 			}
 
-			image.AddVolume(fmt.Sprintf("%s:%s", tmpPath, absolutePath))
+			err := os.MkdirAll(absoluteFrom, os.ModePerm)
+			if err != nil {
+				return fmt.Errorf("error creating tmp path %s for mount: %s", absoluteFrom, err)
+			}
+
+			image.AddVolume(fmt.Sprintf("%s:%s", absoluteFrom, absoluteMountpoint))
 		}
+
+		var labelName string
+		switch mountType {
+		case "tmp_dir":
+			labelName = "dapp-mount-type-tmp-dir"
+		case "build_dir":
+		default:
+			panic(fmt.Sprintf("unknown mount type %s", mountType))
+		}
+
+		labelValue := strings.Join(mountpoints, ";")
+
+		image.AddServiceChangeLabel(labelName, labelValue)
 	}
 
 	return nil
@@ -138,8 +155,11 @@ func (s *BaseStage) addCustomMounts(image Image, prevImage Image) error {
 		if mountCfg.Type != "custom_dir" {
 			continue
 		}
-		fromPath := util.ExpandPath(mountCfg.From)
-		mountpointsByFrom[fromPath] = util.UniqAppendString(mountpointsByFrom[fromPath], mountCfg.To)
+
+		from := filepath.Clean(mountCfg.From)
+		mountpoint := filepath.Clean(mountCfg.To)
+
+		mountpointsByFrom[from] = util.UniqAppendString(mountpointsByFrom[from], mountpoint)
 	}
 
 	var labels map[string]string
@@ -153,44 +173,30 @@ func (s *BaseStage) addCustomMounts(image Image, prevImage Image) error {
 		}
 
 		parts := strings.SplitN(k, "dapp-mount-custom-dir-", 2)
-		fromPath := util.ExpandPath(strings.Replace(parts[1], "--", "/", -1))
-		mountpoints := util.UniqStrings(strings.Split(v, ";"))
+		from := strings.Replace(parts[1], "--", "/", -1)
 
-		mountpointsByFrom[fromPath] = mountpoints
+		mountpoints := util.RejectEmptyStrings(util.UniqStrings(strings.Split(v, ";")))
+		mountpointsByFrom[from] = mountpoints
 	}
 
 	for from, mountpoints := range mountpointsByFrom {
-		err := os.MkdirAll(from, os.ModePerm)
+		absoluteFrom := util.ExpandPath(from)
+
+		err := os.MkdirAll(absoluteFrom, os.ModePerm)
 		if err != nil {
-			return fmt.Errorf("error creating %s: %s", from)
+			return fmt.Errorf("error creating %s: %s", absoluteFrom, err)
 		}
 
 		for _, mountpoint := range mountpoints {
-			image.AddVolume(fmt.Sprintf("%s:%s", from, mountpoint))
+			absoluteMountpoint := filepath.Join("/", mountpoint)
+			image.AddVolume(fmt.Sprintf("%s:%s", absoluteFrom, absoluteMountpoint))
 		}
 
 		labelName := fmt.Sprintf("dapp-mount-custom-dir-%s", strings.Replace(from, "/", "--", -1))
 		labelValue := strings.Join(mountpoints, ";")
-
 		image.AddServiceChangeLabel(labelName, labelValue)
 	}
 
-	return nil
-}
-
-func addMountsLabels(image Image, prevImage Image) error {
-	/*
-	   def image_add_mounts_labels
-	     [:tmp_dir, :build_dir].each do |type|
-	       next if (mounts = adding_mounts_by_type(type)).empty?
-	       image.add_service_change_label :"dapp-mount-#{type.to_s.tr('_', '-')}" => mounts.join(';')
-	     end
-
-	     adding_custom_dir_mounts.each do |from, to_pathes|
-	       image.add_service_change_label :"dapp-mount-custom-dir-#{from.gsub('/', '--')}" => to_pathes.join(';')
-	     end
-	   end
-	*/
 	return nil
 }
 
