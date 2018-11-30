aec5f95b466b4dd9b529103ab8616f88aa2c5975
Alexey igrychev <alexey.igrychev@flant.com>
2018-12-01 01:09:18 +0300

    [go build] ga stages

--- pkg/build/stage/ga.go
+++ pkg/build/stage/ga.go
@@ -2,9 +2,25 @@ package stage
 
 func newGAStage() *GAStage {
 	s := &GAStage{}
+	s.BaseStage = newBaseStage()
 	return s
 }
 
 type GAStage struct {
 	*BaseStage
+}
+
+func (s *GAStage) willLatestCommitBeBuiltOnPrevStage(prevBuiltImage Image) bool {
+	if prevBuiltImage == nil {
+		return true
+	}
+
+	for _, ga := range s.gitArtifacts {
+		_, exist := prevBuiltImage.Labels()[ga.GetParamshash()]
+		if !exist {
+			return false
+		}
+	}
+
+	return true
 }
