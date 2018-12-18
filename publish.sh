788d9233ce23186748092addcf99e0f728452668
Timofey Kirillov <timofey.kirillov@flant.com>
2018-12-18 16:44:56 +0300

    publish.sh for 1.0 golang only

--- publish.sh
+++ publish.sh
@@ -3,23 +3,22 @@ set -e
 
 #BINTRAY_AUTH=            # bintray auth user:TOKEN
 BINTRAY_SUBJECT=dapp      # bintray organization
-BINTRAY_REPO=ruby2go      # bintray repository
-BINTRAY_PACKAGE=ruby2go   # bintray package in repository
+BINTRAY_REPO=dapp         # bintray repository
+BINTRAY_PACKAGE=Dapp      # bintray package in repository
 
 GITHUB_OWNER=flant     # github user/org
 GITHUB_REPO=dapp       # github repository
 
-RUBY2GO_BINARIES_NAMES=$(ls cmd)
 UPLOAD_FROM_DIR=$GOPATH/bin
 
 GIT_REMOTE=origin      # can be changed to upstream with env
 
 # Dapp publisher utility
-# Publish gem to RubyGems. Create github release and upload go binary as asset.
+# Create github release and upload go binary as asset.
 main() {
   parse_args "$@" || (usage && exit 1)
 
-  if [ -z "$BINTRAY_AUTH" -a -z "$GITHUB_TOKEN" -a -z "$RUBYGEMS_TOKEN" ] ; then
+  if [ -z "$BINTRAY_AUTH" -a -z "$GITHUB_TOKEN" ] ; then
     echo "Warning! No tokens specified!"
     echo
   fi
@@ -47,14 +46,13 @@ main() {
   # change to *contents to get commit message
   TAG_RELEASE_MESSAGE=$($gitPath for-each-ref --format="%(contents)" refs/tags/$GIT_TAG | jq -R -s '.' )
 
-  build_gem && echo "Build gem is successful" || ( exit 1 )
   calculate_binaries_checksums && echo "Binaries checksums calculation is successful" || ( exit 1 )
 
   echo "Publish version $VERSION from git tag $GIT_TAG"
   if [ -n "$BINTRAY_AUTH" ] ; then
     ( bintray_create_version && echo "Bintray: Version $VERSION created" ) || ( exit 1 )
 
-    for bin in $RUBY2GO_BINARIES_NAMES ; do
+    for $bin in dapp ; do
       ( bintray_upload_file $bin ) || ( exit 1 )
       ( bintray_upload_file $bin.sha ) || ( exit 1 )
     done
@@ -64,24 +62,12 @@ main() {
     ( github_create_release && echo "Github: Release for tag $GIT_TAG created" ) || ( exit 1 )
   fi
 
-  if [ -n "$RUBYGEMS_TOKEN" ] ; then
-    ( rubygems_upload_gem && echo "Rubygems: Gem uploaded" ) || ( exit 1 )
-  fi
-
-}
-
-build_gem() {
-  echo "Building gem dapp"
-  rm -f dapp-*.gem
-  gem build ./dapp.gemspec
-
-  GEM_FILE_PATH=$(ls -1 dapp-*.gem | head -n 1)
 }
 
 calculate_binaries_checksums() {
-  echo "Calculating ruby2go binaries checksums"
+  echo "Calculating binaries checksums"
 
-  for bin in $RUBY2GO_BINARIES_NAMES ; do
+  for bin in dapp ; do
     sha256sum $UPLOAD_FROM_DIR/$bin | cut -d' ' -f 1 > $UPLOAD_FROM_DIR/$bin.sha
   done
 }
@@ -183,31 +169,6 @@ JSON
   return $ret
 }
 
-rubygems_upload_gem() {
-  curlResponse=$(mktemp)
-  status=$(curl -s -w %{http_code} -o $curlResponse \
-      --request POST \
-      --header "Authorization: ${RUBYGEMS_TOKEN}" \
-      --header "Content-type: application/octet-stream" \
-      --data-binary @${GEM_FILE_PATH} \
-      https://rubygems.org/api/v1/gems
-  )
-
-  echo "Rubygems upload ${GEM_FILE_PATH}: curl return status $status with response"
-  cat $curlResponse
-  echo
-  rm $curlResponse
-
-  ret=0
-  if [ "x$(echo $status | cut -c1)" != "x2" ]
-  then
-    ret=1
-  fi
-
-  return $ret
-}
-
-
 check_git() {
   gitPath=$(which git) || return 1
 }
@@ -219,8 +180,7 @@ check_curl() {
 
 
 usage() {
-printf " Usage: $0 --tag <tagname> [--github-token TOKEN] [--rubygems-token TOKEN]
-                           [--bintray-token TOKEN]
+printf " Usage: $0 --tag <tagname> [--github-token TOKEN] [--bintray-token TOKEN]
 
     --tag
             Release is a tag based. Tag should be present if gh-token specified.
@@ -230,9 +190,6 @@ printf " Usage: $0 --tag <tagname> [--github-token TOKEN] [--rubygems-token TOKE
 
     --bintray-auth user:TOKEN
             User and token for upload to bintray.com. No bintray actions if no token specified.
-
-    --rubygems-token TOKEN
-            Token for rubygems.org. No upload to rubygems if no token specified
 
     --help|-h
             Print help
@@ -253,10 +210,6 @@ parse_args() {
         ;;
       --bintray-auth)
         BINTRAY_AUTH="$2"
-        shift
-        ;;
-      --rubygems-token)
-        RUBYGEMS_TOKEN="$2"
         shift
         ;;
       --help|-h)
