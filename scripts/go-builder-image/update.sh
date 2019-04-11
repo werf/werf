be89a6ab3f882cbb11918ea43f31810b87bb493c
Timofey Kirillov <timofey.kirillov@flant.com>
2019-04-12 01:00:22 +0300

    Update release scripts to use go modules

--- scripts/go-builder-image/update.sh
+++ scripts/go-builder-image/update.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-IMAGE_NAME=flant/werf-builder:latest
+IMAGE_NAME=flant/werf-builder:1.0.0
 
 docker build -f scripts/go-builder-image/Dockerfile -t $IMAGE_NAME .
 docker login -u $DOCKER_HUB_LOGIN -p $DOCKER_HUB_PASSWORD || true
