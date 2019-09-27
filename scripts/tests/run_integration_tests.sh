89e865f0c6898d5ee2cbc26d6023c295b3b3f4dc
Alexey Igrychev <alexey.igrychev@flant.com>
2019-09-27 14:44:23 +0100

    [tests] Scripts to run tests

---
+++ scripts/tests/run_integration_tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+project_dir=$script_dir/../..
+
+run_bats() {
+  if [ "$1" == "all" ]
+  then
+    test_glob="$project_dir/tests/integration/guides/**"
+  else
+    test_glob="$1"
+  fi
+
+  bats_options="-r $test_glob"
+
+  if [ "$2" != "0" ]
+  then
+    bats_options="$bats_options --jobs $2"
+  fi
+
+  bats $bats_options
+}
+
+run_bats ${1:-all} ${2:-0}
