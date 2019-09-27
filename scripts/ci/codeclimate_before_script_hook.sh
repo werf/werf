1d46833d34ad21ca63b1322bfbd026166743d969
Alexey Igrychev <alexey.igrychev@flant.com>
2019-09-27 14:46:59 +0100

    [tests] Travis and codeclimate integration

---
+++ scripts/ci/codeclimate_before_script_hook.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+chmod +x ./cc-test-reporter
+./cc-test-reporter before-build
