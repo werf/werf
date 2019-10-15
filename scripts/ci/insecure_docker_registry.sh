bbba8aa425800b043051b21fb1e4be67f4da9981
Alexey Igrychev <alexey.igrychev@flant.com>
2019-10-15 11:00:17 +0100

    [github actions] Run integration tests on 1.11-1.16 k8s clusters

--- scripts/ci/insecure_docker_registry.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-sudo bash -c "cat <<EOF > /etc/docker/daemon.json
-{ \"insecure-registries\":[\"$WERF_TEST_K8S_INSECURE_DOCKER_REGISTRY\"] }
-EOF"
-
-sudo systemctl daemon-reload
-sudo service docker restart
