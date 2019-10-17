7ad1cfda35b7ed55444912cc099956483693e571
Timofey Kirillov <timofey.kirillov@flant.com>
2019-10-17 15:46:55 +0000

    [stapel image] Update LFS packages versions to latest stable
    refs https://github.com/flant/werf/issues/1803

--- scripts/stapel/dev_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker build -t flant/werf-stapel-base:dev --target base --file stapel/Dockerfile .
-docker build -t flant/werf-stapel:dev --target final --file stapel/Dockerfile .
-#docker build -t flant/werf-stapel:dev --file stapel/Dockerfile .
