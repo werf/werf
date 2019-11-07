1dd23236e898d263d7a751f7420a79665988a23b
Alexey Igrychev <alexey.igrychev@flant.com>
2019-11-07 14:31:13 +0300

    [tests] Stapel Image/Base Image Suite: complete from and from latest test cases

--- integration/build/stapel_image/base_image/from_and_from_latest_test.go
+++ integration/build/stapel_image/base_image/from_and_from_latest_test.go
@@ -3,6 +3,7 @@
 package base_image
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -20,6 +21,7 @@ import (
 var _ = Describe("from and from latest", func() {
 	var testDirPath string
 	var fromBaseRepoImageState1ID, fromBaseRepoImageState2ID string
+	var fromImage string
 
 	fromBaseRepoImageState1IDFunc := func() string { return fromBaseRepoImageState1ID }
 	fromBaseRepoImageState2IDFunc := func() string { return fromBaseRepoImageState2ID }
@@ -45,16 +47,14 @@ var _ = Describe("from and from latest", func() {
 		fromBaseRepoImageState1ID = utilsDocker.ImageID(state1Image)
 		fromBaseRepoImageState2ID = utilsDocker.ImageID(state2Image)
 
-		registryProjectRepositoryLatestAs(state2Image)
+		fromImage = registryProjectRepository
 
 		Ω(os.Setenv("WERF_STAGES_STORAGE", ":local")).Should(Succeed())
-		Ω(os.Setenv("FROM_IMAGE", registryProjectRepository))
+		Ω(os.Setenv("FROM_IMAGE", fromImage))
 		Ω(os.Setenv("FROM_LATEST", "false"))
 	})
 
 	AfterEach(func() {
-		utilsDocker.ImageRemoveIfExists(registryProjectRepository)
-
 		utils.RunSucceedCommand(
 			testDirPath,
 			werfBinPath,
@@ -66,77 +66,138 @@ var _ = Describe("from and from latest", func() {
 		fromLatest              bool
 		expectedOutputMatchers  []types.GomegaMatcher
 		expectedFromStageParent func() string
+		expectedErr             bool
 	}
 
 	entryItBody := func(e entry) {
 		Ω(os.Setenv("FROM_LATEST", strconv.FormatBool(e.fromLatest)))
 
-		out := utils.SucceedCommandOutput(
+		res, err := utils.RunCommand(
 			testDirPath,
 			werfBinPath,
 			"build",
 		)
 
-		for _, matcher := range e.expectedOutputMatchers {
-			Ω(out).Should(matcher)
+		if e.expectedErr {
+			Ω(err).Should(HaveOccurred())
+		} else {
+			Ω(err).ShouldNot(HaveOccurred())
 		}
 
-		resultImageName := utils.SucceedCommandOutput(
-			testDirPath,
-			werfBinPath,
-			"stage", "image",
-		)
+		for _, matcher := range e.expectedOutputMatchers {
+			Ω(string(res)).Should(matcher)
+		}
 
-		Ω(utilsDocker.ImageParent(strings.TrimSpace(resultImageName))).Should(Equal(e.expectedFromStageParent()))
+		if err == nil {
+			resultImageName := utils.SucceedCommandOutput(
+				testDirPath,
+				werfBinPath,
+				"stage", "image",
+			)
+
+			Ω(utilsDocker.ImageParent(strings.TrimSpace(resultImageName))).Should(Equal(e.expectedFromStageParent()))
+		}
 	}
 
 	Context("when from stage is not built", func() {
-		Context("when local from image is actual", func() {
-			BeforeEach(func() {
-				Ω(utilsDocker.CliPull(registryProjectRepository)).Should(Succeed(), "docker pull")
-			})
-
-			DescribeTable("checking from stage logic",
-				entryItBody,
-				Entry("should be built without pulling (fromLatest: false)", entry{
-					fromLatest: false,
-					expectedOutputMatchers: []types.GomegaMatcher{
-						ContainSubstring("Trying to get from base image id from registry"),
-						Not(ContainSubstring("Pulling base image")),
-						ContainSubstring("Building stage from"),
-					},
-					expectedFromStageParent: fromBaseRepoImageState2IDFunc,
-				}),
-				Entry("should be built without pulling (fromLatest: true)", entry{
-					fromLatest: true,
-					expectedOutputMatchers: []types.GomegaMatcher{
-						ContainSubstring("Trying to get from base image id from registry"),
-						Not(ContainSubstring("Pulling base image")),
-						ContainSubstring("Building stage from"),
-					},
-					expectedFromStageParent: fromBaseRepoImageState2IDFunc,
-				}),
-			)
-		})
-
-		Context("when local from image is not actual", func() {
-			Context("when from image does not exist locally", func() {
-				DescribeTable("checking from stage logic",
-					entryItBody,
-					Entry("should be built with pulling, without checking registry (fromLatest: false)", entry{
+		Context("when registry from image does not exist", func() {
+			Context("when local from image does not exist", func() {
+				It("should fail during pulling (fromLatest: false)", func() {
+					entryItBody(entry{
 						fromLatest: false,
 						expectedOutputMatchers: []types.GomegaMatcher{
 							Not(ContainSubstring("Trying to get from base image id from registry")),
 							ContainSubstring("Pulling base image"),
+							Not(ContainSubstring("Building stage from")),
+							ContainSubstring(fmt.Sprintf("Error: prepare base image %s failed", fromImage)),
+						},
+						expectedErr: true,
+					})
+				})
+
+				It("should fail during getting actual id (fromLatest: true)", func() {
+					entryItBody(entry{
+						fromLatest: true,
+						expectedOutputMatchers: []types.GomegaMatcher{
+							ContainSubstring("Trying to get from base image id from registry"),
+							Not(ContainSubstring("Pulling base image")),
+							Not(ContainSubstring("Building stage from")),
+							ContainSubstring(fmt.Sprintf("Error: can not get base image id from registry (%s)", fromImage)),
+						},
+						expectedErr: true,
+					})
+				})
+			})
+
+			Context("when local from image exist", func() {
+				BeforeEach(func() {
+					Ω(utilsDocker.CliTag(state1Image, fromImage)).Should(Succeed(), "docker tag")
+				})
+
+				AfterEach(func() {
+					utilsDocker.ImageRemoveIfExists(fromImage)
+				})
+
+				It("should be built with local image and warnings (fromLatest: false)", func() {
+					entryItBody(entry{
+						fromLatest: false,
+						expectedOutputMatchers: []types.GomegaMatcher{
+							ContainSubstring("Trying to get from base image id from registry"),
+							ContainSubstring("cannot get base image id"),
+							ContainSubstring(fmt.Sprintf("using existing image %s without pull", fromImage)),
+							ContainSubstring("Trying to get from base image id from registry"),
+							Not(ContainSubstring("Pulling base image")),
+							ContainSubstring("Building stage from"),
+						},
+						expectedFromStageParent: fromBaseRepoImageState1IDFunc,
+					})
+				})
+
+				It("should fail during getting actual id (fromLatest: true)", func() {
+					entryItBody(entry{
+						fromLatest: true,
+						expectedOutputMatchers: []types.GomegaMatcher{
+							ContainSubstring("Trying to get from base image id from registry"),
+							Not(ContainSubstring("Pulling base image")),
+							Not(ContainSubstring("Building stage from")),
+							ContainSubstring(fmt.Sprintf("Error: can not get base image id from registry (%s)", fromImage)),
+						},
+						expectedErr: true,
+					})
+				})
+			})
+		})
+
+		Context("when registry from image exists", func() {
+			BeforeEach(func() {
+				registryProjectRepositoryLatestAs(state2Image)
+			})
+
+			AfterEach(func() {
+				utilsDocker.ImageRemoveIfExists(registryProjectRepository)
+			})
+
+			Context("when local from image is actual", func() {
+				BeforeEach(func() {
+					Ω(utilsDocker.CliPull(registryProjectRepository)).Should(Succeed(), "docker pull")
+				})
+
+				DescribeTable("checking from stage logic",
+					entryItBody,
+					Entry("should be built with local image (fromLatest: false)", entry{
+						fromLatest: false,
+						expectedOutputMatchers: []types.GomegaMatcher{
+							ContainSubstring("Trying to get from base image id from registry"),
+							Not(ContainSubstring("Pulling base image")),
 							ContainSubstring("Building stage from"),
 						},
 						expectedFromStageParent: fromBaseRepoImageState2IDFunc,
 					}),
-					Entry("should be built with getting id and pulling (fromLatest: true)", entry{
+					Entry("should be built with local image (fromLatest: true)", entry{
 						fromLatest: true,
 						expectedOutputMatchers: []types.GomegaMatcher{
 							ContainSubstring("Trying to get from base image id from registry"),
-							ContainSubstring("Pulling base image"),
+							Not(ContainSubstring("Pulling base image")),
 							ContainSubstring("Building stage from"),
 						},
 						expectedFromStageParent: fromBaseRepoImageState2IDFunc,
@@ -144,38 +205,72 @@ var _ = Describe("from and from latest", func() {
 				)
 			})
 
-			Context("when from image exists locally", func() {
-				BeforeEach(func() {
-					Ω(utilsDocker.CliPull(state1Image)).Should(Succeed(), "docker pull")
-					Ω(utilsDocker.CliTag(state1Image, registryProjectRepository)).Should(Succeed(), "docker tag")
+			Context("when local from image is not actual", func() {
+				Context("when from image does not exist locally", func() {
+					DescribeTable("checking from stage logic",
+						entryItBody,
+						Entry("should be built with actual image (fromLatest: false)", entry{
+							fromLatest: false,
+							expectedOutputMatchers: []types.GomegaMatcher{
+								Not(ContainSubstring("Trying to get from base image id from registry")),
+								ContainSubstring("Pulling base image"),
+								ContainSubstring("Building stage from"),
+							},
+							expectedFromStageParent: fromBaseRepoImageState2IDFunc,
+						}),
+						Entry("should be built with actual image (fromLatest: true)", entry{
+							fromLatest: true,
+							expectedOutputMatchers: []types.GomegaMatcher{
+								ContainSubstring("Trying to get from base image id from registry"),
+								ContainSubstring("Pulling base image"),
+								ContainSubstring("Building stage from"),
+							},
+							expectedFromStageParent: fromBaseRepoImageState2IDFunc,
+						}),
+					)
 				})
 
-				DescribeTable("checking from stage logic",
-					entryItBody,
-					Entry("should be built with pulling and checking registry (fromLatest: false)", entry{
-						fromLatest: false,
-						expectedOutputMatchers: []types.GomegaMatcher{
-							ContainSubstring("Trying to get from base image id from registry"),
-							ContainSubstring("Pulling base image"),
-							ContainSubstring("Building stage from"),
-						},
-						expectedFromStageParent: fromBaseRepoImageState2IDFunc,
-					}),
-					Entry("should be built with getting id and pulling (fromLatest: true)", entry{
-						fromLatest: true,
-						expectedOutputMatchers: []types.GomegaMatcher{
-							ContainSubstring("Trying to get from base image id from registry"),
-							ContainSubstring("Pulling base image"),
-							ContainSubstring("Building stage from"),
-						},
-						expectedFromStageParent: fromBaseRepoImageState2IDFunc,
-					}),
-				)
+				Context("when from image exists locally", func() {
+					BeforeEach(func() {
+						Ω(utilsDocker.CliPull(state1Image)).Should(Succeed(), "docker pull")
+						Ω(utilsDocker.CliTag(state1Image, registryProjectRepository)).Should(Succeed(), "docker tag")
+					})
+
+					DescribeTable("checking from stage logic",
+						entryItBody,
+						Entry("should be built with actual image (fromLatest: false)", entry{
+							fromLatest: false,
+							expectedOutputMatchers: []types.GomegaMatcher{
+								ContainSubstring("Trying to get from base image id from registry"),
+								ContainSubstring("Pulling base image"),
+								ContainSubstring("Building stage from"),
+							},
+							expectedFromStageParent: fromBaseRepoImageState2IDFunc,
+						}),
+						Entry("should be built with actual image (fromLatest: true)", entry{
+							fromLatest: true,
+							expectedOutputMatchers: []types.GomegaMatcher{
+								ContainSubstring("Trying to get from base image id from registry"),
+								ContainSubstring("Pulling base image"),
+								ContainSubstring("Building stage from"),
+							},
+							expectedFromStageParent: fromBaseRepoImageState2IDFunc,
+						}),
+					)
+				})
 			})
 		})
 	})
 
 	Context("when from stage is built", func() {
+		BeforeEach(func() {
+			registryProjectRepositoryLatestAs(state2Image)
+		})
+
+		AfterEach(func() {
+			utilsDocker.ImageRemoveIfExists(registryProjectRepository)
+		})
+
 		type entryWithPreBuild struct {
 			entry
 			afterFirstBuildHook func()
@@ -200,7 +295,7 @@ var _ = Describe("from and from latest", func() {
 		Context("when from stage image is actual", func() {
 			DescribeTable("checking from stage logic",
 				entryWithPreBuildItBody,
-				Entry("should not be rebuilt, without checking registry (fromLatest: false)", entryWithPreBuild{
+				Entry("should not be rebuilt (fromLatest: false)", entryWithPreBuild{
 					afterFirstBuildHook: func() {
 						Ω(utilsDocker.CliPull(registryProjectRepository)).Should(Succeed(), "docker pull")
 					},
@@ -214,7 +309,7 @@ var _ = Describe("from and from latest", func() {
 						expectedFromStageParent: fromBaseRepoImageState2IDFunc,
 					},
 				}),
-				Entry("should not be rebuilt, with checking registry (fromLatest: true)", entryWithPreBuild{
+				Entry("should not be rebuilt (fromLatest: true)", entryWithPreBuild{
 					afterFirstBuildHook: func() {
 						Ω(utilsDocker.CliPull(registryProjectRepository)).Should(Succeed(), "docker pull")
 					},
@@ -234,7 +329,7 @@ var _ = Describe("from and from latest", func() {
 		Context("when from stage image is not actual", func() {
 			DescribeTable("checking from stage logic",
 				entryWithPreBuildItBody,
-				Entry("should not be rebuilt, without checking registry (fromLatest: false)", entryWithPreBuild{
+				Entry("should not be rebuilt (fromLatest: false)", entryWithPreBuild{
 					afterFirstBuildHook: func() {
 						Ω(utilsDocker.CliRmi(registryProjectRepository)).Should(Succeed(), "docker rmi")
 						registryProjectRepositoryLatestAs(state1Image)
@@ -249,7 +344,7 @@ var _ = Describe("from and from latest", func() {
 						expectedFromStageParent: fromBaseRepoImageState2IDFunc,
 					},
 				}),
-				Entry("should be rebuilt with getting id and pulling (fromLatest: true)", entryWithPreBuild{
+				Entry("should be rebuilt with actual image (fromLatest: true)", entryWithPreBuild{
 					afterFirstBuildHook: func() {
 						Ω(utilsDocker.CliRmi(registryProjectRepository)).Should(Succeed(), "docker rmi")
 						registryProjectRepositoryLatestAs(state1Image)
