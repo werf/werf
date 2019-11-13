d3f6a1912d310ef865af78cbe71d6064478d3036
Timofey Kirillov <timofey.kirillov@flant.com>
2019-11-13 11:18:53 +0300

    [ci] Use golang 1.13.4, update builder image to flant/werf-builder:1.2.0

---
+++ scripts/werf-builder/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" ] ; then
+	echo "Provide new werf-builder version: $0 VERSION!" 1>&2
+	exit 1
+fi
+NEW_VERSION=$1
+
+IMAGE_NAME=flant/werf-builder:$NEW_VERSION
+
+docker build -f scripts/werf-builder/Dockerfile -t $IMAGE_NAME .
