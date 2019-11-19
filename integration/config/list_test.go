fc6b55e3af37cbea8aeefb797bfdf233a1eb8c77
Alexey Igrychev <alexey.igrychev@flant.com>
2019-11-19 13:01:07 +0000

    [tests] Config Suite

---
+++ integration/config/list_test.go
@@ -0,0 +1,49 @@
+// +build integration
+
+package config_test
+
+import (
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+
+	"github.com/flant/werf/integration/utils"
+)
+
+type listEntry struct {
+	extraArgs              []string
+	expectedImagesNames    []string
+	notExpectedImagesNames []string
+}
+
+var listItBody = func(entry listEntry) {
+	testDirPath = fixturePath("list")
+
+	werfArgs := []string{"config", "list"}
+	werfArgs = append(werfArgs, entry.extraArgs...)
+
+	output := utils.SucceedCommandOutput(
+		testDirPath,
+		werfBinPath,
+		werfArgs...,
+	)
+
+	lines := utils.StringToLines(output)
+	for _, name := range entry.expectedImagesNames {
+		Ω(lines).Should(ContainElement(name))
+	}
+
+	for _, name := range entry.notExpectedImagesNames {
+		Ω(lines).ShouldNot(ContainElement(name))
+	}
+}
+
+var _ = DescribeTable("config list", listItBody,
+	Entry("all", listEntry{
+		extraArgs:           []string{},
+		expectedImagesNames: []string{"image_a", "image_b", "image_c", "artifact"},
+	}),
+	Entry("images only", listEntry{
+		extraArgs:              []string{"--images-only"},
+		expectedImagesNames:    []string{"image_a", "image_b", "image_c"},
+		notExpectedImagesNames: []string{"artifact"},
+	}))
---
+++ integration/config/suite_test.go
@@ -0,0 +1,48 @@
+// +build integration integration_k8s
+
+package config_test
+
+import (
+	"path/filepath"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+
+	"github.com/flant/werf/integration/utils"
+)
+
+var fixtureDir = "_fixtures"
+
+func TestIntegration(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Integration Config Suite")
+}
+
+var testDirPath string
+var werfBinPath string
+
+var _ = SynchronizedBeforeSuite(func() []byte {
+	computedPathToWerf := utils.ProcessWerfBinPath()
+	return []byte(computedPathToWerf)
+}, func(computedPathToWerf []byte) {
+	werfBinPath = string(computedPathToWerf)
+})
+
+var _ = SynchronizedAfterSuite(func() {}, func() {
+	gexec.CleanupBuildArtifacts()
+})
+
+var _ = BeforeEach(func() {
+	utils.BeforeEachOverrideWerfProjectName()
+})
+
+var _ = AfterEach(func() {
+	utils.ResetEnviron()
+})
+
+func fixturePath(paths ...string) string {
+	pathsToJoin := append([]string{fixtureDir}, paths...)
+	return filepath.Join(pathsToJoin...)
+}
