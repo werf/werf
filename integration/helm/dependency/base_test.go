a99b518e39d34e1c88fa040aca065cf99dbce822
Alexey Igrychev <alexey.igrychev@flant.com>
2019-11-19 14:59:57 +0000

    [tests] Helm/Dependency Suite

---
+++ integration/helm/dependency/base_test.go
@@ -0,0 +1,74 @@
+// +build integration
+
+package dependency_test
+
+import (
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/flant/werf/integration/utils"
+)
+
+var _ = Describe("helm dependency", func() {
+	BeforeEach(func() {
+		utils.CopyIn(fixturePath("default"), testDirPath)
+
+		Ω(os.Setenv("WERF_HELM_HOME", filepath.Join(testDirPath, ".helm"))).Should(Succeed())
+		utils.RunSucceedCommand(
+			testDirPath,
+			werfBinPath,
+			"helm", "repo", "init",
+		)
+	})
+
+	It("should be listed", func() {
+		output := utils.SucceedCommandOutput(
+			testDirPath,
+			werfBinPath,
+			"helm", "dependency", "list",
+		)
+
+		for _, substr := range []string{
+			"mysql",
+			"redis",
+			"rabbitmq",
+		} {
+			Ω(output).Should(ContainSubstring(substr))
+		}
+	})
+
+	It("should be built", func() {
+		output := utils.SucceedCommandOutput(
+			testDirPath,
+			werfBinPath,
+			"helm", "dependency", "build",
+		)
+
+		for _, substr := range []string{
+			"Downloading mysql from repo",
+			"Downloading redis from repo",
+			"Downloading rabbitmq from repo",
+		} {
+			Ω(output).Should(ContainSubstring(substr))
+		}
+	})
+
+	It("should be updated", func() {
+		output := utils.SucceedCommandOutput(
+			testDirPath,
+			werfBinPath,
+			"helm", "dependency", "build",
+		)
+
+		for _, substr := range []string{
+			"Downloading mysql from repo",
+			"Downloading redis from repo",
+			"Downloading rabbitmq from repo",
+		} {
+			Ω(output).Should(ContainSubstring(substr))
+		}
+	})
+})
---
+++ integration/helm/dependency/suite_test.go
@@ -0,0 +1,62 @@
+// +build integration integration_k8s
+
+package dependency_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+
+	"github.com/flant/werf/integration/utils"
+)
+
+func TestIntegration(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Integration Helm/Dependency Suite")
+}
+
+var testDirPath string
+var tmpDir string
+var werfBinPath string
+
+var _ = SynchronizedBeforeSuite(func() []byte {
+	computedPathToWerf := utils.ProcessWerfBinPath()
+	return []byte(computedPathToWerf)
+}, func(computedPathToWerf []byte) {
+	werfBinPath = string(computedPathToWerf)
+})
+
+var _ = SynchronizedAfterSuite(func() {}, func() {
+	gexec.CleanupBuildArtifacts()
+})
+
+var _ = BeforeEach(func() {
+	var err error
+	tmpDir, err = utils.GetTempDir()
+	Ω(err).ShouldNot(HaveOccurred())
+
+	testDirPath = tmpPath()
+
+	utils.BeforeEachOverrideWerfProjectName()
+})
+
+var _ = AfterEach(func() {
+	err := os.RemoveAll(tmpDir)
+	Ω(err).ShouldNot(HaveOccurred())
+
+	utils.ResetEnviron()
+})
+
+func tmpPath(paths ...string) string {
+	pathsToJoin := append([]string{tmpDir}, paths...)
+	return filepath.Join(pathsToJoin...)
+}
+
+func fixturePath(paths ...string) string {
+	pathsToJoin := append([]string{"_fixtures"}, paths...)
+	return filepath.Join(pathsToJoin...)
+}
