4133875c320da750a43902847c11bb7d77a015e3
Alexey Igrychev <alexey.igrychev@flant.com>
2019-11-19 15:20:34 +0000

    [tests] Helm/Lint Suite

---
+++ integration/helm/lint/base_test.go
@@ -0,0 +1,26 @@
+// +build integration
+
+package lint_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/flant/werf/integration/utils"
+)
+
+var _ = Describe("helm lint", func() {
+	BeforeEach(func() {
+		utils.CopyIn(fixturePath("base"), testDirPath)
+	})
+
+	It("should be linted", func() {
+		output := utils.SucceedCommandOutput(
+			testDirPath,
+			werfBinPath,
+			"helm", "lint",
+		)
+
+		Ω(output).Should(ContainSubstring("1 chart(s) linted, no failures"))
+	})
+})
