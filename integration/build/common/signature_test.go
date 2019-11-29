0a90bd065cc1630dea45e8ee5237b1c386c80f9c
Alexey Igrychev <alexey.igrychev@flant.com>
2019-11-29 11:28:55 +0000

    [tests] Integration Build/Common Suite: improve permanent signature test Skip dockerfile image check on windows

--- integration/build/common/signature_test.go
+++ integration/build/common/signature_test.go
@@ -3,7 +3,10 @@
 package common_test
 
 import (
+	"runtime"
+
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
 	"github.com/flant/werf/integration/utils"
@@ -32,27 +35,91 @@ var _ = Describe("persistent stage signatures", func() {
 		)
 	})
 
-	It("should not be changed", func() {
+	type entry struct {
+		imageName          string
+		expectedSignatures []string
+		skipOnWindows      bool
+	}
+
+	DescribeTable("should not be changed", func(e entry) {
+		if e.skipOnWindows && runtime.GOOS == "windows" {
+			Skip("skip on windows")
+		}
+
 		output, err := utils.RunCommand(
 			testDirPath,
 			werfBinPath,
-			"run", "-s", ":local",
+			"run", "-s", ":local", e.imageName,
 		)
 
 		Ω(err).Should(HaveOccurred())
 
-		for _, signature := range []string{
-			"dockerfile:             268b26f069427bdeb1f7f29756d8c5b6ccb58a569bde747b1933c2e33c9007b8",
-			"from:                   1fd881d714bc93857277419810a4b0bc276ad90435b1b16d21a8a36507354303",
-			"beforeInstall:          39505e9134789f71716aeb1dfdbef6e163b3dc6b79f768ab35f63eb21499b38e",
-			"importsBeforeInstall:   8083303ecaa2699bd3baaca1fd8d3c79699a047c18e60f2ded1f496e01d1c110",
-			"install:                962fad5e56e72d80ff9dbe8530fdc21ead8340eef6f064b746a07f62a3f52e18",
-			"beforeSetup:            e08c7c6211e028a058cd144d8ea6218d0e1bc7e52765e8e569467a693d47c2a5",
-			"setup:                  805dc430725ef3c9e788c415da116d3369aa2804bbab9409cd7f3b5b1785954f",
-			"importsAfterSetup:      e1a251bab5af242f14a6ff034ad33e84a68f0040aa312a64d72aaeec8fdc8167",
-			"dockerInstructions:     7a5a8f49b55a9dfc03c29d9208fe6e04304fb10e0d92785931051a8350f53bb0",
-		} {
+		for _, signature := range e.expectedSignatures {
 			Ω(string(output)).Should(ContainSubstring(signature))
 		}
-	})
+	},
+		Entry("dockerfile_image", entry{
+			imageName: "dockerfile_image",
+			expectedSignatures: []string{
+				"dockerfile:             f5429aa29cfadb8c07af7cd2109479dbf72f9132627c18514855a4ab82fe256e",
+			},
+			skipOnWindows: true,
+		}),
+		Entry("stapel_image_shell", entry{
+			imageName: "stapel_image_shell",
+			expectedSignatures: []string{
+				"from:                   4a10aead4134628c8e3b326a934e0a0068b28f824216a30b163b795e20401222",
+				"from:                   ce404c2be4cf10404b1dd231691acb74ab780e31e8b5a687480ccea2c2be69cb",
+				"beforeInstall:          00612d8175a189b28078400638571e0311eecd289df39b7c4e82e3fa246e76e4",
+				"importsBeforeInstall:   4e8a2199b5852119b15124a97e08b8016f9c6cf4eec9d7b335a3632365a526e4",
+				"install:                46d7e5256d350557064528320a9e9f6d4ede12e1071389abf9e9c7626342a8bf",
+				"beforeSetup:            a883b7a0e30c66109377b779855c60615e1b731adaa1031b396797bf4ecb497a",
+				"setup:                  d4624fe646ceff8341d240f75e8e00fb581164ba24e1b45de716b20de35316c9",
+				"importsAfterSetup:      1bfe02b030a0bfca7ddfb68650f67b63ed8fda6a440044d9a86168b059ffab18",
+				"dockerInstructions:     e8be54d6d3de841bd81ac6df9395a1e31e362638b12122d4c8835c1c07552822",
+			},
+		}),
+		Entry("stapel_image_ansible", entry{
+			imageName: "stapel_image_ansible",
+			expectedSignatures: []string{
+				"from:                   4a10aead4134628c8e3b326a934e0a0068b28f824216a30b163b795e20401222",
+				"from:                   ce404c2be4cf10404b1dd231691acb74ab780e31e8b5a687480ccea2c2be69cb",
+				"beforeInstall:          fb440453199cda29a121c8672c281b8e4c4b4c0ced28f0f37ed0ec230ae1cfa9",
+				"importsBeforeInstall:   6ccb2730bb8f40c3f24be980c669939cd9fd11cf4e74ffb470b7a877703328db",
+				"install:                873da454ce602eee95faf6d08993da6b767c595e5ad7ce3b09f8f02512bb878b",
+				"beforeSetup:            bed74bae853ea7d042e933f4a78a09a63785cebe7ff49c643c8cb24f8aa29b51",
+				"setup:                  11d9dab6b39938d495357999557e0e0ece362dc02b2c8d8aebac523baded0fbf",
+				"importsAfterSetup:      73e6cbd236ec78dd57ccdc26b524d1b44d91e15e550646a6557946a6e4e9101b",
+				"dockerInstructions:     39a038f34ee2d2a79f83911bddd568e7b0828da99f68822c0e09990de677e9bb",
+			},
+		}),
+		Entry("stapel_image_from_image", entry{
+			imageName: "stapel_image_from_image",
+			expectedSignatures: []string{
+				"from:                   4a10aead4134628c8e3b326a934e0a0068b28f824216a30b163b795e20401222",
+				"from:                   ce404c2be4cf10404b1dd231691acb74ab780e31e8b5a687480ccea2c2be69cb",
+				"beforeInstall:          00612d8175a189b28078400638571e0311eecd289df39b7c4e82e3fa246e76e4",
+				"importsBeforeInstall:   4e8a2199b5852119b15124a97e08b8016f9c6cf4eec9d7b335a3632365a526e4",
+				"install:                46d7e5256d350557064528320a9e9f6d4ede12e1071389abf9e9c7626342a8bf",
+				"beforeSetup:            a883b7a0e30c66109377b779855c60615e1b731adaa1031b396797bf4ecb497a",
+				"setup:                  d4624fe646ceff8341d240f75e8e00fb581164ba24e1b45de716b20de35316c9",
+				"importsAfterSetup:      1bfe02b030a0bfca7ddfb68650f67b63ed8fda6a440044d9a86168b059ffab18",
+				"dockerInstructions:     e8be54d6d3de841bd81ac6df9395a1e31e362638b12122d4c8835c1c07552822",
+				"from:                   928c083f5a0d876cf2ba334ac5f9a9b31664cf5805a1e735d51161d346f9b775",
+			},
+		}),
+		Entry("stapel_image_from_artifact", entry{
+			imageName: "stapel_image_from_artifact",
+			expectedSignatures: []string{
+				"from:                   4a10aead4134628c8e3b326a934e0a0068b28f824216a30b163b795e20401222",
+				"from:                   ce404c2be4cf10404b1dd231691acb74ab780e31e8b5a687480ccea2c2be69cb",
+				"beforeInstall:          00612d8175a189b28078400638571e0311eecd289df39b7c4e82e3fa246e76e4",
+				"importsBeforeInstall:   4e8a2199b5852119b15124a97e08b8016f9c6cf4eec9d7b335a3632365a526e4",
+				"install:                46d7e5256d350557064528320a9e9f6d4ede12e1071389abf9e9c7626342a8bf",
+				"beforeSetup:            a883b7a0e30c66109377b779855c60615e1b731adaa1031b396797bf4ecb497a",
+				"setup:                  d4624fe646ceff8341d240f75e8e00fb581164ba24e1b45de716b20de35316c9",
+				"importsAfterSetup:      1bfe02b030a0bfca7ddfb68650f67b63ed8fda6a440044d9a86168b059ffab18",
+				"from:                   b5b1babc9125e58adc29572951df6347aa68ba4cefcd64c4125b1f63116a4070",
+			},
+		}))
 })
