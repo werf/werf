b08099591caeb118e97ee8ac600481af10a1d979
Alexey Igrychev <alexey.igrychev@flant.com>
2020-01-22 21:28:08 +0000

    [tests] Disable timeout for ansible suite tests

--- integration/ansible/ansible_test.go
+++ integration/ansible/ansible_test.go
@@ -17,10 +17,9 @@ var _ = Describe("Stapel builder with ansible", func() {
 			werfPurge("general", liveexec.ExecCommandOptions{})
 		})
 
-		It("should successfully build image using arbitrary ansible modules", func(done Done) {
+		It("should successfully build image using arbitrary ansible modules", func() {
 			Expect(werfBuild("general", liveexec.ExecCommandOptions{})).To(Succeed())
-			close(done)
-		}, 500)
+		})
 	})
 
 	Context("when building stapel image based on centos 6 and 7", func() {
@@ -28,10 +27,9 @@ var _ = Describe("Stapel builder with ansible", func() {
 			werfPurge("yum1", liveexec.ExecCommandOptions{})
 		})
 
-		It("successfully installs packages using yum module", func(done Done) {
+		It("successfully installs packages using yum module", func() {
 			Expect(werfBuild("yum1", liveexec.ExecCommandOptions{})).To(Succeed())
-			close(done)
-		}, 500)
+		})
 	})
 
 	Context("when building stapel image based on centos 8", func() {
@@ -39,11 +37,10 @@ var _ = Describe("Stapel builder with ansible", func() {
 			werfPurge("yum2", liveexec.ExecCommandOptions{})
 		})
 
-		It("successfully installs packages using yum module", func(done Done) {
+		It("successfully installs packages using yum module", func() {
 			Skip("FIXME https://github.com/flant/werf/issues/1983")
 			Expect(werfBuild("yum2", liveexec.ExecCommandOptions{})).To(Succeed())
-			close(done)
-		}, 500)
+		})
 	})
 
 	Context("when become_user task option used", func() {
@@ -51,11 +48,10 @@ var _ = Describe("Stapel builder with ansible", func() {
 			werfPurge("become_user", liveexec.ExecCommandOptions{})
 		})
 
-		It("successfully installs packages using yum module", func(done Done) {
+		It("successfully installs packages using yum module", func() {
 			Skip("FIXME https://github.com/flant/werf/issues/1806")
 			Expect(werfBuild("become_user", liveexec.ExecCommandOptions{})).To(Succeed())
-			close(done)
-		}, 500)
+		})
 	})
 
 	Context("when using apt_key module used (1)", func() {
@@ -63,7 +59,7 @@ var _ = Describe("Stapel builder with ansible", func() {
 			werfPurge("apt_key1-001", liveexec.ExecCommandOptions{})
 		})
 
-		It("should fail to install package without a key and succeed with the key", func(done Done) {
+		It("should fail to install package without a key and succeed with the key", func() {
 			Skip("https://github.com/flant/werf/issues/2000")
 
 			gotNoPubkey := false
@@ -86,9 +82,7 @@ var _ = Describe("Stapel builder with ansible", func() {
 				},
 			})).To(Succeed())
 			Expect(gotPackageInstallDone).To(BeTrue())
-
-			close(done)
-		}, 500)
+		})
 	})
 
 	Context("when using apt_key module used (2)", func() {
@@ -96,12 +90,11 @@ var _ = Describe("Stapel builder with ansible", func() {
 			werfPurge("apt_key2", liveexec.ExecCommandOptions{})
 		})
 
-		It("should fail to install package without a key and succeed with the key", func(done Done) {
+		It("should fail to install package without a key and succeed with the key", func() {
 			Skip("https://github.com/flant/werf/issues/2000")
 
 			Expect(werfBuild("apt_key2", liveexec.ExecCommandOptions{})).To(Succeed())
-			close(done)
-		}, 500)
+		})
 	})
 
 	Context("when apt-mark from apt module used (https://github.com/flant/werf/issues/1820)", func() {
@@ -109,10 +102,9 @@ var _ = Describe("Stapel builder with ansible", func() {
 			werfPurge("apt_mark_panic_1820", liveexec.ExecCommandOptions{})
 		})
 
-		It("should not panic in all supported ubuntu versions", func(done Done) {
+		It("should not panic in all supported ubuntu versions", func() {
 			Expect(werfBuild("apt_mark_panic_1820", liveexec.ExecCommandOptions{})).To(Succeed())
-			close(done)
-		}, 500)
+		})
 	})
 
 	Context("when using yarn module to install nodejs packages", func() {
@@ -122,11 +114,10 @@ var _ = Describe("Stapel builder with ansible", func() {
 			os.RemoveAll("yarn_repo")
 		})
 
-		It("should install packages successfully", func(done Done) {
+		It("should install packages successfully", func() {
 			Expect(setGitRepoState("yarn", "yarn_repo", "initial commit")).To(Succeed())
 			Expect(werfBuild("yarn", liveexec.ExecCommandOptions{})).To(Succeed())
-			close(done)
-		}, 500)
+		})
 	})
 
 	Context("when installing python requirements using ansible and python files contain utf-8 chars", func() {
@@ -136,15 +127,14 @@ var _ = Describe("Stapel builder with ansible", func() {
 			os.RemoveAll("python_encoding_repo")
 		})
 
-		It("should install packages successfully without utf-8 related problems", func(done Done) {
+		It("should install packages successfully without utf-8 related problems", func() {
 			if runtime.GOOS == "windows" {
 				Skip("Skipping on windows")
 			}
 
 			Expect(setGitRepoState("python_encoding", "python_encoding_repo", "initial commit")).To(Succeed())
 			Expect(werfBuild("python_encoding", liveexec.ExecCommandOptions{})).To(Succeed())
-			close(done)
-		}, 900)
+		})
 	})
 
 	Context("Non standard PATH used in the base image (https://github.com/flant/werf/issues/1836) ", func() {
@@ -152,9 +142,8 @@ var _ = Describe("Stapel builder with ansible", func() {
 			werfPurge("path_redefined_in_stapel_1836", liveexec.ExecCommandOptions{})
 		})
 
-		It("PATH should not be redefined in stapel build container", func(done Done) {
+		It("PATH should not be redefined in stapel build container", func() {
 			Expect(werfBuild("path_redefined_in_stapel_1836", liveexec.ExecCommandOptions{})).To(Succeed())
-			close(done)
-		}, 500)
+		})
 	})
 })
