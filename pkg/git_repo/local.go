dfb31171695ea8e383796d51f59bbe3593cc0b3b
Timofey Kirillov <timofey.kirillov@flant.com>
2020-02-13 18:41:51 +0300

    Fix LogProcess Debug->DebugLog option

--- pkg/git_repo/local.go
+++ pkg/git_repo/local.go
@@ -62,7 +62,7 @@ func (repo *Local) CreateArchive(opts ArchiveOptions) (Archive, error) {
 }
 
 func (repo *Local) Checksum(opts ChecksumOptions) (checksum Checksum, err error) {
-	_ = logboek.LogProcess("Calculating checksum", logboek.LogProcessOptions{Debug: true}, func() error {
+	_ = logboek.LogProcess("Calculating checksum", logboek.LogProcessOptions{DebugLog: true}, func() error {
 		checksum, err = repo.checksumWithLsTree(repo.Path, repo.GitDir, repo.getRepoWorkTreeCacheDir(), opts)
 		return nil
 	})
