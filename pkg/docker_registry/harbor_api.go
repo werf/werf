7254053c0afc502321c5760aca6b6a4126f113fd
Alexey Igrychev <alexey.igrychev@flant.com>
2020-03-24 22:04:06 +0000

    Refactor

--- pkg/docker_registry/harbor_api.go
+++ pkg/docker_registry/harbor_api.go
@@ -1,14 +1,9 @@
 package docker_registry
 
 import (
-	"io/ioutil"
 	"net/http"
-	"net/url"
+	neturl "net/url"
 	"path"
-
-	"github.com/flant/logboek"
-
-	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
 )
 
 type harborApi struct{}
@@ -18,41 +13,24 @@ func newHarborApi() harborApi {
 }
 
 func (api *harborApi) DeleteRepository(hostname, repository, username, password string) (*http.Response, error) {
-	u, err := url.Parse("https://" + hostname + "/api")
+	u, err := neturl.Parse("https://" + hostname + "/api")
 	if err != nil {
 		return nil, err
 	}
 
 	u.Path = path.Join(u.Path, "repositories", repository)
+	url := u.String()
 
-	reqUrl := u.String()
-	reqAccept := "application/json"
+	resp, _, err := doRequest(http.MethodDelete, url, nil, doRequestOptions{
+		Headers: map[string]string{
+			"Accept": "application/json",
+		},
+		BasicAuth: doRequestBasicAuth{
+			username: username,
+			password: password,
+		},
+		AcceptedCodes: []int{http.StatusOK, http.StatusAccepted},
+	})
 
-	logboek.Debug.LogF("--> %s %s\n", http.MethodDelete, nil)
-	req, err := http.NewRequest(http.MethodDelete, reqUrl, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Accept", reqAccept)
-	req.SetBasicAuth(username, password)
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return resp, err
-	}
-
-	logboek.Debug.LogF("<-- %s %s\n", resp.Status, respBody)
-
-	if err := transport.CheckError(resp, http.StatusOK, http.StatusAccepted); err != nil {
-		return resp, err
-	}
-
-	return resp, nil
+	return resp, err
 }
