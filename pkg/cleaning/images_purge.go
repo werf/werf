21c8ddb4189d2f5ac73960e4e720b3944e2f821c
Alexey Igrychev <alexey.igrychev@flant.com>
2020-03-26 23:35:42 +0000

    [cleanup] Detailed error when user specifies incorrect credentials

--- pkg/cleaning/images_purge.go
+++ pkg/cleaning/images_purge.go
@@ -1,8 +1,11 @@
 package cleaning
 
 import (
+	"fmt"
+
 	"github.com/flant/logboek"
 
+	"github.com/flant/werf/pkg/docker_registry"
 	"github.com/flant/werf/pkg/image"
 	"github.com/flant/werf/pkg/storage"
 )
@@ -46,5 +49,23 @@ func (m *imagesPurgeManager) run() error {
 }
 
 func deleteRepoImageInImagesRepo(imagesRepo storage.ImagesRepo, dryRun bool, repoImageList ...*image.Info) error {
-	return deleteRepoImage(imagesRepo.DeleteRepoImage, storage.DeleteRepoImageOptions{}, dryRun, repoImageList...)
+	if err := deleteRepoImage(imagesRepo.DeleteRepoImage, storage.DeleteRepoImageOptions{}, dryRun, repoImageList...); err != nil {
+		switch err.(type) {
+		case docker_registry.DockerHubUnauthorizedError:
+			return fmt.Errorf(`%s
+You should specify Docker Hub token or username and password to remove tags with Docker Hub API.
+Check --repo-docker-hub-token/username/password --images-repo-docker-hub-token/username/password options.
+Be aware that access to the resource is forbidden with personal access token.
+Read more details here https://werf.io/documentation/reference/working_with_docker_registries.html#docker-hub`, err)
+		case docker_registry.GitHubPackagesUnauthorizedError:
+			return fmt.Errorf(`%s
+You should specify a token with the read:packages, write:packages, delete:packages and repo scopes to remove package versions.
+Check --repo-github-token and --images-repo-github-token options.
+Read more details here https://werf.io/documentation/reference/working_with_docker_registries.html#github-packages`, err)
+		default:
+			return err
+		}
+	}
+
+	return nil
 }
