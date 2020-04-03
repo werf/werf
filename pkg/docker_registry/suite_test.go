b5e33dad60a6efc420d3ecbaea60125487d1c317
Alexey Igrychev <alexey.igrychev@flant.com>
2020-04-03 18:15:56 +0100

    [unit tests] Resolve implementation name and repo mode

---
+++ pkg/docker_registry/suite_test.go
@@ -0,0 +1,13 @@
+package docker_registry_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Docker Registry Suite")
+}
