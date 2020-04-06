a3cedce1bb0d3d9ae978046fa2fe4c54fae5a912
Alexey Igrychev <alexey.igrychev@flant.com>
2020-04-06 11:25:48 +0100

    [docs] Up fromLatest and remote git mapping with branch warning message

--- pkg/config/git_remote_export.go
+++ pkg/config/git_remote_export.go
@@ -17,9 +17,11 @@ func (c *GitRemoteExport) validate() error {
 		msg := `Pay attention, werf uses git repository history to calculate stages signatures. Thus, the usage of remote git mapping with branch (by default, it is master branch) might break the reproducibility of previous builds. New commits in the branch will make previously built stages not usable.
 
 * Previous pipeline jobs (e.g. deploy) might not be retried without the image rebuild after git remote branch is modified.
-* If git remote branch is modified unexpectedly it might lead to the inexplicably failed pipeline. For instance, the modification occurs after successful build and the following pipeline jobs will be failed because of stages signatures will be changed alongside the branch.
+* If git remote branch is modified unexpectedly it might lead to the inexplicably failed pipeline. For instance, the modification occurs after successful build and the following pipeline jobs will be failed due to changing of stages signatures alongside the branch HEAD.
 
-If you want to use the branch for remote git mapping instead of commit or tag, add 'herebyIAdmitThatBranchMightBreakReproducibility: true' into the remote git mapping section.`
+If you want to use the branch for remote git mapping instead of commit or tag, add 'herebyIAdmitThatBranchMightBreakReproducibility: true' into the remote git mapping section.
+
+We do not recommend using the remote git mapping such way. Use a particular unchangeable reference, tag, or commit to provide controllable and predictable lifecycle of software.`
 
 		msg = "\n\n" + msg
 
--- pkg/config/stapel_image_base.go
+++ pkg/config/stapel_image_base.go
@@ -78,9 +78,11 @@ func (c *StapelImageBase) validate() error {
 		msg := `Pay attention, werf uses actual base image digest in stage signature if 'fromLatest' is specified. Thus, the usage of this directive might break the reproducibility of previous builds. If the base image is changed in the registry, all previously built stages become not usable.
 
 * Previous pipeline jobs (e.g. deploy) cannot be retried without the image rebuild after changing base image in the registry.
-* If base image is modified unexpectedly it might lead to the inexplicably failed pipeline. For instance, the modification occurs after successful build and the following jobs will be failed because of stages signatures are changing alongside base image digest.
+* If base image is modified unexpectedly it might lead to the inexplicably failed pipeline. For instance, the modification occurs after successful build and the following jobs will be failed due to changing of stages signatures alongside base image digest.
 
-If you still want to use this directive, also add 'herebyIAdmitThatFromLatestMightBreakReproducibility: true' alongside 'fromLatest'.`
+If you still want to use this directive, add 'herebyIAdmitThatFromLatestMightBreakReproducibility: true' alongside 'fromLatest'.
+
+We do not recommend using the actual base image such way. Use a particular unchangeable tag or periodically change 'fromCacheVersion' value to provide controllable and predictable lifecycle of software`
 
 		msg = "\n\n" + msg
 
