8322f3d78e131381d8743917cce437e6a3f49d9f
Alexey Igrychev <alexey.igrychev@flant.com>
2020-05-07 19:36:01 +0100

    [tests] Helm/Secret Suite: file/values decrypt/encrypt with pipe test

--- integration/helm/secret/file_and_values_test.go
+++ integration/helm/secret/file_and_values_test.go
@@ -14,39 +14,52 @@ import (
 )
 
 var _ = Describe("helm secret file/values encrypt/decrypt", func() {
-	decryptAndCheckFileOrValues := func(secretType, fileToProcess string) {
-		utils.RunSucceedCommand(
-			testDirPath,
-			werfBinPath,
-			"helm", "secret", secretType, "decrypt", fileToProcess, "-o", "result",
-		)
+	decryptAndCheckFileOrValues := func(secretType, fileToProcess string, withPipe bool) {
+		if withPipe {
+			runSucceedCommandWithFileDataOnStdin([]string{"helm", "secret", secretType, "decrypt", "-o", "result"}, fileToProcess)
+		} else {
+			utils.RunSucceedCommand(
+				testDirPath,
+				werfBinPath,
+				"helm", "secret", secretType, "decrypt", fileToProcess, "-o", "result",
+			)
+		}
+
 		fileContentsShouldBeEqual("result", "secret")
 	}
 
-	var decryptItBody = func(secretType string) {
+	var decryptItBody = func(secretType string, withPipe bool) {
 		utils.CopyIn(utils.FixturePath(secretType), testDirPath)
-		decryptAndCheckFileOrValues(secretType, "encrypted_secret")
+		decryptAndCheckFileOrValues(secretType, "encrypted_secret", withPipe)
 	}
 
-	var encryptItBody = func(secretType string) {
+	var encryptItBody = func(secretType string, withPipe bool) {
 		utils.CopyIn(utils.FixturePath(secretType), testDirPath)
 
-		utils.RunSucceedCommand(
-			testDirPath,
-			werfBinPath,
-			"helm", "secret", secretType, "encrypt", "secret", "-o", "result",
-		)
+		if withPipe {
+			runSucceedCommandWithFileDataOnStdin([]string{"helm", "secret", secretType, "encrypt", "-o", "result"}, "secret")
+		} else {
+			utils.RunSucceedCommand(
+				testDirPath,
+				werfBinPath,
+				"helm", "secret", secretType, "encrypt", "secret", "-o", "result",
+			)
+		}
 
-		decryptAndCheckFileOrValues(secretType, "result")
+		decryptAndCheckFileOrValues(secretType, "result", withPipe)
 	}
 
 	var _ = DescribeTable("encryption", encryptItBody,
-		Entry("secret file", "file"),
-		Entry("secret values", "values"))
+		Entry("secret file", "file", false),
+		Entry("secret file (pipe)", "file", true),
+		Entry("secret values", "values", false),
+		Entry("secret values (pipe)", "values", true))
 
 	var _ = DescribeTable("decryption", decryptItBody,
-		Entry("secret file", "file"),
-		Entry("secret values", "values"))
+		Entry("secret file", "file", false),
+		Entry("secret file (pipe)", "file", true),
+		Entry("secret values", "values", false),
+		Entry("secret values (pipe)", "values", true))
 })
 
 func fileContentsShouldBeEqual(path1, path2 string) {
@@ -68,4 +81,20 @@ func fileContentsShouldBeEqual(path1, path2 string) {
 	_, _ = fmt.Fprintf(GinkgoWriter, "=== %s ===\n", filepath.Join(testDirPath, path2))
 
 	Ω(bytes.Equal(data1, data2)).Should(BeTrue())
+}
+
+func runSucceedCommandWithFileDataOnStdin(werfArgs []string, secretFileName string) {
+	data, err := ioutil.ReadFile(filepath.Join(testDirPath, secretFileName))
+
+	Ω(err).ShouldNot(HaveOccurred())
+
+	_, _ = utils.RunCommandWithOptions(
+		testDirPath,
+		werfBinPath,
+		werfArgs,
+		utils.RunCommandOptions{
+			ToStdin:       string(data),
+			ShouldSucceed: true,
+		},
+	)
 }
