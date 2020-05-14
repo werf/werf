279cfba476b46d87a6dd9deb92a79ca55a23f125
Timofey Kirillov <timofey.kirillov@flant.com>
2020-05-14 14:39:22 +0300

    [tests] Building the same stage for two commits at the same time can lead to bug
    Test for a case when two images of the same stage for two consecutive commits are building at the same time. This can lead to the bug, because werf uses oldest stage by creation timestamp and not oldest by save-into-stages-storage timestamp.

---
+++ integration/build/stapel_image/build_phase/common_test.go
@@ -0,0 +1,29 @@
+package ansible_test
+
+import (
+	"fmt"
+
+	"github.com/flant/werf/pkg/testing/utils"
+	"github.com/flant/werf/pkg/testing/utils/liveexec"
+)
+
+func werfBuild(dir string, opts liveexec.ExecCommandOptions, extraArgs ...string) error {
+	return liveexec.ExecCommand(dir, werfBinPath, opts, utils.WerfBinArgs(append([]string{"build", "--stages-storage", ":local"}, extraArgs...)...)...)
+}
+
+func werfPurge(dir string, opts liveexec.ExecCommandOptions, extraArgs ...string) error {
+	return liveexec.ExecCommand(dir, werfBinPath, opts, utils.WerfBinArgs(append([]string{"stages", "purge", "--stages-storage", ":local"}, extraArgs...)...)...)
+}
+
+func setGitRepoState(workTreeDir, repoDir string, commitMessage string) error {
+	if err := liveexec.ExecCommand(".", "git", liveexec.ExecCommandOptions{}, append([]string{"init", workTreeDir, "--separate-git-dir", repoDir})...); err != nil {
+		return fmt.Errorf("unable to init git repo %s with work tree %s: %s", repoDir, workTreeDir, err)
+	}
+	if err := liveexec.ExecCommand(".", "git", liveexec.ExecCommandOptions{}, append([]string{"-C", workTreeDir, "add", "."})...); err != nil {
+		return fmt.Errorf("unable to add work tree %s files to git repo %s: %s", workTreeDir, repoDir, err)
+	}
+	if err := liveexec.ExecCommand(".", "git", liveexec.ExecCommandOptions{}, append([]string{"-C", workTreeDir, "commit", "-m", commitMessage, "--allow-empty"})...); err != nil {
+		return fmt.Errorf("unable to commit work tree %s files to git repo %s: %s", workTreeDir, repoDir, err)
+	}
+	return nil
+}
