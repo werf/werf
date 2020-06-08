c11ee8620a0dae6e329e8a8f4bcc8fead8bc5376
Timofey Kirillov <timofey.kirillov@flant.com>
2020-06-08 11:30:28 +0300

    [ci] Fix github owner to 'werf' for release publisher scripts

--- scripts/lib/release/global_data.sh
+++ scripts/lib/release/global_data.sh
@@ -3,7 +3,7 @@ BINTRAY_SUBJECT=flant     # bintray organization
 BINTRAY_REPO=werf         # bintray repository
 BINTRAY_PACKAGE=werf      # bintray package in repository
 
-GITHUB_OWNER=flant     # github user/org
+GITHUB_OWNER=werf      # github user/org
 GITHUB_REPO=werf       # github repository
 #PUBLISH_GITHUB_TOKEN  # github token granted permission to create releases
 
