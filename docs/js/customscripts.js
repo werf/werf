7da872fd0f11cd70e5cb41c62395b713e18ba384
Anton Klimov <anton.klimov@flant.ru>
2020-06-18 13:16:25 +0300

    [docs] Applications Guide markup fix

--- docs/js/customscripts.js
+++ docs/js/customscripts.js
@@ -251,7 +251,6 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-
   $('h1:contains("Installation")').each(function( index ) {
     var $title = $(this);
     var $btn1 = $title.next('p');
@@ -267,5 +266,28 @@ $(document).ready(function(){
     $btn2.remove();
     $btn3.remove();
     $title.after(new_btns);
+  });
+});
+
+var action_toast_timeout;
+function showActionToast(text) {
+  clearTimeout(action_toast_timeout);
+  var action_toast = $('.action-toast');
+  action_toast.text(text).fadeIn()
+  action_toast_timeout = setTimeout(function(){ action_toast.fadeOut() }, 5000);
+}
+
+$(document).ready(function(){
+  new ClipboardJS('[data-snippetcut-btn-name]', {
+    text: function(trigger) {
+      showActionToast('Имя скопировано')
+      return $(trigger).closest('[data-snippetcut]').find('[data-snippetcut-name]').text();
+    }
+  });
+  new ClipboardJS('[data-snippetcut-btn-text]', {
+    text: function(trigger) {
+      showActionToast('Текст скопирован')
+      return $(trigger).closest('[data-snippetcut]').find('[data-snippetcut-text]').text();
+    }
   });
 });
