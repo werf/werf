19f9f6545f84549102055eb3928fbfe6257e690a
Timofey Kirillov <timofey.kirillov@flant.com>
2020-06-18 13:35:08 +0300

    Enable 'Waiting for locked ...' log messages for kubernetes locker: update lockgate

--- pkg/werf/main.go
+++ pkg/werf/main.go
@@ -97,8 +97,8 @@ func ReleaseHostLock(lock lockgate.LockHandle) error {
 	return GetHostLocker().Release(lock)
 }
 
-func DefaultLockerOnWait(lock lockgate.LockHandle, doWait func() error) error {
-	logProcessMsg := fmt.Sprintf("Waiting for locked %q", lock.LockName)
+func DefaultLockerOnWait(lockName string, doWait func() error) error {
+	logProcessMsg := fmt.Sprintf("Waiting for locked %q", lockName)
 	return logboek.LogProcessInline(logProcessMsg, logboek.LogProcessInlineOptions{}, func() error {
 		return doWait()
 	})
