da26e2cfd41cd46fe3c7bfbfb9a4fabdb73034ab
Timofey Kirillov <timofey.kirillov@flant.com>
2020-07-23 20:16:09 +0300

    Synchronization http server main page landing with some info

--- pkg/storage/synchronization_server/synchronization_server.go
+++ pkg/storage/synchronization_server/synchronization_server.go
@@ -82,8 +82,34 @@ func (server *SynchronizationServerHandler) handleNewClientID(w http.ResponseWri
 	})
 }
 
+func (server *SynchronizationServerHandler) handleLanding(w http.ResponseWriter, r *http.Request) {
+	rawPage := ` <!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Werf synchronization http server</title>
+</head>
+<body>
+<h1>Werf synchronization http server</h1>
+
+<p>Werf uses --synchronization=https://synchronization.werf.io as a default synchronization service.</p>
+
+<p>Use "werf synchronization" command to run own synchronization http server. You can also configure werf to use local or kubernetes based synchronization backend.</p>
+
+<p>More info about synchronization in werf: <a href="https://werf.io/documentation/reference/stages_and_images.html#synchronization-locks-and-stages-storage-cache">https://werf.io/documentation/reference/stages_and_images.html#synchronization-locks-and-stages-storage-cache</a></p>
+</body>
+</html>
+`
+	fmt.Fprintf(w, rawPage)
+}
+
 func (server *SynchronizationServerHandler) handleRequestByClientID(w http.ResponseWriter, r *http.Request) {
 	logboek.Debug.LogF("SynchronizationServerHandler -- ServeHTTP url path = %q\n", r.URL.Path)
+
+	if r.URL.Path == "/" {
+		server.handleLanding(w, r)
+		return
+	}
 
 	clientID := strings.SplitN(strings.TrimPrefix(r.URL.Path, "/"), "/", 2)[0]
 	logboek.Debug.LogF("SynchronizationServerHandler -- ServeHTTP clientID = %q\n", clientID)
