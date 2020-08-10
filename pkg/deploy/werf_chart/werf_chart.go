31d2c9b348fe4756ed7178b8352e9b54af1bd98d
Timofey Kirillov <timofey.kirillov@flant.com>
2020-08-10 12:49:08 +0300

    Fix deploy error: at <.Values.global.env>: nil pointer evaluating interface {}.env

--- pkg/deploy/werf_chart/werf_chart.go
+++ pkg/deploy/werf_chart/werf_chart.go
@@ -74,17 +74,16 @@ func (chart *WerfChart) SetSecretValuesFile(path string, m secret.Manager) error
 }
 
 func (chart *WerfChart) Deploy(ctx context.Context, releaseName string, namespace string, opts helm.ChartOptions) error {
-	valuesOptions := values.Options{
-		ValueFiles:   append(chart.Values, opts.Values...),
-		StringValues: append(chart.SetString, opts.SetString...),
-		Values:       append(chart.Set, opts.Set...),
-		FileValues:   nil,
-	}
-	secretValues := append(chart.SecretValues, opts.SecretValues...)
-	_ = secretValues
-
 	if os.Getenv("WERF_HELM3") == "1" {
-		fmt.Printf("NAMESPACE=%v\n", namespace)
+		valuesOptions := values.Options{
+			ValueFiles:   append(chart.Values, opts.Values...),
+			StringValues: append(chart.SetString, opts.SetString...),
+			Values:       append(chart.Set, opts.Set...),
+			FileValues:   nil,
+		}
+		secretValues := append(chart.SecretValues, opts.SecretValues...)
+		_ = secretValues
+
 		return helm_v3.Upgrade(ctx, chart.ChartDir, releaseName, helm_v3.UpgradeOptions{
 			ValuesOptions:   valuesOptions,
 			Namespace:       namespace,
@@ -94,6 +93,11 @@ func (chart *WerfChart) Deploy(ctx context.Context, releaseName string, namespac
 			Timeout:         opts.Timeout,
 		})
 	} else {
+		opts.SecretValues = append(chart.SecretValues, opts.SecretValues...)
+		opts.Set = append(chart.Set, opts.Set...)
+		opts.SetString = append(chart.SetString, opts.SetString...)
+		opts.Values = append(chart.Values, opts.Values...)
+
 		return helm.DeployHelmChart(ctx, chart.ChartDir, releaseName, namespace, opts)
 	}
 }
