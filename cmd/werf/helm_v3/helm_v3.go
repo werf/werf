4faf34656343473c2ba215b39c3ffffe9c05600b
Timofey Kirillov <timofey.kirillov@flant.com>
2020-08-31 17:34:30 +0300

    Helmfile support for werf
    Werf now can be used as helm-3 replacement with helmfile as follows:
    
     1. Install latest helmfile.
    
     2. Install helm-diff plugin (which is needed for the helmfile):
    
    ```
    source $(multiwerf use 1.1 alpha --as-file)
    werf helm-v3 plugin install https://github.com/databus23/helm-diff
    ```
    
     3. Prepare helmfile.yaml:
    
    ```
    repositories:
    - name: stable
      url: https://kubernetes-charts.storage.googleapis.com
    
    releases:
    - name: mytraefik
      namespace: aaa
      createNamespace: true
      wait: true
      chart: stable/traefik
      labels:
        name: traefik-public
      version: 1.86.2
    ```
    
     4. Set environment variable `WERF_HELM3_MODE=1` — this will make werf binary CLI interface helm-compatible, then run `helmfile` with `--helm-binary=werf`.
    
    ```
    source $(multiwerf use 1.1 alpha --as-file)
    export WERF_HELM3_MODE=1
    helmfile -b werf apply
    ```
    
     5. Check installed release with werf:
    
    ```
    werf list --namespace aaa
    ```
    
    NOTE: Helmfile does not support live output from helm binary, so werf rich live output will be available only after werf command is done. However werf output with `--wait` flag is more useful than helm's output even in that condition and werf will "fail fast" when something goes wrong during deploy process.
    
    Refs https://github.com/werf/werf/issues/1606
    Refs https://github.com/werf/helm/pull/50

--- cmd/werf/helm_v3/helm_v3.go
+++ cmd/werf/helm_v3/helm_v3.go
@@ -1,59 +1,128 @@
 package helm_v3
 
 import (
-	"github.com/werf/werf/cmd/werf/helm_v3/create"
-	"github.com/werf/werf/cmd/werf/helm_v3/dependency"
-	"github.com/werf/werf/cmd/werf/helm_v3/env"
-	"github.com/werf/werf/cmd/werf/helm_v3/get"
-	"github.com/werf/werf/cmd/werf/helm_v3/history"
-	"github.com/werf/werf/cmd/werf/helm_v3/install"
-	"github.com/werf/werf/cmd/werf/helm_v3/lint"
-	"github.com/werf/werf/cmd/werf/helm_v3/list"
-	package_ "github.com/werf/werf/cmd/werf/helm_v3/package"
-	"github.com/werf/werf/cmd/werf/helm_v3/plugin"
-	"github.com/werf/werf/cmd/werf/helm_v3/pull"
-	"github.com/werf/werf/cmd/werf/helm_v3/repo"
-	"github.com/werf/werf/cmd/werf/helm_v3/rollback"
-	"github.com/werf/werf/cmd/werf/helm_v3/search"
-	"github.com/werf/werf/cmd/werf/helm_v3/show"
-	"github.com/werf/werf/cmd/werf/helm_v3/status"
-	"github.com/werf/werf/cmd/werf/helm_v3/template"
-	"github.com/werf/werf/cmd/werf/helm_v3/test"
-	"github.com/werf/werf/cmd/werf/helm_v3/uninstall"
-	"github.com/werf/werf/cmd/werf/helm_v3/upgrade"
-	"github.com/werf/werf/cmd/werf/helm_v3/verify"
+	"fmt"
+	"os"
+	"reflect"
+	"time"
+
+	"github.com/werf/kubedog/pkg/kube"
+	"github.com/werf/werf/pkg/deploy/helm_v3"
+
+	"helm.sh/helm/v3/pkg/action"
+
+	"github.com/werf/werf/cmd/werf/common"
+	cmd_werf_common "github.com/werf/werf/cmd/werf/common"
 
 	"github.com/spf13/cobra"
+
+	cmd_helm "helm.sh/helm/v3/cmd/helm"
 )
 
+var commonCmdData cmd_werf_common.CmdData
+
 func NewCmd() *cobra.Command {
+	actionConfig := new(action.Configuration)
+
 	cmd := &cobra.Command{
 		Use:   "helm-v3",
 		Short: "Manage application deployment with helm",
 	}
+
+	cmd.PersistentFlags().StringVarP(cmd_helm.Settings.GetNamespaceP(), "namespace", "n", *cmd_helm.Settings.GetNamespaceP(), "namespace scope for this request")
+	cmd_werf_common.SetupKubeConfig(&commonCmdData, cmd)
+	cmd_werf_common.SetupKubeConfigBase64(&commonCmdData, cmd)
+	cmd_werf_common.SetupKubeContext(&commonCmdData, cmd)
+	cmd_werf_common.SetupStatusProgressPeriod(&commonCmdData, cmd)
+	cmd_werf_common.SetupHooksStatusProgressPeriod(&commonCmdData, cmd)
+
 	cmd.AddCommand(
-		uninstall.NewCmd(),
-		dependency.NewCmd(),
-		get.NewCmd(),
-		history.NewCmd(),
-		lint.NewCmd(),
-		list.NewCmd(),
-		template.NewCmd(),
-		repo.NewCmd(),
-		rollback.NewCmd(),
-		install.NewCmd(),
-		upgrade.NewCmd(),
-		create.NewCmd(),
-		env.NewCmd(),
-		package_.NewCmd(),
-		plugin.NewCmd(),
-		pull.NewCmd(),
-		search.NewCmd(),
-		show.NewCmd(),
-		status.NewCmd(),
-		test.NewCmd(),
-		verify.NewCmd(),
+		cmd_helm.NewUninstallCmd(actionConfig, os.Stdout),
+		cmd_helm.NewDependencyCmd(os.Stdout),
+		cmd_helm.NewGetCmd(actionConfig, os.Stdout),
+		cmd_helm.NewHistoryCmd(actionConfig, os.Stdout),
+		cmd_helm.NewLintCmd(os.Stdout),
+		cmd_helm.NewListCmd(actionConfig, os.Stdout),
+		cmd_helm.NewTemplateCmd(actionConfig, os.Stdout),
+		cmd_helm.NewRepoCmd(os.Stdout),
+		cmd_helm.NewRollbackCmd(actionConfig, os.Stdout),
+		cmd_helm.NewInstallCmd(actionConfig, os.Stdout),
+		cmd_helm.NewUpgradeCmd(actionConfig, os.Stdout),
+		cmd_helm.NewCreateCmd(os.Stdout),
+		cmd_helm.NewEnvCmd(os.Stdout),
+		cmd_helm.NewPackageCmd(os.Stdout),
+		cmd_helm.NewPluginCmd(os.Stdout),
+		cmd_helm.NewPullCmd(os.Stdout),
+		cmd_helm.NewSearchCmd(os.Stdout),
+		cmd_helm.NewShowCmd(os.Stdout),
+		cmd_helm.NewStatusCmd(actionConfig, os.Stdout),
+		cmd_helm.NewTestCmd(actionConfig, os.Stdout),
+		cmd_helm.NewVerifyCmd(os.Stdout),
+		cmd_helm.NewVersionCmd(os.Stdout),
 	)
+
+	cmd_helm.LoadPlugins(cmd, os.Stdout)
+
+	commandsQueue := []*cobra.Command{cmd}
+	for len(commandsQueue) > 0 {
+		cmd := commandsQueue[0]
+		commandsQueue = commandsQueue[1:]
+
+		for _, cmd := range cmd.Commands() {
+			commandsQueue = append(commandsQueue, cmd)
+		}
+
+		if cmd.Runnable() {
+			oldRunE := cmd.RunE
+			oldRun := cmd.Run
+
+			cmd.RunE = func(cmd *cobra.Command, args []string) error {
+				// NOTE: Common init block for all runnable commands.
+
+				// FIXME: setup namespace env var for helm diff plugin
+
+				os.Setenv("WERF_HELM3_MODE", "1")
+
+				ctx := common.BackgroundContext()
+
+				helm_v3.InitActionConfig(ctx, cmd_helm.Settings, actionConfig, helm_v3.InitActionConfigOptions{
+					StatusProgressPeriod:      time.Duration(*commonCmdData.StatusProgressPeriodSeconds) * time.Second,
+					HooksStatusProgressPeriod: time.Duration(*commonCmdData.HooksStatusProgressPeriodSeconds) * time.Second,
+				})
+
+				if err := kube.Init(kube.InitOptions{kube.KubeConfigOptions{
+					Context:          *commonCmdData.KubeContext,
+					ConfigPath:       *commonCmdData.KubeConfig,
+					ConfigDataBase64: *commonCmdData.KubeConfigBase64,
+				}}); err != nil {
+					return fmt.Errorf("cannot initialize kube: %s", err)
+				}
+
+				if err := common.InitKubedog(ctx); err != nil {
+					return fmt.Errorf("cannot init kubedog: %s", err)
+				}
+
+				if oldRun != nil {
+					oldRun(cmd, args)
+					return nil
+				} else {
+					if err := oldRunE(cmd, args); err != nil {
+						errValue := reflect.ValueOf(err)
+						if errValue.Kind() == reflect.Struct {
+							codeValue := errValue.FieldByName("code")
+							if !codeValue.IsZero() {
+								os.Exit(int(codeValue.Int()))
+							}
+						}
+
+						return err
+					}
+
+					return nil
+				}
+			}
+		}
+	}
 
 	return cmd
 }
