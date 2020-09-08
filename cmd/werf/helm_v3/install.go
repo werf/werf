734add955db7e71d0ae4e833bd35c0412f346ae4
Timofey Kirillov <timofey.kirillov@flant.com>
2020-09-08 13:56:50 +0300

    [helm-v3] Secret values support for helm-v3 commands
    Refs https://github.com/werf/werf/issues/1606
    Refs https://github.com/werf/helm/pull/52

--- cmd/werf/helm_v3/install.go
+++ cmd/werf/helm_v3/install.go
@@ -1,20 +1,79 @@
 package helm_v3
 
 import (
+	"context"
+	"fmt"
 	"os"
+
+	"github.com/werf/werf/pkg/werf"
+
+	"github.com/werf/werf/pkg/deploy"
+
+	"github.com/werf/werf/pkg/deploy/werf_chart_v2"
 
 	"github.com/spf13/cobra"
 	cmd_werf_common "github.com/werf/werf/cmd/werf/common"
 
 	"helm.sh/helm/v3/pkg/action"
 
-	"github.com/werf/werf/pkg/deploy/helm_v3"
 	cmd_helm "helm.sh/helm/v3/cmd/helm"
 )
 
 func NewInstallCmd(actionConfig *action.Configuration, commonCmdData *cmd_werf_common.CmdData) *cobra.Command {
-	postRenderer := helm_v3.NewExtraAnnotationsAndLabelsPostRenderer(helm_v3.DefaultExtraAnnotations, nil)
-	cmd := cmd_helm.NewInstallCmd(actionConfig, os.Stdout, cmd_helm.InstallCmdOptions{PostRenderer: postRenderer})
-	SetupExtraAnnotationsAndLabelsForCmd(cmd, commonCmdData, postRenderer)
+	wc := werf_chart_v2.NewWerfChart()
+	cmd, helmAction := cmd_helm.NewInstallCmd(actionConfig, os.Stdout, cmd_helm.InstallCmdOptions{
+		PostRenderer: wc.ExtraAnnotationsAndLabelsPostRenderer,
+		ValueOpts:    wc.ValueOpts,
+	})
+
+	cmd_werf_common.SetupTmpDir(commonCmdData, cmd)
+	cmd_werf_common.SetupHomeDir(commonCmdData, cmd)
+
+	cmd_werf_common.SetupAddAnnotations(commonCmdData, cmd)
+	cmd_werf_common.SetupAddLabels(commonCmdData, cmd)
+
+	cmd_werf_common.SetupSecretValues(commonCmdData, cmd)
+	cmd_werf_common.SetupIgnoreSecretKey(commonCmdData, cmd)
+
+	oldRunE := cmd.RunE
+	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		if err := werf.Init(*commonCmdData.TmpDir, *commonCmdData.HomeDir); err != nil {
+			return err
+		}
+
+		werfChartInitOpts := werf_chart_v2.WerfChartInitOptions{
+			SecretValuesFiles: *commonCmdData.SecretValues,
+		}
+		if extraAnnotations, err := cmd_werf_common.GetUserExtraAnnotations(commonCmdData); err != nil {
+			return err
+		} else {
+			werfChartInitOpts.ExtraAnnotations = extraAnnotations
+		}
+		if extraLabels, err := cmd_werf_common.GetUserExtraLabels(commonCmdData); err != nil {
+			return err
+		} else {
+			werfChartInitOpts.ExtraLabels = extraLabels
+		}
+
+		_, chartDir, err := helmAction.NameAndChart(args)
+		if err != nil {
+			return err
+		}
+
+		// NOTE: project-dir is the same as chart-dir for werf helm-v3 install/upgrade commands
+		// NOTE: project-dir is werf-project dir only for werf deploy/dismiss commands
+		if m, err := deploy.GetSafeSecretManager(context.Background(), chartDir, chartDir, *commonCmdData.SecretValues, *commonCmdData.IgnoreSecretKey); err != nil {
+			return err
+		} else {
+			werfChartInitOpts.SecretsManager = m
+		}
+
+		if err := wc.Init(chartDir, werfChartInitOpts); err != nil {
+			return fmt.Errorf("unable to init werf chart: %s", err)
+		}
+
+		return oldRunE(cmd, args)
+	}
+
 	return cmd
 }
---
+++ pkg/deploy/werf_chart_v2/secret_values.yaml.go
@@ -0,0 +1,28 @@
+package werf_chart_v2
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/werf/werf/pkg/deploy/secret"
+	"sigs.k8s.io/yaml"
+)
+
+func DecodeSecretValuesFile(path string, m secret.Manager) (map[string]interface{}, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read file %q: %s", path, err)
+	}
+
+	decodedData, err := m.DecryptYamlData(data)
+	if err != nil {
+		return nil, fmt.Errorf("cannot decode file %q secret data: %s", path, err)
+	}
+
+	rawValues := map[string]interface{}{}
+	if err := yaml.Unmarshal(decodedData, &rawValues); err != nil {
+		return nil, fmt.Errorf("cannot unmarshal secret values file %s: %s", path, err)
+	}
+
+	return rawValues, nil
+}
