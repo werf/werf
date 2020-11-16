4524b2164d781c08ca761d7161d41e10d2307cad
Timofey Kirillov <timofey.kirillov@flant.com>
2020-11-16 12:23:31 +0300

    [determinism] Refactor mount directive restriction in determinism mode: properly use validation func

--- pkg/config/mount.go
+++ pkg/config/mount.go
@@ -12,7 +12,15 @@ type Mount struct {
 	raw *rawMount
 }
 
-func (c *Mount) validate() error {
+func (c *Mount) validate(disableDeterminism bool) error {
+	if !disableDeterminism {
+		return fmt.Errorf("'mount' directive is forbidden, it is recommended to avoid this directive, otherwise disable werf determinism mode with option --disable-determinism (or WERF_DISABLE_DETERMINISM=1 environment variable)")
+	}
+
+	if c.raw.From != "" && c.raw.FromPath != "" {
+		return newDetailedConfigError(fmt.Sprintf("cannot use `from: %s` and `fromPath: %s` at the same time for mount!", c.raw.From, c.raw.FromPath), c, c.raw.rawStapelImage.doc)
+	}
+
 	if c.To == "" || !isAbsolutePath(c.To) {
 		return newDetailedConfigError("`to: PATH` absolute path required for mount!", c.raw, c.raw.rawStapelImage.doc)
 	} else if c.Type == "custom_dir" {
