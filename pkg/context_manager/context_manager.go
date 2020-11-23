b6e77900a77b3cc6a526827888b34c2709512aaf
Timofey Kirillov <timofey.kirillov@flant.com>
2020-11-23 13:27:29 +0300

    [determinism] Apply contextAddFile directive to the context archive when copying archive on the fly

--- pkg/context_manager/context_manager.go
+++ pkg/context_manager/context_manager.go
@@ -9,10 +9,16 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/werf/logboek"
+	uuid "github.com/satori/go.uuid"
 
+	"github.com/werf/logboek"
 	"github.com/werf/werf/pkg/util"
+	"github.com/werf/werf/pkg/werf"
 )
+
+func GetContextTmpDir() string {
+	return filepath.Join(werf.GetServiceDir(), "tmp", "context")
+}
 
 func ContextAddFileChecksum(ctx context.Context, contextAddFile []string, projectDir string) (string, error) {
 	logboek.Context(ctx).Debug().LogF("-- ContextAddFileChecksum %q %q\n", projectDir, contextAddFile)
@@ -42,90 +48,141 @@ func ContextAddFileChecksum(ctx context.Context, contextAddFile []string, projec
 	return fmt.Sprintf("%x", h.Sum(nil)), nil
 }
 
-func ApplyContextAddFileToArchive(ctx context.Context, archivePath string, context string, contextAddFile []string, projectDir string) error {
-	logboek.Context(ctx).Debug().LogF("-- ApplyContextAddFileToArchive %q %q %v %q\n", archivePath, context, contextAddFile, projectDir)
+type contextAddFileDescriptor struct {
+	AddFile           string
+	PathInsideContext string
+}
 
-	file, err := os.OpenFile(archivePath, os.O_RDWR, os.ModePerm)
+func ApplyContextAddFileToArchive(ctx context.Context, originalArchivePath string, contextPath string, contextAddFile []string, projectDir string) (string, error) {
+	path := filepath.Join(GetContextTmpDir(), uuid.NewV4().String())
+	if err := os.MkdirAll(filepath.Dir(path), 0777); err != nil {
+		return "", fmt.Errorf("unable to create dir %q: %s", filepath.Dir(path), err)
+	}
+
+	logboek.Context(ctx).Default().LogF("Will copy %q archive to %q\n", originalArchivePath, path)
+
+	source, err := os.Open(originalArchivePath)
 	if err != nil {
-		return fmt.Errorf("unable to open %q: %s", archivePath, err)
+		return "", fmt.Errorf("unable to open %q: %s", originalArchivePath, err)
 	}
-	if _, err := file.Seek(-2<<9, os.SEEK_END); err != nil {
-		return err
+	defer source.Close()
+
+	destination, err := os.Create(path)
+	if err != nil {
+		return "", fmt.Errorf("unable to create %q: %s", path, err)
 	}
+	defer destination.Close()
 
-	tw := tar.NewWriter(file)
+	tr := tar.NewReader(source)
+	tw := tar.NewWriter(destination)
+	defer tw.Close()
 
+	var contextAddFileDescriptors []*contextAddFileDescriptor
 	for _, addFile := range contextAddFile {
-		sourceFilePath := filepath.Join(projectDir, addFile)
-
 		var destFilePath string
-		if context != "" {
-			if !util.IsSubpathOfBasePath(context, addFile) {
-				return fmt.Errorf("specified contextAddFile %q is out of context %q", addFile, context)
+		if contextPath != "" {
+			if !util.IsSubpathOfBasePath(contextPath, addFile) {
+				return "", fmt.Errorf("specified contextAddFile %q is out of context %q", addFile, contextPath)
 			}
-			destFilePath = util.GetRelativeToBaseFilepath(context, addFile)
+			destFilePath = util.GetRelativeToBaseFilepath(contextPath, addFile)
 		} else {
 			destFilePath = addFile
 		}
-		tarEntryName := filepath.ToSlash(destFilePath)
 
-		sourceFileStat, err := os.Lstat(sourceFilePath)
-		if err != nil {
-			return fmt.Errorf("error accessing %q stat: %s", sourceFilePath, err)
-		}
-
-		isSymlink := sourceFileStat.Mode()&os.ModeSymlink != 0
-		if isSymlink {
-			linkname, err := os.Readlink(sourceFilePath)
-			if err != nil {
-				return fmt.Errorf("cannot read symlink %q: %s", sourceFilePath, err)
-			}
-
-			if err := tw.WriteHeader(&tar.Header{
-				Format:     tar.FormatGNU,
-				Typeflag:   tar.TypeSymlink,
-				Name:       tarEntryName,
-				Linkname:   linkname,
-				Mode:       int64(sourceFileStat.Mode()),
-				Size:       sourceFileStat.Size(),
-				ModTime:    sourceFileStat.ModTime(),
-				AccessTime: sourceFileStat.ModTime(),
-				ChangeTime: sourceFileStat.ModTime(),
-			}); err != nil {
-				return fmt.Errorf("unable to write tar symlink header for file %s: %s", tarEntryName, err)
-			}
-		} else if sourceFileStat.Mode().IsRegular() {
-			if err := tw.WriteHeader(&tar.Header{
-				Format:     tar.FormatGNU,
-				Name:       tarEntryName,
-				Mode:       int64(sourceFileStat.Mode()),
-				Size:       sourceFileStat.Size(),
-				ModTime:    sourceFileStat.ModTime(),
-				AccessTime: sourceFileStat.ModTime(),
-				ChangeTime: sourceFileStat.ModTime(),
-			}); err != nil {
-				return fmt.Errorf("unable to write tar header for file %q: %s", tarEntryName, err)
-			}
-
-			f, err := os.Open(sourceFilePath)
-			if err != nil {
-				return fmt.Errorf("unable to open file %q: %s", sourceFilePath, err)
-			}
-
-			if _, err := io.Copy(tw, f); err != nil {
-				return fmt.Errorf("unable to write data to tar archive from file %q: %s", sourceFilePath, err)
-			}
-
-			if err := f.Close(); err != nil {
-				return fmt.Errorf("error closing file %q: %s", sourceFilePath, err)
-			}
-		} else {
-			return fmt.Errorf("unexpected contextAddFile %q file type %x: only regular files or symlinks are supported", addFile, sourceFileStat.Mode())
-		}
+		contextAddFileDescriptors = append(contextAddFileDescriptors, &contextAddFileDescriptor{
+			AddFile:           addFile,
+			PathInsideContext: destFilePath,
+		})
 	}
 
-	if err := tw.Close(); err != nil {
-		return fmt.Errorf("cannot write tar archive: %s", err)
+CopyArchive:
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return "", fmt.Errorf("error reading archive %q: %s", originalArchivePath, err)
+		}
+
+		for _, addFileDesc := range contextAddFileDescriptors {
+			if hdr.Name == filepath.ToSlash(addFileDesc.PathInsideContext) {
+				logboek.Context(ctx).Default().LogF("Matched file %q for replacement in the archive %q by contextAddFile=%q directive\n", hdr.Name, path, addFileDesc.AddFile)
+				continue CopyArchive
+			}
+		}
+
+		tw.WriteHeader(hdr)
+
+		if _, err := io.Copy(tw, tr); err != nil {
+			return "", fmt.Errorf("error copying %q from %q archive to %q: %s", hdr.Name, originalArchivePath, path, err)
+		}
+
+		logboek.Context(ctx).Default().LogF("Copied %s from %q archive to %q\n", hdr.Name, originalArchivePath, path)
+	}
+
+	for _, addFileDesc := range contextAddFileDescriptors {
+		sourceFilePath := filepath.Join(projectDir, addFileDesc.AddFile)
+		tarEntryName := filepath.ToSlash(addFileDesc.PathInsideContext)
+		if err := copyFileIntoTar(sourceFilePath, tarEntryName, tw); err != nil {
+			return "", fmt.Errorf("unable to copy %q from workinto archive %q: %s", sourceFilePath, path, err)
+		}
+		logboek.Context(ctx).Default().LogF("Copied file %q in the archive %q with %q file from working directory (contextAddFile=%s directive)\n", tarEntryName, path, sourceFilePath, addFileDesc.AddFile)
+	}
+
+	return path, nil
+}
+
+func copyFileIntoTar(sourceFilePath string, tarEntryName string, tw *tar.Writer) error {
+	sourceFileStat, err := os.Lstat(sourceFilePath)
+	if err != nil {
+		return fmt.Errorf("error accessing %q stat: %s", sourceFilePath, err)
+	}
+
+	isSymlink := sourceFileStat.Mode()&os.ModeSymlink != 0
+	if isSymlink {
+		linkname, err := os.Readlink(sourceFilePath)
+		if err != nil {
+			return fmt.Errorf("cannot read symlink %q: %s", sourceFilePath, err)
+		}
+
+		if err := tw.WriteHeader(&tar.Header{
+			Format:     tar.FormatGNU,
+			Typeflag:   tar.TypeSymlink,
+			Name:       tarEntryName,
+			Linkname:   linkname,
+			Mode:       int64(sourceFileStat.Mode()),
+			Size:       sourceFileStat.Size(),
+			ModTime:    sourceFileStat.ModTime(),
+			AccessTime: sourceFileStat.ModTime(),
+			ChangeTime: sourceFileStat.ModTime(),
+		}); err != nil {
+			return fmt.Errorf("unable to write tar symlink header for file %s: %s", tarEntryName, err)
+		}
+	} else if sourceFileStat.Mode().IsRegular() {
+		if err := tw.WriteHeader(&tar.Header{
+			Format:     tar.FormatGNU,
+			Name:       tarEntryName,
+			Mode:       int64(sourceFileStat.Mode()),
+			Size:       sourceFileStat.Size(),
+			ModTime:    sourceFileStat.ModTime(),
+			AccessTime: sourceFileStat.ModTime(),
+			ChangeTime: sourceFileStat.ModTime(),
+		}); err != nil {
+			return fmt.Errorf("unable to write tar header for file %q: %s", tarEntryName, err)
+		}
+
+		f, err := os.Open(sourceFilePath)
+		if err != nil {
+			return fmt.Errorf("unable to open file %q: %s", sourceFilePath, err)
+		}
+
+		if _, err := io.Copy(tw, f); err != nil {
+			return fmt.Errorf("unable to write data to tar archive from file %q: %s", sourceFilePath, err)
+		}
+
+		if err := f.Close(); err != nil {
+			return fmt.Errorf("error closing file %q: %s", sourceFilePath, err)
+		}
 	}
 
 	return nil
