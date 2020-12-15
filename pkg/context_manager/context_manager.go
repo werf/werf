83f23e4c209578838b6dfd5e8d0d718461970f86
Alexey Igrychev <alexey.igrychev@flant.com>
2020-12-15 15:51:33 +0000

    [dockerfile] Fix contextAddFile checksum calculation
    * Fix empty checksum
    * Fix os-sensitive contextAddFile path in the checksum

--- pkg/context_manager/context_manager.go
+++ pkg/context_manager/context_manager.go
@@ -18,56 +18,61 @@ import (
 	"github.com/werf/werf/pkg/werf"
 )
 
-func GetContextTmpDir() string {
+func GetTmpDir() string {
 	return filepath.Join(werf.GetServiceDir(), "tmp", "context")
 }
 
 func GetTmpArchivePath() string {
-	return filepath.Join(GetContextTmpDir(), uuid.NewV4().String())
+	return filepath.Join(GetTmpDir(), uuid.NewV4().String())
 }
 
 func ContextAddFileChecksum(ctx context.Context, projectDir string, contextDir string, contextAddFile []string, matcher path_matcher.PathMatcher) (string, error) {
-	logboek.Context(ctx).Debug().LogF("-- ContextAddFileChecksum %q %q\n", projectDir, contextAddFile)
-
-	h := sha256.New()
-
+	var filePathListRelativeToProject []string
 	for _, addFileRelativeToContext := range contextAddFile {
 		addFileRelativeToProject := filepath.Join(contextDir, addFileRelativeToContext)
 		if !matcher.MatchPath(addFileRelativeToProject) {
 			continue
 		}
 
-		h.Write([]byte(addFileRelativeToContext))
+		filePathListRelativeToProject = append(filePathListRelativeToProject, addFileRelativeToProject)
+	}
 
-		addFileAbsolute := filepath.Join(projectDir, addFileRelativeToProject)
-		if _, err := os.Stat(addFileAbsolute); os.IsNotExist(err) {
+	if len(filePathListRelativeToProject) == 0 {
+		return "", nil
+	}
+
+	h := sha256.New()
+	for _, pathRelativeToProject := range filePathListRelativeToProject {
+		pathWithSlashes := filepath.ToSlash(pathRelativeToProject)
+		h.Write([]byte(pathWithSlashes))
+
+		absolutePath := filepath.Join(projectDir, pathRelativeToProject)
+		if exists, err := util.RegularFileExists(absolutePath); err != nil {
+			return "", fmt.Errorf("unable to check existence of file %q: %s", absolutePath, err)
+		} else if !exists {
 			continue
-		} else if err != nil {
-			return "", fmt.Errorf("error accessing %q: %s", addFileAbsolute, err)
 		}
 
 		if err := func() error {
-			f, err := os.Open(addFileAbsolute)
+			f, err := os.Open(absolutePath)
 			if err != nil {
-				return fmt.Errorf("unable to open %q: %s", addFileAbsolute, err)
+				return fmt.Errorf("unable to open %q: %s", absolutePath, err)
 			}
 			defer f.Close()
 
 			if _, err := io.Copy(h, f); err != nil {
-				return fmt.Errorf("error reading %q: %s", addFileAbsolute, err)
+				return fmt.Errorf("unable to copy file %q: %s", absolutePath, err)
 			}
 
 			return nil
 		}(); err != nil {
 			return "", err
 		}
+
+		logboek.Context(ctx).Debug().LogF("File was added: %q\n", pathWithSlashes)
 	}
 
-	if h.Size() == 0 {
-		return "", nil
-	} else {
-		return fmt.Sprintf("%x", h.Sum(nil)), nil
-	}
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
 }
 
 func AddContextAddFileToContextArchive(ctx context.Context, originalArchivePath string, projectDir string, contextDir string, contextAddFile []string) (string, error) {
