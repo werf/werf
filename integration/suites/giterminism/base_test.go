573ec7b74e3830480d27258b1638f1c326050416
Alexey Igrychev <alexey.igrychev@flant.com>
2021-01-13 14:53:41 +0000

    [tests] Giterminism Suite: the config and templates tests

---
+++ integration/suites/giterminism/base_test.go
@@ -0,0 +1,23 @@
+package giterminism_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/werf/werf/integration/pkg/utils"
+)
+
+var _ = XDescribe("werf-giterminism.yaml", func() {
+	BeforeEach(BaseBeforeEach)
+
+	It("werf-giterminism.yaml must be committed", func() {
+		output, err := utils.RunCommand(
+			SuiteData.TestDirPath,
+			SuiteData.WerfBinPath,
+			"config", "render",
+		)
+
+		Ω(err).Should(HaveOccurred())
+		Ω(output).Should(ContainSubstring("giterminism configuration file 'werf-giterminism.yaml' must be committed"))
+	})
+})
