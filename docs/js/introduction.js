3697fb6a7f953023eac0c5528cb95f638b1342bc
Timofey Kirillov <timofey.kirillov@flant.com>
2021-03-12 18:12:38 +0300

    300

--- docs/js/introduction.js
+++ docs/js/introduction.js
@@ -1,12 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
   if ($('#introduction-presentation').length) {
     var ip_slides = [];
     var ip_magic = new ScrollMagic.Controller();
-    var ip_offset = ($('#introduction-presentation').position().top - window.innerHeight/2) + 400;
+    var ip_offset = ($('#introduction-presentation').position().top - window.innerHeight / 2) + 400;
     var ip_offset_step = 400;
 
+    var aaa = 300;
+
     var zi = 1000;
-    $('.introduction-presentation__slide').each(function() {
+    $('.introduction-presentation__slide').each(function () {
       $(this).css('z-index', zi); zi--;
       ip_slides.push($(this)[0]);
     })
@@ -26,44 +28,44 @@ $(document).ready(function() {
         duration: 100,
         offset: ip_offset
       })
-      .setTween(
-        new TimelineMax().to(slide, {
-          opacity: '0',
-          display: 'none'
-        }, 0)
-      )
-      .on('start', (event) => {
-        if (event.scrollDirection == 'FORWARD') {
-          updateControls(slide_index+1)
-        } else {
-          updateControls(slide_index)
-        }
-      })
-      .addTo(ip_magic);
+        .setTween(
+          new TimelineMax().to(slide, {
+            opacity: '0',
+            display: 'none'
+          }, 0)
+        )
+        .on('start', (event) => {
+          if (event.scrollDirection == 'FORWARD') {
+            updateControls(slide_index + 1)
+          } else {
+            updateControls(slide_index)
+          }
+        })
+        .addTo(ip_magic);
 
       ip_offset = ip_offset + ip_offset_step;
     }
     new ScrollMagic.Scene({
-      duration: (ip_slides.length*ip_offset_step) + ip_offset_step/2,
+      duration: (ip_slides.length * ip_offset_step) + ip_offset_step / 2,
       triggerElement: '#introduction-presentation',
-      offset: window.innerHeight*0.7/2
+      offset: window.innerHeight * 0.7 / 2
     })
-    .setPin('#introduction-presentation')
-    .addTo(ip_magic);
+      .setPin('#introduction-presentation')
+      .addTo(ip_magic);
 
     // Hide controls
     new ScrollMagic.Scene({
       duration: 100,
-      offset: (ip_slides.length*ip_offset_step) + ip_offset_step/2
+      offset: (ip_slides.length * ip_offset_step) + ip_offset_step / 2
     })
-    .setTween(
-      new TimelineMax().to('#introduction-presentation-controls', {
-        opacity: '0',
-      }, 0)
-    )
-    .addTo(ip_magic);
+      .setTween(
+        new TimelineMax().to('#introduction-presentation-controls', {
+          opacity: '0',
+        }, 0)
+      )
+      .addTo(ip_magic);
 
-    ip_slides.forEach(function(ip_slide, index) {
+    ip_slides.forEach(function (ip_slide, index) {
       addIpScene(ip_slide, index);
     })
 
@@ -72,10 +74,10 @@ $(document).ready(function() {
       $('.introduction-presentation__controls-selector').toggle();
     })
 
-    $('.introduction-presentation__controls-selector a').on('click', function(e) {
+    $('.introduction-presentation__controls-selector a').on('click', function (e) {
       e.preventDefault();
       var slide = $(this).data('presentation-selector-option');
-      var scroll = ($('#introduction-presentation').position().top - window.innerHeight/2) + 400*(slide+2);
+      var scroll = ($('#introduction-presentation').position().top - window.innerHeight / 2) + 400 * (slide + 2);
 
       window.scrollTo(0, scroll);
       $('.introduction-presentation__controls-selector').toggle();
