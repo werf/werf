609fee66e615a053470dd8575902a8ad6fb33d89
Alexey Igrychev <alexey.igrychev@flant.com>
2021-04-08 11:22:24 +0100

    [logging] Build: add waiting for background tasks log message

--- pkg/util/parallel/parallel.go
+++ pkg/util/parallel/parallel.go
@@ -43,7 +43,8 @@ func DoTasks(ctx context.Context, numberOfTasks int, options DoTasksOptions, tas
 	doneTaskCh := make(chan interface{})
 	doneWorkerCh := make(chan worker)
 	quitCh := make(chan bool)
-	doneWorkersCounter := numberOfWorkers
+	activeTasksCounter := numberOfTasks
+	activeWorkersCounter := numberOfWorkers
 	isLiveOutputOnFlag := options.IsLiveOutputOn
 
 	var liveLogger types.LoggerInterface
@@ -132,6 +133,20 @@ func DoTasks(ctx context.Context, numberOfTasks int, options DoTasksOptions, tas
 					processTaskResultData(ctx, taskResult.data)
 				}
 			}
+
+			activeTasksCounter--
+
+			// skip if all done
+			if activeTasksCounter == 0 {
+				continue
+			}
+
+			// skip if live worker disabled or still in progress
+			if !options.IsLiveOutputOn || isLiveOutputOnFlag {
+				continue
+			}
+
+			logboek.Context(ctx).Default().LogF("Waiting for background tasks (%d/%d) ... \n", activeTasksCounter, numberOfTasks)
 		case res := <-errCh:
 			close(quitCh)
 
@@ -180,8 +195,8 @@ func DoTasks(ctx context.Context, numberOfTasks int, options DoTasksOptions, tas
 				}
 			}
 
-			doneWorkersCounter--
-			if doneWorkersCounter == 0 {
+			activeWorkersCounter--
+			if activeWorkersCounter == 0 {
 				return nil
 			}
 		}
