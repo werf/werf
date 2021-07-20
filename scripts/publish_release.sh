131f8b98a53309e480ed917146951258934a16ca
Timofey Kirillov <timofey.kirillov@flant.com>
2021-07-20 18:17:56 +0300

    infra(release_publisher): upload built binaries to S3

--- scripts/publish_release.sh
+++ scripts/publish_release.sh
@@ -14,19 +14,6 @@ if [ -z "$VERSION" ] ; then
     exit 1
 fi
 
-if [ -z "$PUBLISH_BINTRAY_AUTH"  ] ; then
-    echo "\$PUBLISH_BINTRAY_AUTH required!" 1>&2
-    exit 1
-fi
-
-if [ -z "$PUBLISH_GITHUB_TOKEN"  ] ; then
-    echo "\$PUBLISH_GITHUB_TOKEN required!" 1>&2
-    exit 1
-fi
-
-( which git > /dev/null ) || ( echo "Cannot find git command!" 1>&2 && exit 1 )
-( which curl > /dev/null ) || ( echo "Cannot find curl command!" 1>&2 && exit 1 )
-
-# ( go_build $VERSION ) || ( echo "Failed to build!" 1>&2 && exit 1 )
-# ( publish_binaries $VERSION ) || ( echo "Failed to publish release binaries!" 1>&2 && exit 1 )
+( go_build $VERSION ) || ( echo "Failed to build!" 1>&2 && exit 1 )
+( publish_binaries $VERSION ) || ( echo "Failed to publish release binaries!" 1>&2 && exit 1 )
 ( create_github_release $VERSION ) || ( echo "Failed to create github release!" 1>&2 && exit 1 )
