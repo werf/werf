eb282bd6ad481b1c36cc31d401bc35ba09b4a7a8
Timofey Kirillov <timofey.kirillov@flant.com>
2021-09-14 15:47:01 +0300

    infra: use gox parallel release build tool, update base builder image
     - New script scripts/build_release_v3.sh uses gox build tool to create release binaries.
     - Tag werf-builder images by the commit in the scripts/werf-builder/{build|publish}.sh scripts.
     - Update trdl.yaml to use new build tool and new base image.

---
+++ scripts/build_release_v3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+VERSION=$1
+if [ -z "$VERSION" ] ; then
+    echo "Required version argument!" 1>&2
+    echo 1>&2
+    echo "Usage: $0 VERSION" 1>&2
+    exit 1
+fi
+
+gox -osarch="linux/amd64 linux/arm64 darwin/amd64 darwin/arm64 windows/amd64" \
+    -output="release-build/$VERSION/{{.OS}}-{{.Arch}}/werf" \
+    -tags "dfrunmount dfssh" \
+    -ldflags="-s -w -X github.com/werf/werf/pkg/werf.Version=$VERSION" \
+        github.com/werf/werf/cmd/werf
