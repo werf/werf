1002f400d546ff0261c8fb86d2544d5eb427c8ff
Timofey Kirillov <timofey.kirillov@flant.com>
2021-09-22 14:03:37 +0300

    wip: buildah lowlevel crossplatform package new interface

--- pkg/buildah/buildah.go
+++ pkg/buildah/buildah.go
@@ -2,112 +2,76 @@ package buildah
 
 import (
 	"context"
-	"fmt"
-	"os"
+	"io"
 
-	"github.com/containers/buildah"
-	buildahcli "github.com/containers/buildah/pkg/cli"
+	"github.com/docker/docker/pkg/reexec"
 
-	is "github.com/containers/image/v5/storage"
-	"github.com/containers/storage"
 	"github.com/containers/storage/pkg/unshare"
-
-	"github.com/pkg/errors"
 )
 
-/*
-InitProcess() {
-	if reexec.Init() {
-		return
-	}
-	
-	unshare.MaybeReexecUsingUserNamespace(false)
+type CommonOpts struct {
+	LogWriter io.Writer
 }
 
-Init(Options) -> BuildahObject {
-	parse options & init store
-}
-BuildahObject.Bud(ctx, ...) // обязательно принимать dockerfile через []byte, т.к. гитерминизм 
-BuildahObject.Run(ctx)
-...
-*/
-
-var store storage.Store
-
-func Init() error {
-	options, err := storage.DefaultStoreOptions(unshare.IsRootless(), unshare.GetRootlessUID())
-	if err != nil {
-		return err
-	}
-
-	//fmt.Printf("OPTIONS: %#v\n", options)
-
-	s, err := storage.GetStore(options)
-	if err != nil {
-		return fmt.Errorf("unable to get storage: %s", err)
-	}
-	if s != nil {
-		is.Transport.SetStore(store)
-	}
-	store = s
-
-	return nil
+type BuildFromDockerfileOpts struct {
+	CommonOpts
+	ContextTar io.Reader
 }
 
-type runInputOptions struct {
-	addHistory  bool
-	capAdd      []string
-	capDrop     []string
-	env         []string
-	hostname    string
-	isolation   string
-	mounts      []string
-	runtime     string
-	runtimeFlag []string
-	noPivot     bool
-	terminal    bool
-	volumes     []string
-	workingDir  string
-	*buildahcli.NameSpaceResults
+type RunCommandOpts struct {
+	CommonOpts
+	BuildArgs []string
 }
 
-func NewRunInputOptions() *runInputOptions {
-	return &runInputOptions{
-		NameSpaceResults: &buildahcli.NameSpaceResults{},
+type Buildah interface {
+	BuildFromDockerfile(ctx context.Context, dockerfile []byte, opts BuildFromDockerfileOpts) (string, error)
+	RunCommand(ctx context.Context, container string, command []string, opts RunCommandOpts) error
+}
+
+type BuildahMode string
+
+var (
+	NativeRootless BuildahMode = "NativeRootless"
+	DockerWithFuse BuildahMode = "DockerWithFuse"
+)
+
+type NewBuildahOpts struct {
+	Mode BuildahMode
+}
+
+func NewBuildah(mode BuildahMode) (Buildah, error) {
+	switch mode {
+	case "":
+		// TODO: auto select based on OS
+	case NativeRootless:
+		// TODO: validate selected mode with OS
+	case DockerWithFuse:
+		// TODO: validate selected mode with OS
+	}
+
+	switch mode {
+	case NativeRootless:
+		return NewNativeRootlessBuildah()
+	case DockerWithFuse:
+		return NewDockerWithFuseBuildah()
+	default:
+		panic("unexpected")
 	}
 }
 
-func Run(ctx context.Context, container string, command []string, opts *runInputOptions) error {
-	options := buildah.RunOptions{
-		Hostname: opts.hostname,
-		Runtime:  opts.runtime,
-		// Args:             runtimeFlags,
-		// NoPivot:          noPivot,
-		// User:             c.Flag("user").Value.String(),
-		// Isolation:        isolation,
-		// NamespaceOptions: namespaceOptions,
-		// ConfigureNetwork: networkPolicy,
-		CNIPluginPath:    opts.CNIPlugInPath,
-		CNIConfigDir:     opts.CNIConfigDir,
-		AddCapabilities:  opts.capAdd,
-		DropCapabilities: opts.capDrop,
-		Env:              opts.env,
-		WorkingDir:       opts.workingDir,
-	}
-
-	//fmt.Printf("HELO\n")
-	b, err := buildah.OpenBuilder(store, container)
-	//fmt.Printf("OpenBuilder -- %#v\n", b)
-	if os.IsNotExist(errors.Cause(err)) {
-		b, err = buildah.ImportBuilder(ctx, store, buildah.ImportOptions{
-			Container: container,
-		})
-		if err != nil {
-			return fmt.Errorf("unable to import builder for container %q: %s", container, err)
+func InitProcess(mode BuildahMode) error {
+	switch mode {
+	case NativeRootless:
+		if reexec.Init() {
+			return nil
 		}
-	} else if err != nil {
-		return fmt.Errorf("unable to open builder for container %q: %s", container, err)
-	}
 
-	return b.Run(command, options)
+		unshare.MaybeReexecUsingUserNamespace(false)
+
+		return nil
+	case DockerWithFuse:
+		return nil
+	default:
+		panic("unexpected")
+	}
 }
