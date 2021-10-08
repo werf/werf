467a6751e851d1bb240f7db7b1d0f24faf0ab96a
Ilya Lesikov <ilya@lesikov.com>
2021-10-08 14:04:01 +0300

    wip

--- scripts/build_release_v3.sh
+++ scripts/build_release_v3.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -xeuo pipefail
 
 VERSION="${1:?ERROR: Version should be specified as the first argument.}"
 
@@ -19,18 +19,18 @@ GOOS=linux GOARCH=arm64 CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc \
 
 GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 \
   go build -o "release-build/$VERSION/darwin-amd64/bin/werf" \
-  -ldflags="-s -w -X github.com/werf/werf/pkg/werf.Version=$VERSION"
+  -ldflags="-s -w -X github.com/werf/werf/pkg/werf.Version=$VERSION" \
   -tags="dfrunmount dfssh containers_image_openpgp" \
   github.com/werf/werf/cmd/werf
 
 GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 \
   go build -o "release-build/$VERSION/darwin-arm64/bin/werf" \
-  -ldflags="-s -w -X github.com/werf/werf/pkg/werf.Version=$VERSION"
+  -ldflags="-s -w -X github.com/werf/werf/pkg/werf.Version=$VERSION" \
   -tags="dfrunmount dfssh containers_image_openpgp" \
   github.com/werf/werf/cmd/werf
 
 GOOS=windows GOARCH=amd64 CGO_ENABLED=0 \
   go build -o "release-build/$VERSION/windows-amd64/bin/werf.exe" \
-  -ldflags="-s -w -X github.com/werf/werf/pkg/werf.Version=$VERSION"
+  -ldflags="-s -w -X github.com/werf/werf/pkg/werf.Version=$VERSION" \
   -tags="dfrunmount dfssh containers_image_openpgp" \
   github.com/werf/werf/cmd/werf
