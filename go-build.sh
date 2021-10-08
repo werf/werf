cb9b9153bd9dcda5527bb9d11b2e293012c4c7fa
Ilya Lesikov <ilya@lesikov.com>
2021-10-08 12:42:23 +0300

    wip

--- go-build.sh
+++ go-build.sh
@@ -9,7 +9,11 @@ cd $SOURCE
 
 export GO111MODULE=on
 export CGO_ENABLED=1
-go install -compiler gc -ldflags="-linkmode external -extldflags=-static" -tags="dfrunmount dfssh osusergo exclude_graphdriver_devicemapper netgo no_devmapper static_build" github.com/werf/werf/cmd/werf
-go install -compiler gc -ldflags="-linkmode external -extldflags=-static" -tags="dfrunmount dfssh osusergo exclude_graphdriver_devicemapper netgo no_devmapper static_build" github.com/werf/werf/cmd/buildah-test
+# GOOS=linux GOARCH=amd64 go install -compiler gc -ldflags="-linkmode external -extldflags=-static" -tags="dfrunmount dfssh osusergo exclude_graphdriver_devicemapper netgo no_devmapper static_build" github.com/werf/werf/cmd/werf
+# GOOS=linux GOARCH=arm64 go install -compiler gc -ldflags="-linkmode external -extldflags=-static" -tags="dfrunmount dfssh osusergo exclude_graphdriver_devicemapper netgo no_devmapper static_build" github.com/werf/werf/cmd/werf
+GOOS=darwin GOARCH=amd64 go install -compiler gc -ldflags="-linkmode external -extldflags=-static" -tags="dfrunmount dfssh osusergo exclude_graphdriver_devicemapper netgo no_devmapper static_build" github.com/werf/werf/cmd/werf
+# GOOS=darwin GOARCH=arm64 go install -compiler gc -ldflags="-linkmode external -extldflags=-static" -tags="dfrunmount dfssh osusergo exclude_graphdriver_devicemapper netgo no_devmapper static_build" github.com/werf/werf/cmd/werf
+# GOOS=windows GOARCH=amd64 go install -compiler gc -ldflags="-linkmode external -extldflags=-static" -tags="dfrunmount dfssh osusergo exclude_graphdriver_devicemapper netgo no_devmapper static_build" github.com/werf/werf/cmd/werf
+# go install -compiler gc -ldflags="-linkmode external -extldflags=-static" -tags="dfrunmount dfssh osusergo exclude_graphdriver_devicemapper netgo no_devmapper static_build" github.com/werf/werf/cmd/buildah-test
 
 cd $CWD
--- scripts/build_release_v3.sh
+++ scripts/build_release_v3.sh
@@ -10,10 +10,34 @@ if [ -z "$VERSION" ] ; then
     exit 1
 fi
 
-export CGO_ENABLED=1
+export GO111MODULE="on"
 
-gox -cgo -osarch="linux/amd64 linux/arm64 darwin/amd64 darwin/arm64 windows/amd64" \
-    -output="release-build/$VERSION/{{.OS}}-{{.Arch}}/bin/werf" \
-    -tags="dfrunmount dfssh osusergo exclude_graphdriver_devicemapper netgo no_devmapper static_build" \
-    -ldflags="-linkmode external -extldflags=-static -s -w -X github.com/werf/werf/pkg/werf.Version=$VERSION" \
-        github.com/werf/werf/cmd/werf
+GOOS=linux GOARCH=amd64 CGO_ENABLED=1 \
+  go build -compiler gc -o "release-build/$VERSION/linux-amd64/bin/werf" \
+  -ldflags="-s -w -X github.com/werf/werf/pkg/werf.Version=$VERSION -linkmode external -extldflags=-static" \
+  -tags="dfrunmount dfssh containers_image_openpgp osusergo exclude_graphdriver_devicemapper netgo no_devmapper static_build" \
+  github.com/werf/werf/cmd/werf
+
+GOOS=linux GOARCH=arm64 CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc \
+  go build -compiler gc -o "release-build/$VERSION/linux-arm64/bin/werf" \
+  -ldflags="-s -w -X github.com/werf/werf/pkg/werf.Version=$VERSION -linkmode external -extldflags=-static" \
+  -tags="dfrunmount dfssh containers_image_openpgp osusergo exclude_graphdriver_devicemapper netgo no_devmapper static_build" \
+  github.com/werf/werf/cmd/werf
+
+GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 \
+  go build -o "release-build/$VERSION/darwin-amd64/bin/werf" \
+  -ldflags="-s -w -X github.com/werf/werf/pkg/werf.Version=$VERSION"
+  -tags="dfrunmount dfssh containers_image_openpgp" \
+  github.com/werf/werf/cmd/werf
+
+GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 \
+  go build -o "release-build/$VERSION/darwin-arm64/bin/werf" \
+  -ldflags="-s -w -X github.com/werf/werf/pkg/werf.Version=$VERSION"
+  -tags="dfrunmount dfssh containers_image_openpgp" \
+  github.com/werf/werf/cmd/werf
+
+GOOS=windows GOARCH=amd64 CGO_ENABLED=0 \
+  go build -o "release-build/$VERSION/windows-amd64/bin/werf.exe" \
+  -ldflags="-s -w -X github.com/werf/werf/pkg/werf.Version=$VERSION"
+  -tags="dfrunmount dfssh containers_image_openpgp" \
+  github.com/werf/werf/cmd/werf
