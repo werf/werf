fbf48777b80dc5899851e6bf950f2bf951bfe06d
Ilya Lesikov <ilya@lesikov.com>
2021-10-08 15:30:34 +0300

    feat(build): enable static CGO builds for linux

--- go-build.sh
+++ go-build.sh
@@ -1,15 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-set -e
-
-CWD=`pwd`
-SOURCE=`dirname ${BASH_SOURCE[0]}`
-
-cd $SOURCE
+script_dir="$(cd "$(dirname "$0")"; pwd)"
 
 export GO111MODULE=on
-export CGO_ENABLED=1
-go install -compiler gc -ldflags="-linkmode external -extldflags=-static" -tags="dfrunmount dfssh osusergo exclude_graphdriver_devicemapper netgo no_devmapper static_build" github.com/werf/werf/cmd/werf
-go install -compiler gc -ldflags="-linkmode external -extldflags=-static" -tags="dfrunmount dfssh osusergo exclude_graphdriver_devicemapper netgo no_devmapper static_build" github.com/werf/werf/cmd/buildah-test
 
-cd $CWD
+cd "$script_dir"
+if [[ "$(uname)" == "Linux" ]]; then
+  CGO_ENABLED=1 go install -compiler gc \
+    -ldflags="-linkmode external -extldflags=-static" \
+    -tags="dfrunmount dfssh containers_image_openpgp osusergo exclude_graphdriver_devicemapper netgo no_devmapper static_build" \
+    github.com/werf/werf/cmd/werf
+elif [[ "$(uname)" == "Darwin" ]]; then
+  CGO_ENABLED=0 go install \
+    -tags="dfrunmount dfssh containers_image_openpgp" \
+    github.com/werf/werf/cmd/werf
+else
+  CGO_ENABLED=0 go install \
+    -tags="dfrunmount dfssh containers_image_openpgp" \
+    github.com/werf/werf/cmd/werf
+fi
+cd -
