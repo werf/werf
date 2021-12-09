aae77551bb8d1bfcf1b0e4d711a7c063247d4901
Ilya Lesikov <ilya@lesikov.com>
2021-12-09 18:06:13 +0300

    chore(cleanup): buildah dockerfiles are in another branch

--- scripts/werf-in-docker/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-BUILD_VERSION="$(git rev-parse HEAD)"
-IMAGE_NAME=ghcr.io/werf/werf:$BUILD_VERSION
-docker build -f scripts/werf-in-docker/Dockerfile --build-arg build_version=$BUILD_VERSION -t $IMAGE_NAME .
--- scripts/werf-in-docker/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-IMAGE_NAME=ghcr.io/werf/werf:"$(git rev-parse HEAD)"
-docker push $IMAGE_NAME
