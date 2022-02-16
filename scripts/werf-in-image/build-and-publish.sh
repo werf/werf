8d7d868c06823e29f7e2adc22777f8ec8277ebe7
Ilya Lesikov <ilya@lesikov.com>
2022-02-16 20:48:36 +0300

    feat(bundle): add werf argocd cmp sidecar image
    * added werf-argocd-cmp-sidecar ubuntu image
    * refactored build-and-publish.sh and werf build instructions
    
    Signed-off-by: Ilya Lesikov <ilya@lesikov.com>

--- scripts/werf-in-image/build-and-publish.sh
+++ scripts/werf-in-image/build-and-publish.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
+set -euo pipefail
 
-REPO="ghcr.io/werf/werf"
+BASE_REPO="ghcr.io/werf"
 
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd $SCRIPT_DIR
+script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $script_dir
 
-werf export --repo "${REPO}" --tag "${REPO}:%image%"
+export WERF_REPO="$BASE_REPO/werf"
+
+werf build
+
+werf export --tag "$BASE_REPO/werf:latest" "1.2-alpha-alpine"
+werf export --tag "$BASE_REPO/werf-argocd-cmp-sidecar:latest" "argocd-cmp-sidecar-1.2-alpha-ubuntu"
+
+for group in "1.2"; do
+  werf export --tag "$BASE_REPO/werf:$group" "$group-stable-alpine"
+  werf export --tag "$BASE_REPO/werf-argocd-cmp-sidecar:$group" "argocd-cmp-sidecar-$group-stable-ubuntu"
+
+  for channel in "alpha" "beta" "ea" "stable"; do
+    werf export --tag "$BASE_REPO/werf:$group-$channel" "$group-$channel-alpine"
+    werf export --tag "$BASE_REPO/werf-argocd-cmp-sidecar:$group-$channel" "argocd-cmp-sidecar-$group-$channel-ubuntu"
+
+    for distro in "alpine" "ubuntu" "centos"; do
+      werf export --tag "$BASE_REPO/werf:$group-$channel-$distro" "$group-$channel-$distro"
+    done
+
+    for distro in "ubuntu"; do
+      werf export --tag "$BASE_REPO/werf-argocd-cmp-sidecar:$group-$channel-$distro" "argocd-cmp-sidecar-$group-$channel-$distro"
+    done
+  done
+done
