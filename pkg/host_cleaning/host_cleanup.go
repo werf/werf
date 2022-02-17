64c10dcca2cb9e422acdf0c02ddd14024f12e673
Timofey Kirillov <timofey.kirillov@flant.com>
2022-02-17 20:41:51 +0300

    fix(host-cleanup): host cleanup not working without --docker-server-storage-path option Signed-off-by: Timofey Kirillov <timofey.kirillov@flant.com>
    Signed-off-by: Timofey Kirillov <timofey.kirillov@flant.com>

--- pkg/host_cleaning/host_cleanup.go
+++ pkg/host_cleaning/host_cleanup.go
@@ -80,7 +80,7 @@ func RunAutoHostCleanup(ctx context.Context, options AutoHostCleanupOptions) err
 	if options.AllowedLocalCacheVolumeUsageMarginPercentage != nil {
 		args = append(args, "--allowed-docker-storage-volume-usage-margin", fmt.Sprintf("%d", *options.AllowedLocalCacheVolumeUsageMarginPercentage))
 	}
-	if options.DockerServerStoragePath != nil {
+	if options.DockerServerStoragePath != nil && *options.DockerServerStoragePath != "" {
 		args = append(args, "--docker-server-storage-path", *options.DockerServerStoragePath)
 	}
 
