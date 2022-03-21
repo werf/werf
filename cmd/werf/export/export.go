007f44276186150c1a2b91644344d5974905d046
Alexey Igrychev <alexey.igrychev@flant.com>
2022-03-21 14:03:04 +0000

    refactor(dev): skip publication of metadata images in dev mode
    Signed-off-by: Alexey Igrychev <alexey.igrychev@flant.com>

--- cmd/werf/export/export.go
+++ cmd/werf/export/export.go
@@ -222,7 +222,7 @@ func run(ctx context.Context, imagesToProcess, tagTemplateList []string) error {
 
 		return c.Export(ctx, build.ExportOptions{
 			BuildPhaseOptions: build.BuildPhaseOptions{
-				BuildOptions:      build.BuildOptions{},
+				BuildOptions:      build.BuildOptions{SkipImageMetadataPublication: *commonCmdData.Dev},
 				ShouldBeBuiltMode: *commonCmdData.SkipBuild,
 			},
 			ExportPhaseOptions: build.ExportPhaseOptions{
