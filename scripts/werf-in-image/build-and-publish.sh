ffae3501d45dc2a12774c9e39c6289acfc53f7b0
Alexey Igrychev <alexey.igrychev@flant.com>
2022-03-25 15:34:38 +0000

    ci: store images in werf registry
    Signed-off-by: Alexey Igrychev <alexey.igrychev@flant.com>

--- scripts/werf-in-image/build-and-publish.sh
+++ scripts/werf-in-image/build-and-publish.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 set -euo pipefail
 
-BASE_REPO="ghcr.io/werf"
-
 script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $script_dir
 
-export WERF_REPO="$BASE_REPO/werf"
-
+export WERF_REPO=registry-write.werf.io/werf/werf-storage
 werf build
 
-werf export --tag "$BASE_REPO/werf:latest" "1.2-stable-alpine"
-werf export --tag "$BASE_REPO/werf-argocd-cmp-sidecar:latest" "argocd-cmp-sidecar-1.2-stable-ubuntu"
+for dest_repo in "ghcr.io/werf" "registry-write.werf.io/werf"; do
+  DEST_REPO=$dest_repo
 
-for group in "1.2"; do
-  werf export --tag "$BASE_REPO/werf:$group" "$group-stable-alpine"
-  werf export --tag "$BASE_REPO/werf-argocd-cmp-sidecar:$group" "argocd-cmp-sidecar-$group-stable-ubuntu"
+  werf export --tag "$DEST_REPO/werf:latest" "1.2-stable-alpine"
+  werf export --tag "$DEST_REPO/werf-argocd-cmp-sidecar:latest" "argocd-cmp-sidecar-1.2-stable-ubuntu"
 
-  for channel in "alpha" "beta" "ea" "stable"; do
-    werf export --tag "$BASE_REPO/werf:$group-$channel" "$group-$channel-alpine"
-    werf export --tag "$BASE_REPO/werf-argocd-cmp-sidecar:$group-$channel" "argocd-cmp-sidecar-$group-$channel-ubuntu"
+  for group in "1.2"; do
+    werf export --tag "$DEST_REPO/werf:$group" "$group-stable-alpine"
+    werf export --tag "$DEST_REPO/werf-argocd-cmp-sidecar:$group" "argocd-cmp-sidecar-$group-stable-ubuntu"
 
-    for distro in "alpine" "ubuntu" "centos"; do
-      werf export --tag "$BASE_REPO/werf:$group-$channel-$distro" "$group-$channel-$distro"
-    done
+    for channel in "alpha" "beta" "ea" "stable"; do
+      werf export --tag "$DEST_REPO/werf:$group-$channel" "$group-$channel-alpine"
+      werf export --tag "$DEST_REPO/werf-argocd-cmp-sidecar:$group-$channel" "argocd-cmp-sidecar-$group-$channel-ubuntu"
 
-    for distro in "ubuntu"; do
-      werf export --tag "$BASE_REPO/werf-argocd-cmp-sidecar:$group-$channel-$distro" "argocd-cmp-sidecar-$group-$channel-$distro"
+      for distro in "alpine" "ubuntu" "centos"; do
+        werf export --tag "$DEST_REPO/werf:$group-$channel-$distro" "$group-$channel-$distro"
+      done
+
+      for distro in "ubuntu"; do
+        werf export --tag "$DEST_REPO/werf-argocd-cmp-sidecar:$group-$channel-$distro" "argocd-cmp-sidecar-$group-$channel-$distro"
+      done
     done
   done
 done
