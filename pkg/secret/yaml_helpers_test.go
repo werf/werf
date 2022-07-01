289400d3aec3794dc310942fb81d0b3423843753
Timofey Kirillov <timofey.kirillov@flant.com>
2022-07-01 19:22:42 +0300

    fix(secrets): panic and incorrect behaviour during secrets edit
    Problem reproduced when adding, removing or changin order of map keys. Or when removing and changing order of array elements.
    
    Signed-off-by: Timofey Kirillov <timofey.kirillov@flant.com>

--- pkg/secret/yaml_helpers_test.go
+++ pkg/secret/yaml_helpers_test.go
@@ -201,5 +201,282 @@ values:
   key5: *common-list
 `),
 		}),
+
+		Entry("added elements into map and array", MergeEncodedYamlTest{
+			OldData: []byte(`
+database:
+  user: vasya
+  password: gfhjkm
+hosts:
+  - name: local1
+    address: a1
+  - name: local2
+    address: a2
+`),
+			OldEncodedData: []byte(`
+database:
+  user: enc1
+  password: enc2
+hosts:
+  - name: enc3
+    address: enc4
+  - name: enc5
+    address: enc6
+`),
+			NewData: []byte(`
+database:
+  user: vasya
+  password: gfhjkm
+  admin: petya
+  adminPassword:
+    _default: "1234"
+    production: "#@$213sldfzjxXFLKJSdf1233s"
+hosts:
+  - name: local1
+    address: a1
+    options:
+      timeout: 5s
+  - name: local2
+    address: a2
+`),
+			NewEncodedData: []byte(`
+database:
+  user: enc1-1
+  password: enc2-1
+  admin: enc3-1
+  adminPassword:
+    _default: enc4-1
+    production: enc5-1
+hosts:
+  - name: enc6-1
+    address: enc7-1
+    options:
+      timeout: enc8-1
+  - name: enc9-1
+    address: enc10-1
+`),
+			ExpectedResult: []byte(`
+database:
+  user: enc1
+  password: enc2
+  admin: enc3-1
+  adminPassword:
+    _default: enc4-1
+    production: enc5-1
+hosts:
+  - name: enc3
+    address: enc4
+    options:
+      timeout: enc8-1
+  - name: enc5
+    address: enc6
+`),
+		}),
+
+		Entry("removed elements from map and array", MergeEncodedYamlTest{
+			OldData: []byte(`
+database:
+  user: vasya
+  password: gfhjkm
+  admin: petya
+  adminPassword:
+    _default: "1234"
+    production: "#@$213sldfzjxXFLKJSdf1233s"
+hosts:
+  - name: local1
+    address: a1
+    options:
+      timeout: 5s
+  - name: local2
+    address: a2
+`),
+			OldEncodedData: []byte(`
+database:
+  user: enc1
+  password: enc2
+  admin: enc3
+  adminPassword:
+    _default: enc4
+    production: enc5
+hosts:
+  - name: enc6
+    address: enc7
+    options:
+      timeout: enc8
+  - name: enc9
+    address: enc10
+`),
+			NewData: []byte(`
+database:
+  user: vasya
+  adminPassword:
+    _default: "1234"
+    production: "#@$213sldfzjxXFLKJSdf1233s"
+hosts:
+  - name: local2
+    address: a2
+`),
+			NewEncodedData: []byte(`
+database:
+  user: enc1-1
+  adminPassword:
+    _default: enc2-1
+    production: enc3-1
+hosts:
+  - name: enc4-1
+    address: enc5-1
+`),
+			ExpectedResult: []byte(`
+database:
+  user: enc1
+  adminPassword:
+    _default: enc4
+    production: enc5
+hosts:
+  - name: enc4-1
+    address: enc5-1
+`),
+		}),
+
+		Entry("no changes in array", MergeEncodedYamlTest{
+			OldData: []byte(`
+arr:
+  - k1: v1
+    k2: v2
+  - k1: v3
+    k2: v4
+  - k1: v5
+    k2: v6
+`),
+			OldEncodedData: []byte(`
+arr:
+  - k1: enc1
+    k2: enc2
+  - k1: enc3
+    k2: enc4
+  - k1: enc5
+    k2: enc6
+`),
+			NewData: []byte(`
+arr:
+  - k1: v1
+    k2: v2
+  - k1: v3
+    k2: v4
+  - k1: v5
+    k2: v6
+`),
+			NewEncodedData: []byte(`
+arr:
+  - k1: enc1-1
+    k2: enc2-1
+  - k1: enc3-1
+    k2: enc4-1
+  - k1: enc5-1
+    k2: enc6-1
+`),
+			ExpectedResult: []byte(`
+arr:
+  - k1: enc1
+    k2: enc2
+  - k1: enc3
+    k2: enc4
+  - k1: enc5
+    k2: enc6
+`),
+		}),
+
+		Entry("random crazy mindless changes: change order of array elements, swap map keys with different values, change order of map values", MergeEncodedYamlTest{
+			OldData: []byte(`
+database:
+  user: vasya
+  password: gfhjkm
+  admin: petya
+  adminPassword:
+    _default: "1234"
+    production: "#@$213sldfzjxXFLKJSdf1233s"
+hosts:
+  - name: local0
+    address: a0
+  - name: local1
+    address: a1
+    options:
+      timeout: 5s
+  - name: local2
+    address: a2
+`),
+			OldEncodedData: []byte(`
+database:
+  user: enc1
+  password: enc2
+  admin: enc3
+  adminPassword:
+    _default: enc4
+    production: enc5
+hosts:
+  - name: enc6
+    address: enc7
+  - name: enc8
+    address: enc9
+    options:
+      timeout: enc10
+  - name: enc11
+    address: enc12
+`),
+			NewData: []byte(`
+database:
+  password: gfhjkm
+  user: vasya
+  adminPassword: petya
+  admin:
+    _default: "1234"
+    production: "#@$213sldfzjxXFLKJSdf1233s"
+hosts:
+  - name: local0
+    address: new-addr
+  - name: local2
+    address: a2
+  - name: local1
+    address: a1
+    options:
+      timeout: 5s
+`),
+			NewEncodedData: []byte(`
+database:
+  password: enc1-1
+  user: enc2-1
+  adminPassword: enc3-1
+  admin:
+    _default: enc4-1
+    production: enc5-1
+hosts:
+  - name: enc6-1
+    address: enc7-1
+  - name: enc8-1
+    address: enc9-1
+  - name: enc10-1
+    address: enc11-1
+    options:
+      timeout: enc12-1
+`),
+			ExpectedResult: []byte(`
+database:
+  password: enc2
+  user: enc1
+  adminPassword: enc3-1
+  admin:
+    _default: enc4-1
+    production: enc5-1
+hosts:
+  - name: enc6
+    address: enc7-1
+  - name: enc8-1
+    address: enc9-1
+  - name: enc10-1
+    address: enc11-1
+    options:
+      timeout: enc12-1
+`),
+		}),
 	)
 })
