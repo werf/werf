892f94d45485c0d23ab848deaf9c113c9cdaa578
Alexey Igrychev <alexey.igrychev@flant.com>
2022-08-14 23:07:33 +0100

    chore(telemetry): add userId field
    Signed-off-by: Alexey Igrychev <alexey.igrychev@flant.com>

--- pkg/telemetry/no_telemetrywerfio.go
+++ pkg/telemetry/no_telemetrywerfio.go
@@ -5,6 +5,7 @@ import "context"
 type NoTelemetryWerfIO struct{}
 
 func (t *NoTelemetryWerfIO) CommandStarted(context.Context)                     {}
+func (t *NoTelemetryWerfIO) SetUserID(context.Context, string)                  {}
 func (t *NoTelemetryWerfIO) SetProjectID(context.Context, string)               {}
 func (t *NoTelemetryWerfIO) SetCommand(context.Context, string)                 {}
 func (t *NoTelemetryWerfIO) CommandExited(context.Context, int)                 {}
