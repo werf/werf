b47c49d982409393634980680359e8a3ac28844f
Zhbert <zhbert@yandex.ru>
2022-10-14 15:26:27 +0300

    Fix helm version command

--- cmd/werf/docs/replacers/helm/helm_docs.go
+++ cmd/werf/docs/replacers/helm/helm_docs.go
@@ -251,3 +251,27 @@ func GetHelmVerifyDocs() structs.DocsStruct {
 
 	return docs
 }
+
+func GetHelmVersionDocs() structs.DocsStruct {
+	var docs structs.DocsStruct
+
+	docs.LongMD = "Show the version for Helm.\n\n" +
+		"This will print a representation the version of Helm. " +
+		"The output will look something like this:\n" +
+		"```\nversion.BuildInfo{Version:\"v3.2.1\", GitCommit:\"fe51cd1e31e6a202cba7dead9552a6d418ded79a\", " +
+		"GitTreeState:\"clean\", GoVersion:\"go1.13.10\"}\n```\n" +
+		"- `Version` is the semantic version of the release;\n" +
+		"- `GitCommit` is the SHA for the commit that this version was built from;\n" +
+		"- `GitTreeState` is `clean` if there are no local code changes when this binary was" +
+		"  built, and `dirty` if the binary was built from locally modified code;\n" +
+		"- `GoVersion` is the version of Go that was used to compile Helm.\n\n" +
+		"When using the `--template` flag the following properties are available to use in " +
+		"the template:\n" +
+		"- `.Version` contains the semantic version of Helm;\n" +
+		"- `.GitCommit` is the git commit;\n" +
+		"- `.GitTreeState` is the state of the git tree when Helm was built;\n" +
+		"- `.GoVersion` contains the version of Go that Helm was compiled with.\n\n" +
+		"For example, `--template='Version: {{.Version}}'` outputs `'Version: v3.2.1'`."
+
+	return docs
+}
