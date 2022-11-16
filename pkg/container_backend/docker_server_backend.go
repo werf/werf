31df88c6125c1725712562637215f5bec275b765
Timofey Kirillov <timofey.kirillov@flant.com>
2022-11-16 18:24:28 +0300

    fix(build): inconsistent report path when final-repo used
    - Fix image descriptors copying: deep copy image.StageDescription and image.Info.
    - Set final image info into report when final-repo specified.
    - Fix inconsistent image rename in container backend implementations.
    
    Signed-off-by: Timofey Kirillov <timofey.kirillov@flant.com>

--- pkg/container_backend/docker_server_backend.go
+++ pkg/container_backend/docker_server_backend.go
@@ -148,10 +148,9 @@ func (runtime *DockerServerBackend) RenameImage(ctx context.Context, img LegacyI
 		img.SetInfo(info)
 	}
 
-	desc := img.GetStageDescription()
-
-	if desc != nil {
+	if desc := img.GetStageDescription(); desc != nil {
 		repository, tag := image.ParseRepositoryAndTag(newImageName)
+		desc.Info.Name = newImageName
 		desc.Info.Repository = repository
 		desc.Info.Tag = tag
 	}
