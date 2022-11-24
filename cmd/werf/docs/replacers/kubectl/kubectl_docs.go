24f7ec37ad9dd22c09a2529914eafb556193142e
Zhbert <zhbert@yandex.ru>
2022-11-24 20:43:24 +0300

    Fix docs on edit command

--- cmd/werf/docs/replacers/kubectl/kubectl_docs.go
+++ cmd/werf/docs/replacers/kubectl/kubectl_docs.go
@@ -762,3 +762,27 @@ func GetDrainDocs() structs.DocsStruct {
 
 	return docs
 }
+
+func GetEditDocs() structs.DocsStruct {
+	var docs structs.DocsStruct
+
+	docs.LongMD = "Edit a resource from the default editor.\n\n" +
+		"The edit command allows you to directly edit any API resource you can retrieve via the " +
+		"command-line tools. It will open the editor defined by your `KUBE_EDITOR`, or `EDITOR` " +
+		"environment variables, or fall back to `vi` for Linux or `notepad` for Windows. " +
+		"You can edit multiple objects, although changes are applied one at a time. The command " +
+		"accepts file names as well as command-line arguments, although the files you point to must " +
+		"be previously saved versions of resources.\n\n" +
+		"Editing is done with the API version used to fetch the resource. To edit using a specific " +
+		"API version, fully-qualify the resource, version, and group.\n\n" +
+		"The default format is YAML. To edit in JSON, specify `-o json`.\n\n" +
+		"The flag `--windows-line-endings` can be used to force Windows line endings, otherwise the " +
+		"default for your operating system will be used.\n\n" +
+		"In the event an error occurs while updating, a temporary file will be created on disk " +
+		"that contains your unapplied changes. The most common error when updating a resource " +
+		"is another editor changing the resource on the server. When this occurs, you will have " +
+		"to apply your changes to the newer version of the resource, or update your temporary " +
+		"saved copy to include the latest resource version."
+
+	return docs
+}
