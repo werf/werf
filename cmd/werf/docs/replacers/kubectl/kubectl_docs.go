0d6082405a7fde837f941b625735cf94b8ba6b2b
Zhbert <zhbert@yandex.ru>
2022-11-24 22:14:10 +0300

    Fix docs on get command
    Signed-off-by: Alexey Igrychev <alexey.igrychev@flant.com>

--- cmd/werf/docs/replacers/kubectl/kubectl_docs.go
+++ cmd/werf/docs/replacers/kubectl/kubectl_docs.go
@@ -830,3 +830,18 @@ func GetExposeDocs() structs.DocsStruct {
 
 	return docs
 }
+
+func GetGetDocs() structs.DocsStruct {
+	var docs structs.DocsStruct
+
+	docs.LongMD = "Display one or many resources.\n\n" +
+		"Prints a table of the most important information about the specified resources. " +
+		"You can filter the list using a label selector and the `--selector` flag. If the " +
+		"desired resource type is namespaced you will only see results in your current " +
+		"namespace unless you pass `--all-namespaces`.\n\n" +
+		"By specifying the output as `template` and providing a Go template as the value " +
+		"of the `--template` flag, you can filter the attributes of the fetched resources.\n\n" +
+		"Use `kubectl api-resources` for a complete list of supported resources."
+
+	return docs
+}
