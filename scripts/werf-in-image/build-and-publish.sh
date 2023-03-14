5ba7045568744c50be58b60087cca949cff2efd6
Aleksei Igrychev <aleksei.igrychev@palark.com>
2023-03-14 13:24:57 +0000

    ci(distribution): fix failed workflow
    Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>

--- scripts/werf-in-image/build-and-publish.sh
+++ scripts/werf-in-image/build-and-publish.sh
@@ -5,11 +5,11 @@ script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $script_dir
 
 if [[ -z "$1" ]]; then
-  echo "script requires argument <destination repo>" >&2
+  echo "script requires argument <destination registry>" >&2
   exit 1
 fi
 
-DEST_REPO=$1
+DEST_SUBREPO=$1/werf
 
 export WERF_REPO=ghcr.io/werf/werf-storage
 
@@ -22,27 +22,27 @@ export WERF_EXPORT_ADD_LABEL_1=io.artifacthub.package.readme-url=https://raw.git
        WERF_EXPORT_ADD_LABEL_6=org.opencontainers.image.created=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
        WERF_EXPORT_ADD_LABEL_7=org.opencontainers.image.description="Official image to run werf in containers"
 
-werf export --tag "$DEST_REPO/werf:latest" "1.2-stable-alpine"
-werf export --tag "$DEST_REPO/werf-argocd-cmp-sidecar:latest" "argocd-cmp-sidecar-1.2-stable-ubuntu"
+werf export --tag "$DEST_SUBREPO/werf:latest" "1.2-stable-alpine"
+werf export --tag "$DEST_SUBREPO/werf-argocd-cmp-sidecar:latest" "argocd-cmp-sidecar-1.2-stable-ubuntu"
 
 for group in "1.2"; do
-  werf export --tag "$DEST_REPO/werf:$group" "$group-stable-alpine"
-  werf export --tag "$DEST_REPO/werf-argocd-cmp-sidecar:$group" "argocd-cmp-sidecar-$group-stable-ubuntu"
+  werf export --tag "$DEST_SUBREPO/werf:$group" "$group-stable-alpine"
+  werf export --tag "$DEST_SUBREPO/werf-argocd-cmp-sidecar:$group" "argocd-cmp-sidecar-$group-stable-ubuntu"
 
   for distro in "alpine" "ubuntu" "centos" "fedora"; do
-    werf export --tag "$DEST_REPO/werf:$group-$distro" "$group-stable-$distro"
+    werf export --tag "$DEST_SUBREPO/werf:$group-$distro" "$group-stable-$distro"
   done
 
   for channel in "alpha" "beta" "ea" "stable" "rock-solid"; do
-    werf export --tag "$DEST_REPO/werf:$group-$channel" "$group-$channel-alpine"
-    werf export --tag "$DEST_REPO/werf-argocd-cmp-sidecar:$group-$channel" "argocd-cmp-sidecar-$group-$channel-ubuntu"
+    werf export --tag "$DEST_SUBREPO/werf:$group-$channel" "$group-$channel-alpine"
+    werf export --tag "$DEST_SUBREPO/werf-argocd-cmp-sidecar:$group-$channel" "argocd-cmp-sidecar-$group-$channel-ubuntu"
 
     for distro in "alpine" "ubuntu" "centos" "fedora"; do
-      werf export --tag "$DEST_REPO/werf:$group-$channel-$distro" "$group-$channel-$distro"
+      werf export --tag "$DEST_SUBREPO/werf:$group-$channel-$distro" "$group-$channel-$distro"
     done
 
     for distro in "ubuntu"; do
-      werf export --tag "$DEST_REPO/werf-argocd-cmp-sidecar:$group-$channel-$distro" "argocd-cmp-sidecar-$group-$channel-$distro"
+      werf export --tag "$DEST_SUBREPO/werf-argocd-cmp-sidecar:$group-$channel-$distro" "argocd-cmp-sidecar-$group-$channel-$distro"
     done
   done
 done
