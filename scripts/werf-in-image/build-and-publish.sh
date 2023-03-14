7b438b32c390b301a2144d7c1610b9bf7f479e26
Aleksei Igrychev <aleksei.igrychev@palark.com>
2023-03-14 20:07:58 +0000

    ci(distribute): add artifacthub label
    Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>

--- scripts/werf-in-image/build-and-publish.sh
+++ scripts/werf-in-image/build-and-publish.sh
@@ -17,6 +17,7 @@ export WERF_REPO=ghcr.io/werf/werf-storage
 export WERF_EXPORT_ADD_LABEL_AH1=io.artifacthub.package.readme-url=https://raw.githubusercontent.com/werf/werf/main/README.md \
        WERF_EXPORT_ADD_LABEL_AH2=io.artifacthub.package.logo-url=https://raw.githubusercontent.com/werf/website/main/assets/images/werf-logo.svg \
        WERF_EXPORT_ADD_LABEL_AH3=io.artifacthub.package.category=integration-delivery \
+       WERF_EXPORT_ADD_LABEL_AH4=io.artifacthub.package.keywords="cli,ci,cd,build,test,deploy,distribute,cleanup" \
        WERF_EXPORT_ADD_LABEL_OC1=org.opencontainers.image.url=https://github.com/werf/werf/tree/main/scripts/werf-in-image \
        WERF_EXPORT_ADD_LABEL_OC2=org.opencontainers.image.source=https://github.com/werf/werf/tree/main/scripts/werf-in-image \
        WERF_EXPORT_ADD_LABEL_OC3=org.opencontainers.image.created=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
