b7044c1ea9976c95df6e4c3a87f39425ebd0e128
Aleksei Igrychev <aleksei.igrychev@palark.com>
2024-05-23 11:40:24 +0100

    feat(harbor): try to get the image again due to PROJECTPOLICYVIOLATION error
    Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>

--- pkg/docker_registry/harbor.go
+++ pkg/docker_registry/harbor.go
@@ -5,8 +5,13 @@ import (
 	"errors"
 	"net/http"
 	"strings"
+	"time"
 
+	"github.com/cenkalti/backoff/v4"
 	"github.com/google/go-containerregistry/pkg/name"
+
+	"github.com/werf/logboek"
+	"github.com/werf/werf/v2/pkg/image"
 )
 
 const (
@@ -14,7 +19,7 @@ const (
 	harborRepositoryNotFoundErrPrefix = "harbor repository not found: "
 )
 
-var harborPatterns = []string{"^harbor\\..*"}
+var harborPatterns = []string{"^harbor\\..*", "demo\\.goharbor\\.io"}
 
 type HarborRepositoryNotFoundErr apiError
 
@@ -57,6 +62,38 @@ func newHarbor(options harborOptions) (*harbor, error) {
 	}
 
 	return harbor, nil
+}
+
+func (r *harbor) GetRepoImage(ctx context.Context, reference string) (*image.Info, error) {
+	var info *image.Info
+	operation := func() error {
+		var err error
+		info, err = r.api.GetRepoImage(ctx, reference)
+		if err != nil {
+			if strings.Contains(err.Error(), "PROJECTPOLICYVIOLATION") {
+				return err
+			}
+
+			// Do not retry on other errors.
+			return backoff.Permanent(err)
+		}
+		return nil
+	}
+
+	notify := func(err error, duration time.Duration) {
+		logboek.Context(ctx).Warn().LogF("WARNING: %s. Retrying in %v...\n", err.Error(), duration)
+	}
+
+	eb := backoff.NewExponentialBackOff()
+	eb.InitialInterval = 2 * time.Second
+	eb.MaxElapsedTime = 2 * time.Minute // Maximum time for all retries.
+
+	err := backoff.RetryNotify(operation, eb, notify)
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
 }
 
 func (r *harbor) DeleteRepo(ctx context.Context, reference string) error {
