35ae8c082e47af0b66e66adc7945e89b53dd7b87
Yaroslav Pershin <62902094+iapershin@users.noreply.github.com>
2024-12-04 22:05:24 +0300

    format
    Signed-off-by: Yaroslav Pershin <62902094+iapershin@users.noreply.github.com>

--- test/e2e/export/complex_test.go
+++ test/e2e/export/complex_test.go
@@ -7,6 +7,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
 	"github.com/werf/werf/v2/test/pkg/utils"
 	"github.com/werf/werf/v2/test/pkg/utils/docker"
 	"github.com/werf/werf/v2/test/pkg/werf"
