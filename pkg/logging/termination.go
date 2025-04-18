cfa852a973ad60e534e069b48a75d7469274cbaa
Alexandr Zaytsev <alexandr.zaytsev@flant.com>
2025-04-18 19:03:35 +0300

    fix(build): print "Error: " prefix on termination error
    Signed-off-by: Alexandr Zaytsev <alexandr.zaytsev@flant.com>

--- pkg/logging/termination.go
+++ pkg/logging/termination.go
@@ -7,7 +7,7 @@ import (
 func Error(message string) {
 	logboek.Streams().DisablePrefix()
 	logboek.Streams().DisableLineWrapping()
-	logboek.Error().LogLn(message)
+	logboek.Error().LogF("Error: %s\n", message)
 }
 
 func Default(message string) {
