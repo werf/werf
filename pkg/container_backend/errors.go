81314a8189cb07a622f7ef3c030a1ff1b3c75b77
Yaroslav Pershin <62902094+iapershin@users.noreply.github.com>
2025-05-21 20:44:36 +0300

    fix(stapel): fix git apply patch error handling
    Signed-off-by: Yaroslav Pershin <62902094+iapershin@users.noreply.github.com>

--- pkg/container_backend/errors.go
+++ pkg/container_backend/errors.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/containers/storage/types"
+	"github.com/docker/cli/cli"
 )
 
 var (
@@ -13,3 +14,34 @@ var (
 	ErrImageUsedByContainer         = types.ErrImageUsedByContainer
 	ErrPruneIsAlreadyRunning        = errors.New("a prune operation is already running")
 )
+
+var (
+	ErrPatchApply = errors.New(`werf cannot apply the patch to the current source code because the files being added were modified by user commands in earlier stages.
+
+- If these files should NOT be changed, update the instructions for the preceding stages with user commands.
+
+- If these files SHOULD be changed, declare this dependency using the stageDependencies directive, and these files will be updated before running user commands.
+
+- If these files are NOT required, exclude them using the git.[*].includePaths / excludePaths directives.`)
+)
+
+const (
+	ErrPatchApplyCode = 42
+)
+
+var errByCode = map[int]error{
+	ErrPatchApplyCode: ErrPatchApply,
+}
+
+func CliErrorByCode(err error) error {
+	if err == nil {
+		return nil
+	}
+	var statusError cli.StatusError
+	if errors.As(err, &statusError) {
+		if e, ok := errByCode[statusError.StatusCode]; ok {
+			return e
+		}
+	}
+	return err
+}
