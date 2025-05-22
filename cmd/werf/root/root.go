8642cb38d5fe7f3c47cae9f46d8b2e67bea6c41e
Alexandr Zaytsev <alexandr.zaytsev@flant.com>
2025-05-22 15:23:19 +0300

    feat(sbom): hide experimental command
    Signed-off-by: Alexandr Zaytsev <alexandr.zaytsev@flant.com>

--- cmd/werf/root/root.go
+++ cmd/werf/root/root.go
@@ -9,8 +9,10 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 
+	"github.com/werf/common-go/pkg/util"
 	"github.com/werf/werf/v2/cmd/werf/build"
 	bundle_apply "github.com/werf/werf/v2/cmd/werf/bundle/apply"
 	bundle_copy "github.com/werf/werf/v2/cmd/werf/bundle/copy"
@@ -93,7 +95,7 @@ func ConstructRootCmd(ctx context.Context) (*cobra.Command, error) {
 		},
 		{
 			Message: "Helper commands",
-			Commands: []*cobra.Command{
+			Commands: hideExperimental([]*cobra.Command{
 				ci_env.NewCmd(ctx),
 				build.NewCmd(ctx),
 				export.NewExportCmd(ctx),
@@ -104,7 +106,7 @@ func ConstructRootCmd(ctx context.Context) (*cobra.Command, error) {
 				render.NewCmd(ctx),
 				includesCmd(ctx),
 				sbomCmd(ctx),
-			},
+			}),
 		},
 		{
 			Message: "Low-level management commands",
@@ -314,4 +316,15 @@ func PrintStackTraces() {
 			time.Sleep(time.Second * time.Duration(period))
 		}
 	}()
+}
+
+func hideExperimental(cmdList []*cobra.Command) []*cobra.Command {
+	return lo.Filter(cmdList, func(cmd *cobra.Command, _ int) bool {
+		switch cmd.Use {
+		case "sbom":
+			return util.GetBoolEnvironmentDefaultFalse("WERF_EXPERIMENTAL_SBOM")
+		default:
+			return true
+		}
+	})
 }
