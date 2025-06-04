90fb98511daef48144ccb40aee996a242160537a
Alexandr Zaytsev <alexandr.zaytsev@flant.com>
2025-06-04 10:00:31 +0300

    chore(dev): add signer verifier pkg
    Signed-off-by: Alexandr Zaytsev <alexandr.zaytsev@flant.com>

---
+++ pkg/signver/signver.go
@@ -0,0 +1,153 @@
+package signver
+
+import (
+	"bytes"
+	"context"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/google/certificate-transparency-go/x509util"
+	"github.com/sigstore/sigstore/pkg/cryptoutils"
+	"github.com/sigstore/sigstore/pkg/signature"
+)
+
+const (
+	SigstorePrivateKeyPemType = "ENCRYPTED SIGSTORE PRIVATE KEY"
+)
+
+type SignerVerifier struct {
+	Cert  []byte
+	Chain []byte
+	signature.SignerVerifier
+}
+
+func NewSignerVerifier(ctx context.Context, certPath, certChainPath string, ko KeyOpts) (*SignerVerifier, error) {
+	if ko.KeyRef == "" {
+		return nil, errors.New("ko.KeyRef must not be empty string")
+	}
+
+	k, err := signerVerifierFromKeyRef(ctx, ko.KeyRef, ko.PassFunc)
+	if err != nil {
+		return nil, fmt.Errorf("reading key: %w", err)
+	}
+
+	certSigner := &SignerVerifier{
+		SignerVerifier: k,
+	}
+
+	var leafCert *x509.Certificate
+
+	// NOTE: PKCS11 keys are unsupported
+
+	// Handle --cert flag
+	if certPath != "" {
+		// Allow both DER and PEM encoding
+		certBytes, err := os.ReadFile(certPath)
+		if err != nil {
+			return nil, fmt.Errorf("read certificate: %w", err)
+		}
+		// Handle PEM
+		if bytes.HasPrefix(certBytes, []byte("-----")) {
+			decoded, _ := pem.Decode(certBytes)
+			if decoded.Type != "CERTIFICATE" {
+				return nil, fmt.Errorf("supplied PEM file is not a certificate: %s", certPath)
+			}
+			certBytes = decoded.Bytes
+		}
+		parsedCert, err := x509.ParseCertificate(certBytes)
+		if err != nil {
+			return nil, fmt.Errorf("parse x509 certificate: %w", err)
+		}
+		pk, err := k.PublicKey()
+		if err != nil {
+			return nil, fmt.Errorf("get public key: %w", err)
+		}
+		if cryptoutils.EqualKeys(pk, parsedCert.PublicKey) != nil {
+			return nil, errors.New("public key in certificate does not match the provided public key")
+		}
+		pemBytes, err := cryptoutils.MarshalCertificateToPEM(parsedCert)
+		if err != nil {
+			return nil, fmt.Errorf("marshaling certificate to PEM: %w", err)
+		}
+		if certSigner.Cert != nil {
+			return nil, errors.New("overriding x509 certificate retrieved from the PKCS11 token")
+		}
+		leafCert = parsedCert
+		certSigner.Cert = pemBytes
+	}
+
+	if certChainPath == "" {
+		return certSigner, nil
+	} else if certSigner.Cert == nil {
+		return nil, errors.New("no leaf certificate found or provided while specifying chain")
+	}
+
+	// Handle --cert-chain flag
+	// Accept only PEM encoded certificate chain
+	certChainBytes, err := os.ReadFile(certChainPath)
+	if err != nil {
+		return nil, fmt.Errorf("reading certificate chain from path: %w", err)
+	}
+	certChain, err := cryptoutils.LoadCertificatesFromPEM(bytes.NewReader(certChainBytes))
+	if err != nil {
+		return nil, fmt.Errorf("loading certificate chain: %w", err)
+	}
+	if len(certChain) == 0 {
+		return nil, errors.New("no certificates in certificate chain")
+	}
+	// Verify certificate chain is valid
+	rootPool := x509.NewCertPool()
+	rootPool.AddCert(certChain[len(certChain)-1])
+	subPool := x509.NewCertPool()
+	for _, c := range certChain[:len(certChain)-1] {
+		subPool.AddCert(c)
+	}
+	if _, err := trustedCert(leafCert, rootPool, subPool); err != nil {
+		return nil, fmt.Errorf("unable to validate certificate chain: %w", err)
+	}
+	// Verify SCT if present in the leaf certificate.
+	contains, err := containsSCT(leafCert.Raw)
+	if err != nil {
+		return nil, err
+	}
+	if contains {
+		return nil, errors.New("verification of embedded SCT is unsupported")
+	}
+	certSigner.Chain = certChainBytes
+
+	return certSigner, nil
+}
+
+func trustedCert(cert *x509.Certificate, roots, intermediates *x509.CertPool) ([][]*x509.Certificate, error) {
+	chains, err := cert.Verify(x509.VerifyOptions{
+		// THIS IS IMPORTANT: WE DO NOT CHECK TIMES HERE
+		// THE CERTIFICATE IS TREATED AS TRUSTED FOREVER
+		// WE CHECK THAT THE SIGNATURES WERE CREATED DURING THIS WINDOW
+		CurrentTime:   cert.NotBefore,
+		Roots:         roots,
+		Intermediates: intermediates,
+		KeyUsages: []x509.ExtKeyUsage{
+			x509.ExtKeyUsageCodeSigning,
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("cert verification failed: %w. Check your TUF root (see cosign initialize) or set a custom root with env var SIGSTORE_ROOT_FILE", err)
+	}
+	return chains, nil
+}
+
+// ContainsSCT checks if the certificate contains embedded SCTs. cert can either be
+// DER or PEM encoded.
+func containsSCT(cert []byte) (bool, error) {
+	embeddedSCTs, err := x509util.ParseSCTsFromCertificate(cert)
+	if err != nil {
+		return false, err
+	}
+	if len(embeddedSCTs) != 0 {
+		return true, nil
+	}
+	return false, nil
+}
