8db90371c1ba568a8bc0ffbc4f3f3c97922384d0
Aleksei Igrychev <aleksei.igrychev@palark.com>
2025-06-17 11:43:34 +0100

    chore(task): run format
    Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>

--- pkg/build/stage/image_spec.go
+++ pkg/build/stage/image_spec.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	v1 "github.com/google/go-containerregistry/pkg/v1"
+
 	"github.com/werf/common-go/pkg/util"
 	"github.com/werf/werf/v2/pkg/config"
 	"github.com/werf/werf/v2/pkg/container_backend"
