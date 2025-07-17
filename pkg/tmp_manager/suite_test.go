1280c7e2fd9298805688a23d0f051266414885c2
Alexandr Zaytsev <alexandr.zaytsev@flant.com>
2025-07-17 10:35:10 +0300

    test(host-cleanup): cover tmp manager gc
    Signed-off-by: Alexandr Zaytsev <alexandr.zaytsev@flant.com>

---
+++ pkg/tmp_manager/suite_test.go
@@ -0,0 +1,13 @@
+package tmp_manager
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Tmp Manager Suite")
+}
