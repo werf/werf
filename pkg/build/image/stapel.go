c0050a22267d58e4a50b5ffee03abbb5c3726c44
Aleksei Igrychev <aleksei.igrychev@palark.com>
2025-08-01 13:12:18 +0100

    fix: fix verity-annotation and sign stages order
    Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>

--- pkg/build/image/stapel.go
+++ pkg/build/image/stapel.go
@@ -125,12 +125,12 @@ func initStages(ctx context.Context, image *Image, metaConfig *config.Meta, stap
 		stages = appendIfExist(ctx, stages, stage.GenerateImageSpecStage(imageBaseConfig.ImageSpec, baseStageOptions))
 	}
 
-	if opts.ManifestSigningOptions.Enabled && imageBaseConfig.IsFinal() {
-		stages = append(stages, stage.GenerateSignStage(baseStageOptions, opts.ManifestSigningOptions))
-	}
-
 	if opts.VerityAnnotationOptions.Enabled && imageBaseConfig.IsFinal() {
 		stages = append(stages, stage.GenerateVerityAnnotationStage(baseStageOptions))
+	}
+
+	if opts.ManifestSigningOptions.Enabled && imageBaseConfig.IsFinal() {
+		stages = append(stages, stage.GenerateSignStage(baseStageOptions, opts.ManifestSigningOptions))
 	}
 
 	if len(gitMappings) != 0 {
