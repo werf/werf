ca37f92fa0267c18bbc993bd5b1032eb43ef554d
Aleksei Igrychev <aleksei.igrychev@flant.com>
2025-08-08 02:20:59 +0300

    test(sign, elf): fix playground Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>
    Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>

--- playground/elf/run.sh
+++ playground/elf/run.sh
@@ -3,4 +3,4 @@
 export LD_LIBRARY_PATH=./lib 
 export PATH=$PWD:$PATH
 export WERF_ELF_PGP_PRIVATE_KEY_BASE64=$(cat key)
-werf build --sign-elf-files --dev --repo localhost:5000/test
+werf build --bsign-elf-files --dev --repo localhost:5000/test
