60a5fdcf85a4b8fdc5597732908d8e0a2ad568f3
Alexandr Zaytsev <alexandr.zaytsev@flant.com>
2025-08-25 12:38:45 +0300

    fix(buildah,host-cleanup): do requested changes
    Signed-off-by: Alexandr Zaytsev <alexandr.zaytsev@flant.com>

--- pkg/host_cleaning/cleanup_report.go
+++ pkg/host_cleaning/cleanup_report.go
@@ -7,22 +7,22 @@ import (
 
 type cleanupReport prune.Report
 
-func newCleanupReport(size int) cleanupReport {
+func newCleanupReport() cleanupReport {
 	return cleanupReport{
-		ItemsDeleted:   make([]string, 0, size),
+		ItemsDeleted:   []string{},
 		SpaceReclaimed: 0,
 	}
 }
 
 func mapPruneReportToCleanupReport(report prune.Report) cleanupReport {
-	cr := newCleanupReport(0)
+	cr := newCleanupReport()
 	cr.SpaceReclaimed = report.SpaceReclaimed
 	cr.ItemsDeleted = append(cr.ItemsDeleted, report.ItemsDeleted...)
 	return cr
 }
 
 func mapImageListToCleanupReport(list image.ImagesList) cleanupReport {
-	report := newCleanupReport(0)
+	report := newCleanupReport()
 	report.ItemsDeleted = make([]string, 0, len(list))
 	for _, img := range list {
 		report.ItemsDeleted = append(report.ItemsDeleted, img.ID)
