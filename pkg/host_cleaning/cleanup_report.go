5e161d036714111b299a11ec040710f3aef7163a
Alexandr Zaytsev <alexandr.zaytsev@flant.com>
2025-09-01 17:45:42 +0300

    fix(host-cleanup): clarify cleanupReport using
    Signed-off-by: Alexandr Zaytsev <alexandr.zaytsev@flant.com>

--- pkg/host_cleaning/cleanup_report.go
+++ pkg/host_cleaning/cleanup_report.go
@@ -1,32 +1,44 @@
 package host_cleaning
 
 import (
+	"slices"
+
 	"github.com/werf/werf/v2/pkg/container_backend/prune"
 	"github.com/werf/werf/v2/pkg/image"
 )
 
 type cleanupReport prune.Report
 
-func newCleanupReport() cleanupReport {
-	return cleanupReport{
-		ItemsDeleted:   []string{},
-		SpaceReclaimed: 0,
+func (cr cleanupReport) Normalize() cleanupReport {
+	if len(cr.ItemsDeleted) > 0 {
+		return cleanupReport{
+			ItemsDeleted:   slices.Clip(cr.ItemsDeleted),
+			SpaceReclaimed: cr.SpaceReclaimed,
+		}
 	}
+	return cleanupReport{}
 }
 
 func mapPruneReportToCleanupReport(report prune.Report) cleanupReport {
-	cr := newCleanupReport()
-	cr.SpaceReclaimed = report.SpaceReclaimed
-	cr.ItemsDeleted = append(cr.ItemsDeleted, report.ItemsDeleted...)
-	return cr
+	return cleanupReport{
+		ItemsDeleted:   report.ItemsDeleted,
+		SpaceReclaimed: report.SpaceReclaimed,
+	}
 }
 
 func mapImageListToCleanupReport(list image.ImagesList) cleanupReport {
-	report := newCleanupReport()
-	report.ItemsDeleted = make([]string, 0, len(list))
+	itemsDeleted := make([]string, 0, len(list))
+	var spaceReclaimed uint64
+
 	for _, img := range list {
-		report.ItemsDeleted = append(report.ItemsDeleted, img.ID)
-		report.SpaceReclaimed += uint64(img.Size)
+		itemsDeleted = append(itemsDeleted, img.ID)
+		spaceReclaimed += uint64(img.Size)
 	}
-	return report
+
+	report := cleanupReport{
+		ItemsDeleted:   itemsDeleted,
+		SpaceReclaimed: spaceReclaimed,
+	}
+
+	return report.Normalize()
 }
