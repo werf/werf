33a525b6ea66646c7780d152dcf78e298a40c0dd
Alexandr Zaytsev <alexandr.zaytsev@flant.com>
2025-09-25 18:44:51 +0300

    refactor(build): use parallel writer and reader
    Signed-off-by: Alexandr Zaytsev <alexandr.zaytsev@flant.com>

--- pkg/util/parallel/worker.go
+++ pkg/util/parallel/worker.go
@@ -2,78 +2,41 @@ package parallel
 
 import (
 	"fmt"
-	"io"
 	"os"
+	"time"
 )
 
-type bufferedWorker struct {
+type parallelWorker struct {
 	ID int
 
-	outStream           *os.File
-	readerOffset        int
-	readerLastReadBytes int
+	file *os.File
 
-	doneCh chan struct{}
+	Writer *parallelWorkerWriter
+	Reader *parallelWorkerReader
 }
 
-func (w *bufferedWorker) MarkWritingDone() {
-	// unblocking write
-	select {
-	case w.doneCh <- struct{}{}:
-	default:
+func (w *parallelWorker) Close() error {
+	if err := w.file.Close(); err != nil {
+		return fmt.Errorf("failed to close temp file for worker %d: %w", w.ID, err)
 	}
-}
-
-func (w *bufferedWorker) OutStream() *os.File {
-	return w.outStream
-}
-
-func (w *bufferedWorker) ReadFromStream() ([]byte, error) {
-	if _, err := w.outStream.Seek(int64(w.readerOffset), 0); err != nil {
-		return nil, fmt.Errorf("failed to seek to offset %d: %w", w.readerOffset, err)
+	if err := os.Remove(w.file.Name()); err != nil {
+		return fmt.Errorf("failed to remove temp file for worker %d: %w", w.ID, err)
 	}
-
-	data := make([]byte, 1024)
-
-	var err error
-	w.readerLastReadBytes, err = w.outStream.Read(data)
-
-	if err == io.EOF {
-		return nil, nil
-	} else if err != nil {
-		return nil, fmt.Errorf("failed to read from stream: %w", err)
-	}
-
-	w.readerOffset += w.readerLastReadBytes
-
-	return data[:w.readerLastReadBytes], nil
+	return nil
 }
 
-func (w *bufferedWorker) Scan() bool {
-	if len(w.doneCh) > 0 && w.readerLastReadBytes == 0 {
-		return false
-	}
-	return true
-}
-
-func (w *bufferedWorker) Close() error {
-	return w.outStream.Close()
-}
-
-func newBufferedWorker(id int) (*bufferedWorker, error) {
+func newParallelWorker(id int) (*parallelWorker, error) {
 	pid := os.Getpid()
 
-	file, err := os.CreateTemp("", fmt.Sprintf("parallel-worker-%d-%d", pid, id))
+	file, err := os.CreateTemp("", fmt.Sprintf("parallel-worker-%d-%d-%d", pid, id, time.Now().UnixMilli()))
 	if err != nil {
 		return nil, fmt.Errorf("failed to create temp file for worker %d: %w", id, err)
 	}
 
-	return &bufferedWorker{
+	return &parallelWorker{
 		ID:     id,
-		doneCh: make(chan struct{}, 1),
-
-		outStream:           file,
-		readerOffset:        0,
-		readerLastReadBytes: 0,
+		file:   file,
+		Writer: newParallelWorkerWriter(file),
+		Reader: newParallelWorkerReader(file),
 	}, nil
 }
