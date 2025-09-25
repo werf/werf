df7cd66cd3560404c373e32e8a46dbb9aca47f6a
Alexandr Zaytsev <alexandr.zaytsev@flant.com>
2025-09-25 11:19:59 +0300

    refactor(build): use files for output in parallel mode
    Signed-off-by: Alexandr Zaytsev <alexandr.zaytsev@flant.com>

--- pkg/util/parallel/worker.go
+++ pkg/util/parallel/worker.go
@@ -1,32 +1,79 @@
 package parallel
 
 import (
-	"github.com/werf/common-go/pkg/util"
+	"fmt"
+	"io"
+	"os"
 )
 
 type bufferedWorker struct {
-	ID  int
-	buf *util.GoroutineSafeBuffer
+	ID int
+
+	outStream           *os.File
+	readerOffset        int
+	readerLastReadBytes int
 
 	doneCh chan struct{}
 }
 
-func (w *bufferedWorker) MarkDone() {
-	w.doneCh <- struct{}{}
+func (w *bufferedWorker) MarkWritingDone() {
+	// unblocking write
+	select {
+	case w.doneCh <- struct{}{}:
+	default:
+	}
 }
 
-func (w *bufferedWorker) Buffer() *util.GoroutineSafeBuffer {
-	return w.buf
+func (w *bufferedWorker) OutStream() *os.File {
+	return w.outStream
 }
 
-func (w *bufferedWorker) IsDone() bool {
-	return len(w.doneCh) > 0
+func (w *bufferedWorker) ReadFromStream() ([]byte, error) {
+	if _, err := w.outStream.Seek(int64(w.readerOffset), 0); err != nil {
+		return nil, fmt.Errorf("failed to seek to offset %d: %w", w.readerOffset, err)
+	}
+
+	data := make([]byte, 1024)
+
+	var err error
+	w.readerLastReadBytes, err = w.outStream.Read(data)
+
+	if err == io.EOF {
+		return nil, nil
+	} else if err != nil {
+		return nil, fmt.Errorf("failed to read from stream: %w", err)
+	}
+
+	w.readerOffset += w.readerLastReadBytes
+
+	return data[:w.readerLastReadBytes], nil
 }
 
-func newBufferedWorker(id int) *bufferedWorker {
+func (w *bufferedWorker) Scan() bool {
+	if len(w.doneCh) > 0 && w.readerLastReadBytes == 0 {
+		return false
+	}
+	return true
+}
+
+func (w *bufferedWorker) Close() error {
+	return w.outStream.Close()
+}
+
+func newBufferedWorker(id int) (*bufferedWorker, error) {
+	pid := os.Getpid()
+
+	file, err := os.CreateTemp("", fmt.Sprintf("parallel-worker-%d-%d", pid, id))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create temp file for worker %d: %w", id, err)
+	}
+
 	return &bufferedWorker{
 		ID:     id,
-		buf:    util.NewGoroutineSafeBuffer(),
 		doneCh: make(chan struct{}, 1),
-	}
+
+		outStream:           file,
+		readerOffset:        0,
+		readerLastReadBytes: 0,
+	}, nil
 }
