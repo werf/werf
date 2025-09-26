4ec9dfaaf18ee128ebf48b28a9e4993f581cbd08
Evgeniy Frolov <evgeniy.frolov@flant.com>
2025-09-26 13:25:37 +0300

    chore: fix expected owner/group
    Signed-off-by: Evgeniy Frolov <evgeniy.frolov@flant.com>

--- integration/suites/build/stapel_image/git/file_lifecycle_test.go
+++ integration/suites/build/stapel_image/git/file_lifecycle_test.go
@@ -28,8 +28,8 @@ var _ = Describe("file lifecycle", func() {
 	gitExecutableFilePerm := os.FileMode(0o755)
 	gitOrdinaryFilePerm := os.FileMode(0o644)
 
-	expectedOwner := "55555"
-	expectedGroup := "55555"
+	expectedOwner := "test"
+	expectedGroup := "test"
 
 	type fileLifecycleEntry struct {
 		relPath string
