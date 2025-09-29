f9af22e2ec4a4eae12a19148fb3e52c3a6348745
Aleksei Igrychev <aleksei.igrychev@palark.com>
2025-09-29 16:17:31 +0100

    fix(ci-env): fix destination for source script
    Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>

--- cmd/werf/ci_env/ci_env.go
+++ cmd/werf/ci_env/ci_env.go
@@ -568,7 +568,7 @@ func createSourceFile(data []byte) (string, error) {
 			tempFilePattern = fmt.Sprintf(".env_%d_*", time.Now().Unix())
 		}
 
-		f, err = tmp_manager.TempFile(tempFilePattern)
+		f, err = os.CreateTemp(sourceDir, tempFilePattern)
 		if err != nil {
 			return "", err
 		}
