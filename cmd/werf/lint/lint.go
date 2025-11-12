6f10f91b07841a83d49048f68c3f7c349c94d1cd
Aleksei Igrychev <aleksei.igrychev@palark.com>
2025-11-12 12:27:12 +0000

    positive cases
    Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>

--- cmd/werf/lint/lint.go
+++ cmd/werf/lint/lint.go
@@ -165,12 +165,6 @@ func runLint(ctx context.Context, imageNameListFromArgs []string) error {
 		return fmt.Errorf("component init error: %w", err)
 	}
 
-	defer func() {
-		if err := tmp_manager.DelegateCleanup(ctx); err != nil {
-			logboek.Context(ctx).Warn().LogF("Temporary files cleanup preparation failed: %s\n", err)
-		}
-	}()
-
 	containerBackend := commonManager.ContainerBackend()
 
 	defer func() {
