bf0eb5bae91866f3a23f7456febbdcdf7730b8e0
Radmir Khurum <radmir.khurum@flant.com>
2025-11-13 02:50:04 +0700

    feat(build): add file_manager
    Signed-off-by: Radmir Khurum <radmir.khurum@flant.com>

---
+++ pkg/file_manager/common.go
@@ -0,0 +1,29 @@
+package file_manager
+
+import (
+	"fmt"
+	"os"
+)
+
+func newFileWithPath(path string) (string, error) {
+	var newFile *os.File
+	var err error
+
+	if path == "" {
+		return "", fmt.Errorf("empty path")
+	} else {
+		newFile, err = os.Create(path)
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	path = newFile.Name()
+
+	if err = newFile.Close(); err != nil {
+		return "", err
+	}
+
+	return path, err
+}
