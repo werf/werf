e655ac0763fa418386db141bd225f1e9d13ba552
Alexandr Zaytsev <alexandr.zaytsev@flant.com>
2025-11-17 13:52:24 +0300

    fix(host-cleanup): test keeping of files outputted with ci-env
    Signed-off-by: Alexandr Zaytsev <alexandr.zaytsev@flant.com>

--- test/e2e/ci-env/simple_test.go
+++ test/e2e/ci-env/simple_test.go
@@ -2,6 +2,8 @@ package ci_env_test
 
 import (
 	"path/filepath"
+	"slices"
+	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -10,39 +12,60 @@ import (
 )
 
 var _ = Describe("Simple ci-env", Label("e2e", "ci-env", "simple"), func() {
-	It("should keep copy of ~/.docker dir after host cleanup", func(ctx SpecContext) {
-		tmpDir := GinkgoT().TempDir()
+	DescribeTable("host cleanup after ci-env",
+		func(ctx SpecContext, ciEnvArgs []string, expectDockerDir, expectOutputFile bool) {
+			tmpDir := GinkgoT().TempDir()
 
-		werfProject := werf.NewProject(SuiteData.WerfBinPath, tmpDir)
+			SuiteData.InitTestRepo(ctx, tmpDir, "some-repo")
 
-		outputCiEnv := werfProject.CiEnv(ctx, &werf.CiEnvOptions{
-			CommonOptions: werf.CommonOptions{
-				ExtraArgs: []string{
-					"gitlab",
-					"--tmp-dir", tmpDir,
+			werfProject := werf.NewProject(SuiteData.WerfBinPath, SuiteData.GetTestRepoPath(tmpDir))
+
+			outputCiEnv := werfProject.CiEnv(ctx, &werf.CiEnvOptions{
+				CommonOptions: werf.CommonOptions{
+					ExtraArgs: slices.Concat(ciEnvArgs, []string{
+						"--tmp-dir", tmpDir,
+					}),
 				},
-			},
-		})
-		Expect(outputCiEnv).To(ContainSubstring("export DOCKER_CONFIG="))
+			})
 
-		globPattern := filepath.Join(tmpDir, "werf-docker-config-*")
-		Expect(glob(globPattern)).To(HaveLen(1))
-
-		outputHostCleanup := werfProject.HostCleanup(ctx, &werf.HostCleanupOptions{
-			CommonOptions: werf.CommonOptions{
-				ExtraArgs: []string{
-					"--tmp-dir", tmpDir,
+			outputHostCleanup := werfProject.HostCleanup(ctx, &werf.HostCleanupOptions{
+				CommonOptions: werf.CommonOptions{
+					ExtraArgs: []string{
+						"--tmp-dir", tmpDir,
+					},
 				},
+			})
+
+			dockerDirPattern := filepath.Join(tmpDir, "werf-docker-config-*")
+			dockerDirMatches, err := filepath.Glob(dockerDirPattern)
+			Expect(err).To(Succeed())
+
+			if expectDockerDir {
+				Expect(dockerDirMatches).To(HaveLen(1))
+				Expect(outputHostCleanup).NotTo(ContainSubstring(dockerDirMatches[0]))
+			}
+
+			if expectOutputFile {
+				Expect(strings.TrimSpace(outputCiEnv)).To(BeARegularFile())
+			}
+		},
+		Entry(
+			"should keep copy of ~/.docker dir and outputted env file",
+			[]string{
+				"gitlab",
+				"--as-env-file",
 			},
-		})
-		globMatches := glob(globPattern)
-		Expect(globMatches).To(HaveLen(1))
-		Expect(outputHostCleanup).NotTo(ContainSubstring(globMatches[0]))
-	})
+			true,
+			true,
+		),
+		Entry(
+			"should keep copy of ~/.docker dir and outputted script file",
+			[]string{
+				"github",
+				"--as-file",
+			},
+			true,
+			true,
+		),
+	)
 })
-
-func glob(pattern string) []string {
-	matches, err := filepath.Glob(pattern)
-	Expect(err).To(Succeed())
-	return matches
-}
