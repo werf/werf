9ff99aa5d3edb5a9e124fa3c36f55cdc82da3f33
Polina Sizintseva <polina.sizintseva@flant.com>
2025-12-08 18:14:29 +0300

    test(docker): avoid ephemeral port race by letting Docker choose host port
    Signed-off-by: Polina Sizintseva <polina.sizintseva@flant.com>

--- test/pkg/utils/docker/docker_registry.go
+++ test/pkg/utils/docker/docker_registry.go
@@ -3,7 +3,6 @@ package docker
 import (
 	"context"
 	"fmt"
-	"net"
 	"strings"
 
 	. "github.com/onsi/gomega"
@@ -15,17 +14,15 @@ func LocalDockerRegistryRun(ctx context.Context) (string, string, string) {
 	containerName := fmt.Sprintf("werf_test_docker_registry-%s", utils.GetRandomString(10))
 	imageName := "registry:2"
 
-	port, err := getFreeEphemeralPort()
-	Expect(err).ShouldNot(HaveOccurred())
-
 	dockerCliRunArgs := []string{
 		"-d",
-		"-p", fmt.Sprintf("%d:5000", port),
+		"-p", "0:5000",
 		"-e", "REGISTRY_STORAGE_DELETE_ENABLED=true",
 		"--name", containerName,
 		imageName,
 	}
-	err = CliRun(ctx, dockerCliRunArgs...)
+
+	err := CliRun(ctx, dockerCliRunArgs...)
 	Expect(err).ShouldNot(HaveOccurred(), "docker run "+strings.Join(dockerCliRunArgs, " "))
 
 	inspect := ContainerInspect(ctx, containerName)
@@ -33,21 +30,15 @@ func LocalDockerRegistryRun(ctx context.Context) (string, string, string) {
 	Expect(inspect.NetworkSettings.Networks["bridge"].IPAddress).ShouldNot(BeEmpty())
 	registryInternalAddress := fmt.Sprintf("%s:%d", inspect.NetworkSettings.Networks["bridge"].IPAddress, 5000)
 
-	registryLocalAddress := fmt.Sprintf("localhost:%d", port)
+	portBindings := inspect.NetworkSettings.Ports["5000/tcp"]
+	Expect(portBindings).ShouldNot(BeNil())
+	Expect(portBindings).ShouldNot(BeEmpty())
+
+	hostPort := portBindings[0].HostPort
+	registryLocalAddress := fmt.Sprintf("localhost:%s", hostPort)
 	registryWithScheme := fmt.Sprintf("http://%s", registryLocalAddress)
 
 	utils.WaitTillHostReadyToRespond(registryWithScheme, utils.DefaultWaitTillHostReadyToRespondMaxAttempts)
 
 	return registryLocalAddress, registryInternalAddress, containerName
-}
-
-// Listen on a random port using TCP protocol on localhost and return the free ephemeral port number.
-func getFreeEphemeralPort() (int, error) {
-	listener, err := net.Listen("tcp", "localhost:0")
-	if err != nil {
-		return 0, err
-	}
-	defer listener.Close()
-	port := listener.Addr().(*net.TCPAddr).Port
-	return port, nil
 }
