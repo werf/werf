48ad89064b8a59fd47234a22c5687547aa54e6d4
Radmir Khurum <radmir.khurum@flant.com>
2025-12-25 16:36:34 +0700

    feat(build): add a ability to use build report instead of building and e2e tests
    Signed-off-by: Radmir Khurum <radmir.khurum@flant.com>

--- test/e2e/kube-run/simple_test.go
+++ test/e2e/kube-run/simple_test.go
@@ -104,6 +104,109 @@ var _ = Describe("Simple kube-run", Label("e2e", "kube-run", "simple"), func() {
 			SpecTimeout(time.Minute*3),
 		),
 	)
+
+	DescribeTable("should",
+		func(ctx SpecContext, kubeRunOpts *werf.KubeRunOptions, expectOutFn func(out string)) {
+			By("initializing")
+			setupEnv()
+			repoDirname := "repo0"
+			fixtureRelPath := "simple/state0"
+			buildReportName := "report0.json"
+
+			By("state0: preparing test repo")
+			SuiteData.InitTestRepo(ctx, repoDirname, fixtureRelPath)
+			werfProject := werf.NewProject(SuiteData.WerfBinPath, SuiteData.GetTestRepoPath(repoDirname))
+
+			By("state0: building images")
+			buildOut, _ := werfProject.BuildWithReport(ctx, SuiteData.GetBuildReportPath(buildReportName), &werf.BuildWithReportOptions{
+				CommonOptions: werf.CommonOptions{
+					ShouldFail: false,
+					ExtraArgs:  []string{"--use-build-report", "--build-report-path", SuiteData.GetBuildReportPath(buildReportName)},
+				},
+			})
+			Expect(buildOut).To(ContainSubstring("Building stage"))
+			Expect(buildOut).NotTo(ContainSubstring("Use previously built image"))
+
+			By("state0: execute kube-run")
+			combinedOut := werfProject.KubeRun(ctx, kubeRunOpts)
+			Expect(combinedOut).To(ContainSubstring("Creating namespace"))
+			Expect(combinedOut).To(ContainSubstring("Running pod"))
+			Expect(combinedOut).To(ContainSubstring("Executing into pod"))
+			Expect(combinedOut).To(ContainSubstring("Stopping container"))
+			Expect(combinedOut).To(ContainSubstring("Cleaning up pod"))
+
+			expectOutFn(combinedOut)
+		},
+		Entry(
+			"succeed and produce expected output with using build report, running non-interactively",
+			&werf.KubeRunOptions{
+				Command: []string{"sh", "-ec", "cat /etc/os-release"},
+				Image:   "main",
+			},
+			func(out string) {
+				Expect(out).To(ContainSubstring("ID=ubuntu"))
+			},
+		),
+		Entry(
+			"succeed and produce expected output with using build report, running interactively with TTY",
+			&werf.KubeRunOptions{
+				Command: []string{"sh", "-ec", "cat /etc/os-release"},
+				Image:   "main",
+				CommonOptions: werf.CommonOptions{
+					ExtraArgs: []string{"-i", "-t"},
+				},
+			},
+			func(out string) {
+				Expect(out).To(ContainSubstring("ID=ubuntu"))
+			},
+		),
+		Entry(
+			"fail and produce expected output with using build report, running non-interactively",
+			&werf.KubeRunOptions{
+				Command: []string{"sh", "-ec", "cat /etc/os-release; exit 1"},
+				Image:   "main",
+				CommonOptions: werf.CommonOptions{
+					ShouldFail: true,
+				},
+			},
+			func(out string) {
+				Expect(out).To(ContainSubstring("ID=ubuntu"))
+			},
+		),
+		Entry(
+			"fail and produce expected output with using build report, running interactively with TTY",
+			&werf.KubeRunOptions{
+				Command: []string{"sh", "-ec", "cat /etc/os-release; exit 1"},
+				Image:   "main",
+				CommonOptions: werf.CommonOptions{
+					ShouldFail: true,
+					ExtraArgs:  []string{"-i", "-t"},
+				},
+			},
+			func(out string) {
+				Expect(out).To(ContainSubstring("ID=ubuntu"))
+			},
+		),
+		Entry(
+			"should cancel long running process with using build report",
+			&werf.KubeRunOptions{
+				Command: []string{"/opt/app.sh"},
+				Image:   "main",
+				CommonOptions: werf.CommonOptions{
+					ShouldFail:            true,
+					ExtraArgs:             []string{}, // be able to work without "-it" options
+					CancelOnOutput:        "Looping ...",
+					CancelOnOutputTimeout: time.Minute,
+				},
+			},
+			func(out string) {
+				Expect(out).To(ContainSubstring("Signal container"))
+				Expect(out).To(ContainSubstring("Signal handled"))   // from script
+				Expect(out).To(ContainSubstring("Script completed")) // from script
+			},
+			SpecTimeout(time.Minute*3),
+		),
+	)
 })
 
 func setupEnv() {
