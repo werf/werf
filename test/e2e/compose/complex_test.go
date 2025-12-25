ccaaa6dc9b07f7b34807b8f15755aca3ddb1aee6
Radmir Khurum <radmir.khurum@flant.com>
2025-12-25 22:18:48 +0700

    feat(build): fix kube-run e2e, add compose e2e
    Signed-off-by: Radmir Khurum <radmir.khurum@flant.com>

--- test/e2e/compose/complex_test.go
+++ test/e2e/compose/complex_test.go
@@ -66,4 +66,61 @@ var _ = Describe("Complex compose", Label("e2e", "compose", "complex"), func() {
 			Repo:             "repo1",
 		}),
 	)
+
+	DescribeTable("should",
+		func(ctx SpecContext, opts simpleTestOptions) {
+			By(fmt.Sprintf("%s: starting", opts.State))
+			{
+				repoDirname := opts.Repo
+				fixtureRelPath := fmt.Sprintf("simple/%s", opts.State)
+				buildReportName := "report0.json"
+				opts.ExtraArgs = append(opts.ExtraArgs, "--use-build-report", "--build-report-path", SuiteData.GetBuildReportPath(buildReportName))
+
+				By(fmt.Sprintf("%s: preparing test repo", opts.State))
+				SuiteData.InitTestRepo(ctx, repoDirname, fixtureRelPath)
+				werfProject := werf.NewProject(SuiteData.WerfBinPath, SuiteData.GetTestRepoPath(repoDirname))
+
+				By("state0: building images")
+				buildOut, _ := werfProject.BuildWithReport(ctx, SuiteData.GetBuildReportPath(buildReportName), &werf.BuildWithReportOptions{
+					CommonOptions: werf.CommonOptions{
+						ShouldFail: false,
+						ExtraArgs:  []string{"--save-build-report", "--build-report-path", SuiteData.GetBuildReportPath(buildReportName)},
+					},
+				})
+				Expect(buildOut).To(ContainSubstring("Building stage"))
+				Expect(buildOut).NotTo(ContainSubstring("Use previously built image"))
+
+				By(fmt.Sprintf("%s: %s", opts.State, opts.StateDescription))
+				composeOut := werfProject.Compose(ctx, &werf.BuildOptions{
+					CommonOptions: werf.CommonOptions{
+						ExtraArgs: append([]string{commandUp}, opts.ExtraArgs...),
+					},
+				})
+				Expect(composeOut).To(ContainSubstring("Building stage"))
+				Expect(composeOut).To(ContainSubstring("image backend"))
+				By(fmt.Sprintf("%s: running compose down", opts.State))
+				composeDownOut := werfProject.Compose(ctx, &werf.BuildOptions{
+					CommonOptions: werf.CommonOptions{
+						ExtraArgs: []string{commandDown},
+					},
+				})
+				Expect(composeDownOut).To(ContainSubstring("Removed"))
+			}
+		},
+		Entry("without additional options with build report using", simpleTestOptions{
+			ExtraArgs:        []string{},
+			State:            "state0",
+			StateDescription: "running compose up with no options",
+			Repo:             "repo0",
+		}),
+		Entry("with multiple compose files with build report using", simpleTestOptions{
+			ExtraArgs: []string{
+				"--docker-compose-options", "-f docker-compose.yaml -f docker-compose-b.yaml",
+				"--docker-compose-command-options", "--always-recreate-deps",
+			},
+			State:            "state1",
+			StateDescription: "running compose up with multiple compose files and args",
+			Repo:             "repo1",
+		}),
+	)
 })
