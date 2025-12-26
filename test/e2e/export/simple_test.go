e7eacb204cdfe0db2f4f99657387f88a50876942
Radmir Khurum <radmir.khurum@flant.com>
2025-12-26 23:12:01 +0700

    feat(build): resolve conflicts Signed-off-by: Radmir Khurum <radmir.khurum@flant.com>
    Signed-off-by: Radmir Khurum <radmir.khurum@flant.com>

--- test/e2e/export/simple_test.go
+++ test/e2e/export/simple_test.go
@@ -109,6 +109,108 @@ var _ = Describe("Simple export", Label("e2e", "export", "simple"), func() {
 			},
 		}),
 	)
+
+	DescribeTable("should succeed and export images using build report",
+		func(ctx SpecContext, opts simpleTestOptions) {
+			By("initializing")
+			{
+				setupEnv()
+				repoDirname := "repo"
+				fixtureRelPath := "simple"
+				buildReportName := "report0.json"
+
+				By("preparing test repo")
+				SuiteData.InitTestRepo(ctx, repoDirname, fixtureRelPath)
+				werfProject := werf.NewProject(SuiteData.WerfBinPath, SuiteData.GetTestRepoPath(repoDirname))
+
+				By("building images and saving build report")
+				buildOut, _ := werfProject.BuildWithReport(ctx, SuiteData.GetBuildReportPath(buildReportName), &werf.BuildWithReportOptions{
+					CommonOptions: werf.CommonOptions{
+						ShouldFail: false,
+						ExtraArgs:  []string{"--save-build-report", "--build-report-path", SuiteData.GetBuildReportPath(buildReportName)},
+					},
+				})
+				Expect(buildOut).To(ContainSubstring("Building stage"))
+				Expect(buildOut).NotTo(ContainSubstring("Use previously built image"))
+
+				By("running export with build report")
+				imageName := fmt.Sprintf("%s/werf-export-%s", SuiteData.RegistryLocalAddress, utils.GetRandomString(10))
+
+				exportArgs := getExportArgs(imageName, commonTestOptions{
+					Platforms:    opts.Platforms,
+					CustomLabels: opts.CustomLabels,
+				})
+				exportArgs = append(exportArgs, "--use-build-report", "--build-report-path", SuiteData.GetBuildReportPath(buildReportName))
+
+				exportOut := werfProject.Export(ctx, &werf.ExportOptions{
+					CommonOptions: werf.CommonOptions{
+						ExtraArgs: exportArgs,
+					},
+				})
+				Expect(exportOut).To(ContainSubstring("Exporting image"))
+
+				By("checking result")
+				commonCheckImageConfigFunc := func(config v1.Config) {
+					for key := range config.Labels {
+						if strings.HasSuffix(key, "werf") {
+							Fail(fmt.Sprintf("labels %s should not contain werf service labels", config.Labels))
+						}
+					}
+				}
+
+				var checkImageConfigFunc func(v1.Config)
+				var checkIndexManifestFunc func(*v1.IndexManifest)
+				if len(opts.Platforms) > 0 {
+					checkIndexManifestFunc = func(manifest *v1.IndexManifest) {
+					platformLoop:
+						for _, platform := range opts.Platforms {
+							for _, i := range manifest.Manifests {
+								if i.Platform.String() == platform {
+									break platformLoop
+								}
+							}
+
+							Fail(fmt.Sprintf("platform %s not found in index manifest", platform))
+						}
+
+						Expect(len(opts.Platforms)).To(Equal(len(manifest.Manifests)), "unexpected number of platforms in index manifest")
+					}
+				}
+				if len(opts.CustomLabels) > 0 {
+					checkImageConfigFunc = func(config v1.Config) {
+						for _, label := range opts.CustomLabels {
+							labelParts := strings.Split(label, "=")
+							Expect(config.Labels).To(HaveKeyWithValue(labelParts[0], labelParts[1]))
+						}
+
+						commonCheckImageConfigFunc(config)
+					}
+				} else {
+					checkImageConfigFunc = commonCheckImageConfigFunc
+				}
+
+				if len(opts.Platforms) > 0 {
+					checkIndexManifest(imageName, checkIndexManifestFunc, checkImageConfigFunc)
+				} else {
+					checkImageManifest(imageName, checkImageConfigFunc)
+				}
+			}
+		},
+		Entry("base with build report", simpleTestOptions{
+			CustomLabels: []string{
+				"TEST_LABEL=TEST_VALUE",
+			},
+		}),
+		Entry("multiplatform with build report", simpleTestOptions{
+			CustomLabels: []string{
+				"TEST_LABEL=TEST_VALUE",
+			},
+			Platforms: []string{
+				"linux/amd64",
+				"linux/arm64",
+			},
+		}),
+	)
 })
 
 func checkIndexManifest(reference string, checkIndexManifestFunc func(*v1.IndexManifest), checkImageConfigFunc func(v1.Config)) {
