42de33c64be8227f1e54b4d9582f937c773e83f8
immanuwell <pchpr.00@list.ru>
2025-12-28 21:08:08 +0400

    refactor: restructure docker build script to use variables and function for maintainability
    Signed-off-by: immanuwell <pchpr.00@list.ru>

--- scripts/stapel/build.sh
+++ scripts/stapel/build.sh
@@ -2,5 +2,15 @@
 
 set -e
 
-docker build -t registry-write.werf.io/werf/stapel-base:dev --target base --file stapel/Dockerfile .
-docker build -t registry-write.werf.io/werf/stapel:dev --target final --file stapel/Dockerfile .
+REGISTRY="registry-write.werf.io/werf"
+TAG="dev"
+DOCKERFILE="stapel/Dockerfile"
+
+build() {
+    local image=$1
+    local target=$2
+    docker build -t "${REGISTRY}/${image}:${TAG}" --target "$target" --file "$DOCKERFILE" .
+}
+
+build stapel-base base
+build stapel final
