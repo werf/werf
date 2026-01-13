d64f2271de281bedde007ef22b6d0a89359dbf09
Radmir Khurum <radmir.khurum@flant.com>
2026-01-13 22:24:17 +0700

    refactor: migrate from buffer in `werf stages copy` (#7293)
    Signed-off-by: Radmir Khurum <radmir.khurum@flant.com>

--- pkg/build/stages/archive_storage.go
+++ pkg/build/stages/archive_storage.go
@@ -1,9 +1,9 @@
 package stages
 
 import (
-	"bytes"
 	"context"
 	"fmt"
+	"io"
 
 	"github.com/werf/logboek"
 	"github.com/werf/werf/v2/pkg/build"
@@ -74,14 +74,10 @@ func (s *ArchiveStorage) copyAllFromRemote(ctx context.Context, fromRemote *Remo
 			stageRef := stageDesc.Info.Name
 			tag := stageDesc.Info.Tag
 
-			stageBytes := bytes.NewBuffer(nil)
-
-			if err := fromRemote.RegistryClient.PullImageArchive(ctx, stageBytes, stageRef); err != nil {
-				return fmt.Errorf("error pulling stage %q archive: %w", stageRef, err)
-			}
-
-			if err := writer.WriteStageArchive(tag, stageBytes.Bytes()); err != nil {
-				return fmt.Errorf("error writing image %q into bundle archive: %w", stageRef, err)
+			if err := writer.WriteStageArchive(tag, func(w io.Writer) error {
+				return fromRemote.RegistryClient.PullImageArchive(ctx, w, stageRef)
+			}); err != nil {
+				return fmt.Errorf("error copying stage %q: %w", stageRef, err)
 			}
 		}
 		return nil
@@ -103,14 +99,11 @@ func (s *ArchiveStorage) copyCurrentBuildFromRemote(ctx context.Context, fromRem
 			for _, infoGetter := range infoGetters {
 				logboek.Context(ctx).Default().LogFDetails("Copying stage: %s\n", infoGetter.Tag)
 
-				stageBytes := bytes.NewBuffer(nil)
-
-				if err := fromRemote.RegistryClient.PullImageArchive(ctx, stageBytes, infoGetter.GetName()); err != nil {
-					return fmt.Errorf("error pulling stage %q archive: %w", infoGetter.GetName(), err)
-				}
-
-				if err := s.Writer.WriteStageArchive(infoGetter.Tag, stageBytes.Bytes()); err != nil {
-					return fmt.Errorf("error writing image %q into bundle archive: %w", infoGetter.GetName(), err)
+				stageRef := infoGetter.GetName()
+				if err := writer.WriteStageArchive(infoGetter.Tag, func(w io.Writer) error {
+					return fromRemote.RegistryClient.PullImageArchive(ctx, w, stageRef)
+				}); err != nil {
+					return fmt.Errorf("error copying stage %q: %w", stageRef, err)
 				}
 			}
 
