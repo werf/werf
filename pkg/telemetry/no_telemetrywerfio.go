e81936c78c558189285c73ec12b4d0a7254d5a83
Polina Sizintseva <polina.sizintseva@flant.com>
2026-01-26 18:50:49 +0300

    chore(telemetry): add build, image and stage duration metrics (#7315)
    Signed-off-by: Polina Sizintseva <polina.sizintseva@flant.com>

--- pkg/telemetry/no_telemetrywerfio.go
+++ pkg/telemetry/no_telemetrywerfio.go
@@ -4,10 +4,14 @@ import "context"
 
 type NoTelemetryWerfIO struct{}
 
-func (t *NoTelemetryWerfIO) CommandStarted(context.Context)                     {}
-func (t *NoTelemetryWerfIO) SetUserID(context.Context, string)                  {}
-func (t *NoTelemetryWerfIO) SetProjectID(context.Context, string)               {}
-func (t *NoTelemetryWerfIO) SetCommand(context.Context, string)                 {}
-func (t *NoTelemetryWerfIO) CommandExited(context.Context, int)                 {}
-func (t *NoTelemetryWerfIO) SetCommandOptions(context.Context, []CommandOption) {}
-func (t *NoTelemetryWerfIO) UnshallowFailed(context.Context, error)             {}
+func (t *NoTelemetryWerfIO) CommandStarted(context.Context)                                  {}
+func (t *NoTelemetryWerfIO) SetUserID(context.Context, string)                               {}
+func (t *NoTelemetryWerfIO) SetProjectID(context.Context, string)                            {}
+func (t *NoTelemetryWerfIO) SetCommand(context.Context, string)                              {}
+func (t *NoTelemetryWerfIO) CommandExited(context.Context, int)                              {}
+func (t *NoTelemetryWerfIO) SetCommandOptions(context.Context, []CommandOption)              {}
+func (t *NoTelemetryWerfIO) UnshallowFailed(context.Context, error)                          {}
+func (t *NoTelemetryWerfIO) BuildStarted(context.Context, int)                               {}
+func (t *NoTelemetryWerfIO) BuildFinished(context.Context, bool)                             {}
+func (t *NoTelemetryWerfIO) ImageBuildFinished(context.Context, string, int64, bool)         {}
+func (t *NoTelemetryWerfIO) StageBuildFinished(context.Context, string, string, int64, bool) {}
