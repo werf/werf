13fbe92607c787094dcc8b7bce7f5da4ab8e53c8
Aleksei Igrychev <aleksei.igrychev@palark.com>
2026-02-28 10:04:46 +0000

    refactor: remove deprecated docker directive from stapel images
    Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>

--- pkg/config/stapel_image.go
+++ pkg/config/stapel_image.go
@@ -8,14 +8,9 @@ import (
 
 type StapelImage struct {
 	*StapelImageBase
-	Docker *Docker
 }
 
 func (c *StapelImage) validate() error {
-	if c.Docker != nil && c.ImageSpec != nil {
-		return newDetailedConfigError("`docker` directive is deprecated and can't be used along with `imageSpec`!", nil, c.StapelImageBase.raw.doc)
-	}
-
 	if c.Name == "" {
 		global_warnings.GlobalDeprecationWarningLn(context.Background(), "Support for the nameless image (`image: ~`) is deprecated and will be removed in v3!")
 	}
