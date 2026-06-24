10a3f82fa0c283101f9b921ff3047579e02a73d1
Evgeniy Frolov <evgeniy.frolov@flant.com>
2026-06-24 15:57:42 +0300

    fix
    Signed-off-by: Evgeniy Frolov <evgeniy.frolov@flant.com>

--- test/pkg/utils/image.go
+++ test/pkg/utils/image.go
@@ -4,21 +4,17 @@ import (
 	"archive/tar"
 	"context"
 	"io"
+	"os"
 	"strings"
 
-	"github.com/google/go-containerregistry/pkg/name"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/mutate"
-	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
 	. "github.com/onsi/gomega"
 )
 
-func ExpectFileContentInImage(imageName, filePath, expectedContent string) {
-	ref, err := name.ParseReference(imageName, name.Insecure)
-	Expect(err).NotTo(HaveOccurred())
-
-	img, err := remote.Image(ref)
-	Expect(err).NotTo(HaveOccurred())
-
+func ExpectFileContentInImage(ctx context.Context, backendMode, imageName, filePath, expectedContent string) {
+	img := loadLocalImage(ctx, backendMode, imageName)
 	rc := mutate.Extract(img)
 	defer rc.Close()
 
@@ -42,6 +38,43 @@ func ExpectFileContentInImage(imageName, filePath, expectedContent string) {
 	}
 
 	Expect(fileContent).To(Equal(expectedContent), "expected image file content to match for %s", filePath)
+}
+
+func ExpectImageHasNonEmptyLabels(ctx context.Context, backendMode, imageName string, labelKeys ...string) {
+	img := loadLocalImage(ctx, backendMode, imageName)
+	config, err := img.ConfigFile()
+	Expect(err).NotTo(HaveOccurred())
+
+	labels := config.Config.Labels
+	Expect(labels).NotTo(BeNil())
+
+	for _, key := range labelKeys {
+		Expect(labels).To(HaveKey(key))
+		Expect(labels[key]).NotTo(BeEmpty())
+	}
+}
+
+func loadLocalImage(ctx context.Context, backendMode, imageName string) v1.Image {
+	tempFile, err := os.CreateTemp("", "werf-e2e-image-*.tar")
+	Expect(err).NotTo(HaveOccurred())
+
+	tempTarPath := tempFile.Name()
+	Expect(tempFile.Close()).To(Succeed())
+	defer os.Remove(tempTarPath)
+
+	switch backendMode {
+	case "docker", "vanilla-docker", "buildkit-docker":
+		RunSucceedCommand(ctx, "/", "docker", "save", "-o", tempTarPath, imageName)
+	case "native-rootless", "native-chroot":
+		RunSucceedCommand(ctx, "/", "buildah", "push", "--tls-verify=false", "--format", "docker", imageName, "docker-archive:"+tempTarPath)
+	default:
+		Expect(false).To(BeTrue(), "unsupported backend mode: %s", backendMode)
+	}
+
+	img, err := tarball.ImageFromPath(tempTarPath, nil)
+	Expect(err).NotTo(HaveOccurred())
+
+	return img
 }
 
 func GetBuiltImageLastStageImageName(ctx context.Context, testDirPath, werfBinPath, imageName string) string {
