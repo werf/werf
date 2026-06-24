086703f965e966f03c649fd043c6791ffb48d354
Evgeniy Frolov <evgeniy.frolov@flant.com>
2026-06-24 15:35:22 +0300

    fix
    Signed-off-by: Evgeniy Frolov <evgeniy.frolov@flant.com>

--- test/e2e/build/scratch_test.go
+++ test/e2e/build/scratch_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/werf/werf/v2/pkg/image"
 	"github.com/werf/werf/v2/test/pkg/contback"
 	"github.com/werf/werf/v2/test/pkg/report"
+	"github.com/werf/werf/v2/test/pkg/utils"
 	"github.com/werf/werf/v2/test/pkg/werf"
 )
 
@@ -44,12 +45,7 @@ var _ = Describe("Scratch stapel build", Label("e2e", "build", "scratch", "simpl
 			if testOpts.WithLocalRepo {
 				contRuntime.Pull(ctx, imageName)
 			}
-			contRuntime.ExpectCmdsToSucceed(
-				ctx,
-				imageName,
-				"test -f /etc/werf-test-scratch-import",
-				"echo 'werf-test-scratch-import' | diff /etc/werf-test-scratch-import -",
-			)
+			utils.ExpectFileContentInImage(imageName, "etc/werf-test-scratch-import", "werf-test-scratch-import\n")
 			inspect := contRuntime.GetImageInspect(ctx, imageName)
 			labels := inspect.Config.Labels
 			Expect(labels).NotTo(BeNil())
--- test/pkg/utils/image.go
+++ test/pkg/utils/image.go
@@ -1,9 +1,48 @@
 package utils
 
 import (
+	"archive/tar"
 	"context"
+	"io"
 	"strings"
+
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	. "github.com/onsi/gomega"
 )
+
+func ExpectFileContentInImage(imageName, filePath, expectedContent string) {
+	ref, err := name.ParseReference(imageName, name.Insecure)
+	Expect(err).NotTo(HaveOccurred())
+
+	img, err := remote.Image(ref)
+	Expect(err).NotTo(HaveOccurred())
+
+	rc := mutate.Extract(img)
+	defer rc.Close()
+
+	var fileContent string
+
+	tr := tar.NewReader(rc)
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		Expect(err).NotTo(HaveOccurred())
+
+		if hdr.Name != filePath && hdr.Name != "./"+filePath {
+			continue
+		}
+
+		data, err := io.ReadAll(tr)
+		Expect(err).NotTo(HaveOccurred())
+		fileContent = string(data)
+	}
+
+	Expect(fileContent).To(Equal(expectedContent), "expected image file content to match for %s", filePath)
+}
 
 func GetBuiltImageLastStageImageName(ctx context.Context, testDirPath, werfBinPath, imageName string) string {
 	stageImageName := SucceedCommandOutputString(ctx, testDirPath, werfBinPath, "stage", "image", imageName)
