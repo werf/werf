fdeabf02227f36975daf7aecc32d6ad1578c7374
Aleksei Igrychev <aleksei.igrychev@palark.com>
2026-07-07 10:57:22 +0100

    chore(build): enable stapel arm64 build by default on docker-server backend
    arm64 is a fully supported platform in v3, not experimental anymore -
    the stapel image already ships arm64 manifests. Remove the
    WERF_EXPERIMENTAL_STAPEL_ARM opt-in gate so linux/arm64 (and variants)
    work out of the box alongside linux/amd64, same as before.
    
    Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>

--- pkg/container_backend/legacy_stage_image.go
+++ pkg/container_backend/legacy_stage_image.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"strings"
 
-	"github.com/werf/common-go/pkg/util"
 	"github.com/werf/lockgate"
 	"github.com/werf/logboek"
 	"github.com/werf/werf/v2/pkg/docker"
@@ -77,12 +76,11 @@ func (i *LegacyStageImage) Build(ctx context.Context, options BuildOptions) erro
 		i.container.runOptions.AddNetwork(options.Network)
 	}
 
-	experimentalStapelArm := util.GetBoolEnvironmentDefaultFalse("WERF_EXPERIMENTAL_STAPEL_ARM")
 	targetPlatform := i.GetTargetPlatform()
 	defaultPlatform := i.ContainerBackend.GetDefaultPlatform()
 
 	if targetPlatform == defaultPlatform && defaultPlatform != "linux/amd64" {
-		if !(experimentalStapelArm && isArm64Platform(defaultPlatform)) {
+		if !isArm64Platform(defaultPlatform) {
 			logboek.Context(ctx).Error().LogF("Detected your default build platform as %s.\n", defaultPlatform)
 			logboek.Context(ctx).Error().LogF("Building of stapel-type images using Docker-Server backend for platforms other than linux/amd64 is not supported.\n")
 			logboek.Context(ctx).Error().LogF("Please either:\n * confirm emulation of linux/amd64 by explicitly setting --platform=linux/amd64 param;\n * or use Dockerfile-type image instead.\n")
@@ -92,7 +90,7 @@ func (i *LegacyStageImage) Build(ctx context.Context, options BuildOptions) erro
 	}
 
 	if targetPlatform != "" && targetPlatform != "linux/amd64" {
-		if !(experimentalStapelArm && isArm64Platform(targetPlatform)) {
+		if !isArm64Platform(targetPlatform) {
 			logboek.Context(ctx).Error().LogF("Building of stapel-type images using Docker-Server backend for platforms other than linux/amd64 is not supported.\n")
 			logboek.Context(ctx).Error().LogF("Please either:\n * use Buildah backend to build stapel-type images for arbitrary platforms;\n * or use Dockerfile-type images with any backend.\n")
 			logboek.Context(ctx).Error().LogLn()
