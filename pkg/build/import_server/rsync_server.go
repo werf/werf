20ff2c42335c7303cd3c97e830d971fb52f67f45
Aleksei Igrychev <aleksei.igrychev@palark.com>
2026-07-09 20:15:17 +0100

    fix(import): preserve symlinks in rsync-based imports
    Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>

--- pkg/build/import_server/rsync_server.go
+++ pkg/build/import_server/rsync_server.go
@@ -132,10 +132,7 @@ func (srv *RsyncServer) GetCopyCommand(ctx context.Context, importConfig *config
 
 	rsyncImportPathSpec := fmt.Sprintf("rsync://%s@%s:%s/import/%s", srv.AuthUser, srv.IPAddress, srv.Port, importConfig.Add)
 
-	// TODO(major): remove `-L`
-	// Dereferencing symlinks (`-L`) copies targets instead of symlinks themselves,
-	// which breaks checksums and pulls extra data. Symlinks must be preserved as-is.
-	rsyncStatImportPathCommand := fmt.Sprintf("RSYNC_PASSWORD='%s' %s -L %s", srv.AuthPassword, stapel.RsyncBinPath(), rsyncImportPathSpec)
+	rsyncStatImportPathCommand := fmt.Sprintf("RSYNC_PASSWORD='%s' %s %s", srv.AuthPassword, stapel.RsyncBinPath(), rsyncImportPathSpec)
 
 	// save stat output to variable
 	args = append(args, fmt.Sprintf("statOutput=$(%s)", rsyncStatImportPathCommand))
