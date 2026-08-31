werf/werf

commit 69140cdeff4e39e03123caaaa0fce6d0ab1feb8e

feat(buildah): refactor container-runtime interface and fix usage of runtime
1. Removed excess container_runtime.DockerImage and container_runtime.Image. Use LegacyImageInterface instead for legacy container-runtime operations.
2. Make build conveyor indepdenent of DockerServerRuntime implementation, use generic ContainerRuntime interface instead everywhere.
3. Renamed legacy GetImageInspect to GetImageInfo and refactored LegacyImageInterface and all other code to use this new method.
4. Added necessary Tag and Push operations into the ContainerRuntime interface.

pkg/container_runtime/legacy_base_image.go
@@ -4,23 +4,21 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/docker/docker/api/types"
-
 	"github.com/werf/werf/pkg/image"
 )
 
 type legacyBaseImage struct {
 	name      string
-	inspect   *types.ImageInspect
+	info      *image.Info
 	stageDesc *image.StageDescription
 
-	DockerServerRuntime *DockerServerRuntime
+	ContainerRuntime ContainerRuntime
 }
 
-func newLegacyBaseImage(name string, dockerServerRuntime *DockerServerRuntime) *legacyBaseImage {
+func newLegacyBaseImage(name string, containerRuntime ContainerRuntime) *legacyBaseImage {
 	img := &legacyBaseImage{}
 	img.name = name
-	img.DockerServerRuntime = dockerServerRuntime
+	img.ContainerRuntime = containerRuntime
 	return img
 }
 
@@ -32,29 +30,29 @@ func (i *legacyBaseImage) SetName(name string) {
 	i.name = name
 }
 
-func (i *legacyBaseImage) MustResetInspect(ctx context.Context) error {
-	if inspect, err := i.DockerServerRuntime.GetImageInspect(ctx, i.Name()); err != nil {
-		return fmt.Errorf("unable to get inspect for image %s: %s", i.Name(), err)
+func (i *legacyBaseImage) MustResetInfo(ctx context.Context) error {
+	if info, err := i.ContainerRuntime.GetImageInfo(ctx, i.Name()); err != nil {
+		return fmt.Errorf("unable to get info for image %s: %s", i.Name(), err)
 	} else {
-		i.SetInspect(inspect)
+		i.SetInfo(info)
 	}
 
-	if i.inspect == nil {
-		panic(fmt.Sprintf("runtime error: inspect must be (%s)", i.name))
+	if i.info == nil {
+		panic(fmt.Sprintf("runtime error: info must be set for image %q", i.name))
 	}
 	return nil
 }
 
-func (i *legacyBaseImage) GetInspect() *types.ImageInspect {
-	return i.inspect
+func (i *legacyBaseImage) GetInfo() *image.Info {
+	return i.info
 }
 
-func (i *legacyBaseImage) SetInspect(inspect *types.ImageInspect) {
-	i.inspect = inspect
+func (i *legacyBaseImage) SetInfo(info *image.Info) {
+	i.info = info
 }
 
-func (i *legacyBaseImage) UnsetInspect() {
-	i.inspect = nil
+func (i *legacyBaseImage) UnsetInfo() {
+	i.info = nil
 }
 
 func (i *legacyBaseImage) SetStageDescription(stageDesc *image.StageDescription) {
@@ -66,5 +64,5 @@ func (i *legacyBaseImage) GetStageDescription() *image.StageDescription {
 }
 
 func (i *legacyBaseImage) IsExistsLocally() bool {
-	return i.inspect != nil
+	return i.info != nil
 }

pkg/container_runtime/legacy_stage_image.go
@@ -9,8 +9,6 @@ import (
 	"github.com/werf/lockgate"
 	"github.com/werf/werf/pkg/werf"
 
-	"github.com/docker/docker/api/types"
-
 	"github.com/werf/logboek"
 
 	"github.com/werf/werf/pkg/docker"
@@ -31,10 +29,6 @@ func NewLegacyStageImage(fromImage *LegacyStageImage, name string, localDockerSe
 	stage.fromImage = fromImage
 	stage.container = newLegacyStageImageContainer(stage)
 	return stage
-}
-
-func (i *LegacyStageImage) Inspect() *types.ImageInspect {
-	return i.inspect
 }
 
 func (i *LegacyStageImage) BuilderContainer() LegacyBuilderContainer {
@@ -120,13 +114,13 @@ func (i *LegacyStageImage) Build(ctx context.Context, options LegacyBuildOptions
 		}
 	}
 
-	if inspect, err := i.DockerServerRuntime.GetImageInspect(ctx, i.MustGetBuiltId()); err != nil {
+	if info, err := i.ContainerRuntime.GetImageInfo(ctx, i.MustGetBuiltId()); err != nil {
 		return err
 	} else {
-		i.SetInspect(inspect)
+		i.SetInfo(info)
 		i.SetStageDescription(&image.StageDescription{
 			StageID: nil, // stage id does not available at the moment
-			Info:    image.NewInfoFromInspect(i.Name(), inspect),
+			Info:    info,
 		})
 	}
 
@@ -139,7 +133,7 @@ func (i *LegacyStageImage) Commit(ctx context.Context) error {
 		return err
 	}
 
-	i.buildImage = newLegacyBaseImage(builtId, i.DockerServerRuntime)
+	i.buildImage = newLegacyBaseImage(builtId, i.ContainerRuntime)
 
 	return nil
 }
@@ -160,19 +154,19 @@ func (i *LegacyStageImage) introspectBefore(ctx context.Context) error {
 	return nil
 }
 
-func (i *LegacyStageImage) MustResetInspect(ctx context.Context) error {
+func (i *LegacyStageImage) MustResetInfo(ctx context.Context) error {
 	if i.buildImage != nil {
-		return i.buildImage.MustResetInspect(ctx)
+		return i.buildImage.MustResetInfo(ctx)
 	} else {
-		return i.legacyBaseImage.MustResetInspect(ctx)
+		return i.legacyBaseImage.MustResetInfo(ctx)
 	}
 }
 
-func (i *LegacyStageImage) GetInspect() *types.ImageInspect {
+func (i *LegacyStageImage) GetInfo() *image.Info {
 	if i.buildImage != nil {
-		return i.buildImage.GetInspect()
+		return i.buildImage.GetInfo()
 	} else {
-		return i.legacyBaseImage.GetInspect()
+		return i.legacyBaseImage.GetInfo()
 	}
 }
 
@@ -207,7 +201,7 @@ func (i *LegacyStageImage) Pull(ctx context.Context) error {
 		return err
 	}
 
-	i.legacyBaseImage.UnsetInspect()
+	i.legacyBaseImage.UnsetInfo()
 
 	return nil
 }
@@ -218,7 +212,7 @@ func (i *LegacyStageImage) Push(ctx context.Context) error {
 
 func (i *LegacyStageImage) DockerfileImageBuilder() *DockerfileImageBuilder {
 	if i.dockerfileImageBuilder == nil {
-		i.dockerfileImageBuilder = NewDockerfileImageBuilder(i.DockerServerRuntime) // TODO: Possibly need to change DockerServerRuntime to abstract ContainerRuntime
+		i.dockerfileImageBuilder = NewDockerfileImageBuilder(i.ContainerRuntime) // TODO: Possibly need to change DockerServerRuntime to abstract ContainerRuntime
 	}
 	return i.dockerfileImageBuilder
 }

pkg/container_runtime/legacy_stage_image_container.go
@@ -232,10 +232,16 @@ func (c *LegacyStageImageContainer) prepareInheritedCommitOptions(ctx context.Co
 		panic(fmt.Sprintf("runtime error: FromImage should be (%s)", c.image.name))
 	}
 
-	if err := c.image.fromImage.MustResetInspect(ctx); err != nil {
-		return nil, fmt.Errorf("unable to reset inspect for image %s: %s", c.image.fromImage.Name(), err)
+	if err := c.image.fromImage.MustResetInfo(ctx); err != nil {
+		return nil, fmt.Errorf("unable to reset info for image %s: %s", c.image.fromImage.Name(), err)
 	}
-	fromImageInspect := c.image.fromImage.GetInspect()
+
+	dockerServerRuntime := c.image.ContainerRuntime.(*DockerServerRuntime)
+
+	fromImageInspect, err := dockerServerRuntime.GetImageInspect(ctx, c.image.fromImage.Name())
+	if err != nil {
+		return nil, fmt.Errorf("unable to get image inspect: %s")
+	}
 
 	if len(fromImageInspect.Config.Cmd) != 0 {
 		inheritedOptions.Cmd = fmt.Sprintf("[\"%s\"]", strings.Join(fromImageInspect.Config.Cmd, "\", \""))

pkg/build/build_phase.go
@@ -697,7 +697,7 @@ func (phase *BuildPhase) atomicBuildStageImage(ctx context.Context, img *Image, 
 			phase.Conveyor.SetStageImage(stageImageObj)
 
 			if err := logboek.Context(ctx).Default().LogProcess("Store stage into %s", phase.Conveyor.StorageManager.GetStagesStorage().String()).DoError(func() error {
-				if err := phase.Conveyor.StorageManager.GetStagesStorage().StoreImage(ctx, &container_runtime.DockerImage{Image: stageImage}); err != nil {
+				if err := phase.Conveyor.StorageManager.GetStagesStorage().StoreImage(ctx, stageImage); err != nil {
 					return fmt.Errorf("unable to store stage %s digest %s image %s into repo %s: %s", stg.LogDetailedName(), stg.GetDigest(), stageImage.Name(), phase.Conveyor.StorageManager.GetStagesStorage().String(), err)
 				}
 				if desc, err := phase.Conveyor.StorageManager.GetStagesStorage().GetStageDescription(ctx, phase.Conveyor.projectName(), stg.GetDigest(), uniqueID); err != nil {

pkg/build/image.go
@@ -169,7 +169,7 @@ func (i *Image) FetchBaseImage(ctx context.Context, c *Conveyor) error {
 				options.Style(style.Highlight())
 			}).
 			DoError(func() error {
-				return c.ContainerRuntime.PullImageFromRegistry(ctx, &container_runtime.DockerImage{Image: i.baseImage})
+				return c.ContainerRuntime.PullImageFromRegistry(ctx, i.baseImage)
 			}); err != nil {
 			return err
 		}
@@ -186,7 +186,7 @@ func (i *Image) FetchBaseImage(ctx context.Context, c *Conveyor) error {
 		}
 	case StageAsBaseImage:
 		// TODO: check no bug introduced
-		//if err := c.ContainerRuntime.RefreshImageObject(ctx, &container_runtime.DockerImage{Image: i.baseImage}); err != nil {
+		//if err := c.ContainerRuntime.RefreshImageObject(ctx, &container_runtime.Image{Image: i.baseImage}); err != nil {
 		//	return err
 		//}
 		if err := c.StorageManager.FetchStage(ctx, c.ContainerRuntime, i.stageAsBaseImage); err != nil {

pkg/container_runtime/buildah_runtime.go
@@ -9,19 +9,19 @@ type BuildahImage struct {
 	Image LegacyImageInterface
 }
 
-func (runtime *BuildahRuntime) RefreshImageObject(ctx context.Context, img Image) error {
+func (runtime *BuildahRuntime) RefreshImageObject(ctx context.Context, img LegacyImageInterface) error {
 	panic("not implemented")
 }
 
-func (runtime *BuildahRuntime) PullImageFromRegistry(ctx context.Context, img Image) error {
+func (runtime *BuildahRuntime) PullImageFromRegistry(ctx context.Context, img LegacyImageInterface) error {
 	panic("not implemented")
 }
 
-func (runtime *BuildahRuntime) RenameImage(ctx context.Context, img Image, newImageName string, removeOldName bool) error {
+func (runtime *BuildahRuntime) RenameImage(ctx context.Context, img LegacyImageInterface, newImageName string, removeOldName bool) error {
 	panic("not implemented")
 }
 
-func (runtime *BuildahRuntime) RemoveImage(ctx context.Context, img Image) error {
+func (runtime *BuildahRuntime) RemoveImage(ctx context.Context, img LegacyImageInterface) error {
 	panic("not implemented")
 }
 

pkg/container_runtime/docker_server_runtime.go
@@ -14,12 +14,17 @@ import (
 
 type DockerServerRuntime struct{}
 
-type DockerImage struct {
-	Image LegacyImageInterface
-}
-
 func (runtime *DockerServerRuntime) BuildDockerfile(ctx context.Context, dockerfile []byte, opts BuildDockerfileOptions) (string, error) {
 	panic("not implemeneted")
+}
+
+func (runtime *DockerServerRuntime) GetImageInfo(ctx context.Context, ref string) (*image.Info, error) {
+	inspect, err := docker.ImageInspect(ctx, ref)
+	if client.IsErrNotFound(err) {
+		return nil, nil
+	}
+
+	return image.NewInfoFromInspect(ref, inspect), nil
 }
 
 // GetImageInspect only available for DockerServerRuntime
@@ -40,23 +45,19 @@ func (runtime *DockerServerRuntime) PullImage(ctx context.Context, ref string) e
 	return nil
 }
 
-func (runtime *DockerServerRuntime) RefreshImageObject(ctx context.Context, img Image) error {
-	dockerImage := img.(*DockerImage)
-
-	if inspect, err := runtime.GetImageInspect(ctx, dockerImage.Image.Name()); err != nil {
+func (runtime *DockerServerRuntime) RefreshImageObject(ctx context.Context, img LegacyImageInterface) error {
+	if info, err := runtime.GetImageInfo(ctx, img.Name()); err != nil {
 		return err
 	} else {
-		dockerImage.Image.SetInspect(inspect)
+		img.SetInfo(info)
 	}
 	return nil
 }
 
-func (runtime *DockerServerRuntime) RenameImage(ctx context.Context, img Image, newImageName string, removeOldName bool) error {
-	dockerImage := img.(*DockerImage)
-
-	if err := logboek.Context(ctx).Info().LogProcess(fmt.Sprintf("Tagging image %s by name %s", dockerImage.Image.Name(), newImageName)).DoError(func() error {
-		if err := docker.CliTag(ctx, dockerImage.Image.Name(), newImageName); err != nil {
-			return fmt.Errorf("unable to tag image %s by name %s: %s", dockerImage.Image.Name(), newImageName, err)
+func (runtime *DockerServerRuntime) RenameImage(ctx context.Context, img LegacyImageInterface, newImageName string, removeOldName bool) error {
+	if err := logboek.Context(ctx).Info().LogProcess(fmt.Sprintf("Tagging image %s by name %s", img.Name(), newImageName)).DoError(func() error {
+		if err := docker.CliTag(ctx, img.Name(), newImageName); err != nil {
+			return fmt.Errorf("unable to tag image %s by name %s: %s", img.Name(), newImageName, err)
 		}
 		return nil
 	}); err != nil {
@@ -64,8 +65,8 @@ func (runtime *DockerServerRuntime) RenameImage(ctx context.Context, img Image, 
 	}
 
 	if removeOldName {
-		if err := logboek.Context(ctx).Info().LogProcess(fmt.Sprintf("Removing old image tag %s", dockerImage.Image.Name())).DoError(func() error {
-			if err := docker.CliRmi(ctx, dockerImage.Image.Name()); err != nil {
+		if err := logboek.Context(ctx).Info().LogProcess(fmt.Sprintf("Removing old image tag %s", img.Name())).DoError(func() error {
+			if err := docker.CliRmi(ctx, img.Name()); err != nil {
 				return err
 			}
 			return nil
@@ -74,15 +75,15 @@ func (runtime *DockerServerRuntime) RenameImage(ctx context.Context, img Image, 
 		}
 	}
 
-	dockerImage.Image.SetName(newImageName)
+	img.SetName(newImageName)
 
-	if inspect, err := runtime.GetImageInspect(ctx, dockerImage.Image.Name()); err != nil {
+	if info, err := runtime.GetImageInfo(ctx, img.Name()); err != nil {
 		return err
 	} else {
-		dockerImage.Image.SetInspect(inspect)
+		img.SetInfo(info)
 	}
 
-	desc := dockerImage.Image.GetStageDescription()
+	desc := img.GetStageDescription()
 
 	repository, tag := image.ParseRepositoryAndTag(newImageName)
 	desc.Info.Repository = repository
@@ -91,11 +92,9 @@ func (runtime *DockerServerRuntime) RenameImage(ctx context.Context, img Image, 
 	return nil
 }
 
-func (runtime *DockerServerRuntime) RemoveImage(ctx context.Context, img Image) error {
-	dockerImage := img.(*DockerImage)
-
-	if err := logboek.Context(ctx).Info().LogProcess(fmt.Sprintf("Removing image tag %s", dockerImage.Image.Name())).DoError(func() error {
-		if err := docker.CliRmi(ctx, dockerImage.Image.Name()); err != nil {
+func (runtime *DockerServerRuntime) RemoveImage(ctx context.Context, img LegacyImageInterface) error {
+	if err := logboek.Context(ctx).Info().LogProcess(fmt.Sprintf("Removing image tag %s", img.Name())).DoError(func() error {
+		if err := docker.CliRmi(ctx, img.Name()); err != nil {
 			return err
 		}
 		return nil
@@ -106,27 +105,31 @@ func (runtime *DockerServerRuntime) RemoveImage(ctx context.Context, img Image) 
 	return nil
 }
 
-func (runtime *DockerServerRuntime) PullImageFromRegistry(ctx context.Context, img Image) error {
-	dockerImage := img.(*DockerImage)
-
-	if err := dockerImage.Image.Pull(ctx); err != nil {
-		return fmt.Errorf("unable to pull image %s: %s", dockerImage.Image.Name(), err)
+func (runtime *DockerServerRuntime) PullImageFromRegistry(ctx context.Context, img LegacyImageInterface) error {
+	if err := img.Pull(ctx); err != nil {
+		return fmt.Errorf("unable to pull image %s: %s", img.Name(), err)
 	}
 
-	if inspect, err := runtime.GetImageInspect(ctx, dockerImage.Image.Name()); err != nil {
-		return fmt.Errorf("unable to get inspect of image %s: %s", dockerImage.Image.Name(), err)
+	if info, err := runtime.GetImageInfo(ctx, img.Name()); err != nil {
+		return fmt.Errorf("unable to get inspect of image %s: %s", img.Name(), err)
 	} else {
-		dockerImage.Image.SetInspect(inspect)
+		img.SetInfo(info)
 	}
 
 	return nil
 }
 
-func (runtime *DockerServerRuntime) PushImage(ctx context.Context, img Image) error {
-	dockerImage := img.(*DockerImage)
+func (runtime *DockerServerRuntime) Tag(ctx context.Context, ref, newRef string) error {
+	panic("not implemented")
+}
 
-	if err := logboek.Context(ctx).Info().LogProcess(fmt.Sprintf("Pushing %s", dockerImage.Image.Name())).DoError(func() error {
-		return docker.CliPushWithRetries(ctx, dockerImage.Image.Name())
+func (runtime *DockerServerRuntime) Push(ctx context.Context, ref string) error {
+	panic("not implemented")
+}
+
+func (runtime *DockerServerRuntime) PushImage(ctx context.Context, img LegacyImageInterface) error {
+	if err := logboek.Context(ctx).Info().LogProcess(fmt.Sprintf("Pushing %s", img.Name())).DoError(func() error {
+		return docker.CliPushWithRetries(ctx, img.Name())
 	}); err != nil {
 		return err
 	}
@@ -135,20 +138,18 @@ func (runtime *DockerServerRuntime) PushImage(ctx context.Context, img Image) er
 }
 
 // PushBuiltImage is only available for DockerServerRuntime
-func (runtime *DockerServerRuntime) PushBuiltImage(ctx context.Context, img Image) error {
-	dockerImage := img.(*DockerImage)
-
-	if err := logboek.Context(ctx).Info().LogProcess(fmt.Sprintf("Tagging built image by name %s", dockerImage.Image.Name())).DoError(func() error {
-		if err := dockerImage.Image.TagBuiltImage(ctx); err != nil {
-			return fmt.Errorf("unable to tag built image by name %s: %s", dockerImage.Image.Name(), err)
+func (runtime *DockerServerRuntime) PushBuiltImage(ctx context.Context, img LegacyImageInterface) error {
+	if err := logboek.Context(ctx).Info().LogProcess(fmt.Sprintf("Tagging built image by name %s", img.Name())).DoError(func() error {
+		if err := img.TagBuiltImage(ctx); err != nil {
+			return fmt.Errorf("unable to tag built image by name %s: %s", img.Name(), err)
 		}
 		return nil
 	}); err != nil {
 		return err
 	}
 
-	if err := logboek.Context(ctx).Info().LogProcess(fmt.Sprintf("Pushing %s", dockerImage.Image.Name())).DoError(func() error {
-		return dockerImage.Image.Push(ctx)
+	if err := logboek.Context(ctx).Info().LogProcess(fmt.Sprintf("Pushing %s", img.Name())).DoError(func() error {
+		return img.Push(ctx)
 	}); err != nil {
 		return err
 	}
@@ -157,12 +158,10 @@ func (runtime *DockerServerRuntime) PushBuiltImage(ctx context.Context, img Imag
 }
 
 // TagBuiltImageByName is only available for DockerServerRuntime
-func (runtime *DockerServerRuntime) TagImageByName(ctx context.Context, img Image) error {
-	dockerImage := img.(*DockerImage)
-
-	if dockerImage.Image.GetBuiltId() != "" {
-		if err := dockerImage.Image.TagBuiltImage(ctx); err != nil {
-			return fmt.Errorf("unable to tag image %s: %s", dockerImage.Image.Name(), err)
+func (runtime *DockerServerRuntime) TagImageByName(ctx context.Context, img LegacyImageInterface) error {
+	if img.GetBuiltId() != "" {
+		if err := img.TagBuiltImage(ctx); err != nil {
+			return fmt.Errorf("unable to tag image %s: %s", img.Name(), err)
 		}
 	} else {
 		if err := runtime.RefreshImageObject(ctx, img); err != nil {

pkg/container_runtime/interface.go
@@ -3,6 +3,8 @@ package container_runtime
 import (
 	"context"
 	"io"
+
+	"github.com/werf/werf/pkg/image"
 )
 
 type BuildDockerfileOptions struct {
@@ -28,24 +30,23 @@ type BuildDockerfileOptions struct {
 type ContainerRuntime interface {
 	//GetImageInspect(ctx context.Context, ref string) (image.Info, error)
 	//Pull(ctx context.Context, ref string) error
-	//Tag(ctx, ref, newRef string)
 	//Rmi(ctx, ref string)
-	//Push(ctx, ref string)
 
+	Tag(ctx context.Context, ref, newRef string) error
+	Push(ctx context.Context, ref string) error
+
+	GetImageInfo(ctx context.Context, ref string) (*image.Info, error)
 	BuildDockerfile(ctx context.Context, dockerfile []byte, opts BuildDockerfileOptions) (string, error)
 	//StapelBuild(opts StapelBuildOptions) string
 
 	String() string
 
 	// Legacy
-	RefreshImageObject(ctx context.Context, img Image) error
-	PullImageFromRegistry(ctx context.Context, img Image) error
-	RenameImage(ctx context.Context, img Image, newImageName string, removeOldName bool) error
-	RemoveImage(ctx context.Context, img Image) error
+	RefreshImageObject(ctx context.Context, img LegacyImageInterface) error
+	PullImageFromRegistry(ctx context.Context, img LegacyImageInterface) error
+	RenameImage(ctx context.Context, img LegacyImageInterface, newImageName string, removeOldName bool) error
+	RemoveImage(ctx context.Context, img LegacyImageInterface) error
 }
-
-// Legacy image handle
-type Image interface{}
 
 /*
  * Stapel + docker server

commit 17f71c55c417567a3b3346c87f88bbd0d284bc01

fix(build, stapel): stop panicking when the base image is gone at commit (#7819)
## Summary

Building a stapel stage with the Docker Server backend aborted with
`panic: runtime error: info must be set for image "<ref>"` when the base
image reference disappeared from the local backend while the stage's
build container was running. The panic needs a pre-existing host state —
something outside the werf process removing local images mid-build, e.g.
a prune on a container backend shared with other jobs — so it does not
reproduce from a clean checkout.

## What

- A stage whose base image tag is removed after its build container has
started now commits normally instead of failing. VERIFIED: on a Linux
host, `docker rmi -f <base stage tag>` during the stage's run panics
before this change and builds through after it.
- A base image missing before the container starts still fails the
build, now with the `docker run` error rather than a panic.
- The base image is inspected once per stage, before the container runs,
by the same reference `docker run` receives — the committed image id
when the stage was built in this process, the stage name otherwise.
Previously commit inspected it a second time, and always by name, so the
two could disagree.
- An inspect that finds nothing reports `image <ref> is not available
locally`; it used to be dereferenced immediately. VERIFIED: with the
base image built by the same process and its tag removed mid-run, that
path is a SIGSEGV before this change.
- What lands in the committed image does not change: `Cmd`,
`Entrypoint`, `User`, `WorkingDir` and the `LANG`/`LC_ALL` envs are
still restored from the base image config, because the build container
overrides them.

## Why

The build container is created from the base image with its entrypoint
and command replaced by the stapel build script, so committing it would
keep those overrides. werf restored the original config by inspecting
the base image again at commit time — after the container had run, at a
point where nothing guarantees the image is still there, and where a
missing image was answered with a panic. The image config is immutable,
so reading it before the run and reusing it removes the window instead
of coping with it.

The alternative was to keep the second inspect and fall back to the
image id when the name no longer resolves. That keeps a lookup in the
commit path that can still fail for a reason nobody can act on, and
leaves the two references (`run` by id, commit by name) diverging.

Fixes: 69140cdeff4e ("feat(buildah): refactor container-runtime

pkg/container_backend/legacy_base_image.go
@@ -1,8 +1,6 @@
 package container_backend
 
 import (
-	"context"
-	"fmt"
 	"sync"
 
 	"github.com/werf/werf/v2/pkg/image"
@@ -32,19 +30,6 @@ func (i *legacyBaseImage) Name() string {
 
 func (i *legacyBaseImage) SetName(name string) {
 	i.name = name
-}
-
-func (i *legacyBaseImage) MustResetInfo(ctx context.Context) error {
-	if info, err := i.ContainerBackend.GetImageInfo(ctx, i.Name(), GetImageInfoOpts{}); err != nil {
-		return fmt.Errorf("unable to get info for image %s: %w", i.Name(), err)
-	} else {
-		i.SetInfo(info)
-	}
-
-	if i.info == nil {
-		panic(fmt.Sprintf("runtime error: info must be set for image %q", i.name))
-	}
-	return nil
 }
 
 func (i *legacyBaseImage) GetInfo() *image.Info {

pkg/container_backend/legacy_stage_image.go
@@ -207,14 +207,6 @@ func (i *LegacyStageImage) introspectBefore(ctx context.Context) error {
 	return nil
 }
 
-func (i *LegacyStageImage) MustResetInfo(ctx context.Context) error {
-	if i.buildImage != nil {
-		return i.buildImage.MustResetInfo(ctx)
-	} else {
-		return i.legacyBaseImage.MustResetInfo(ctx)
-	}
-}
-
 func (i *LegacyStageImage) GetInfo() *image.Info {
 	if i.buildImage != nil {
 		return i.buildImage.GetInfo()

pkg/container_backend/legacy_stage_image_container.go
@@ -25,6 +25,7 @@ type LegacyStageImageContainer struct {
 	runOptions                 *LegacyStageImageContainerOptions
 	commitChangeOptions        *LegacyStageImageContainerOptions
 	serviceCommitChangeOptions *LegacyStageImageContainerOptions
+	inheritedCommitOptions     *LegacyStageImageContainerOptions
 }
 
 func newLegacyStageImageContainer(img *LegacyStageImage) *LegacyStageImageContainer {
@@ -218,7 +219,7 @@ func (c *LegacyStageImageContainer) prepareIntrospectOptions(ctx context.Context
 }
 
 func (c *LegacyStageImageContainer) prepareCommitChanges(ctx context.Context, opts LegacyCommitChangeOptions) ([]string, error) {
-	commitOptions, err := c.prepareCommitOptions(ctx)
+	commitOptions, err := c.prepareCommitOptions()
 	if err != nil {
 		return nil, err
 	}
@@ -230,32 +231,29 @@ func (c *LegacyStageImageContainer) prepareCommitChanges(ctx context.Context, op
 	return commitChanges, nil
 }
 
-func (c *LegacyStageImageContainer) prepareCommitOptions(ctx context.Context) (*LegacyStageImageContainerOptions, error) {
-	inheritedCommitOptions, err := c.prepareInheritedCommitOptions(ctx)
-	if err != nil {
-		return nil, err
+func (c *LegacyStageImageContainer) prepareCommitOptions() (*LegacyStageImageContainerOptions, error) {
+	if c.inheritedCommitOptions == nil {
+		return nil, fmt.Errorf("commit options inherited from the base image are not prepared for image %s", c.image.name)
 	}
 
-	commitOptions := inheritedCommitOptions.merge(c.serviceCommitChangeOptions.merge(c.commitChangeOptions))
+	commitOptions := c.inheritedCommitOptions.merge(c.serviceCommitChangeOptions.merge(c.commitChangeOptions))
 	return commitOptions, nil
 }
 
-func (c *LegacyStageImageContainer) prepareInheritedCommitOptions(ctx context.Context) (*LegacyStageImageContainerOptions, error) {
+// prepareInheritedCommitOptions reads the config of the base image to restore in the committed image
+// what running the build container overrides. It must be called while the container has not been
+// committed yet: the base image is guaranteed to be available locally only until then.
+func (c *LegacyStageImageContainer) prepareInheritedCommitOptions(ctx context.Context, fromImageRef string) (*LegacyStageImageContainerOptions, error) {
 	inheritedOptions := newLegacyStageContainerOptions()
-
-	if c.image.fromImage == nil {
-		panic(fmt.Sprintf("runtime error: FromImage should be (%s)", c.image.name))
-	}
-
-	if err := c.image.fromImage.MustResetInfo(ctx); err != nil {
-		return nil, fmt.Errorf("unable to reset info for image %s: %w", c.image.fromImage.Name(), err)
-	}
 
 	dockerServerBackend := c.image.ContainerBackend.(*DockerServerBackend)
 
-	fromImageInspect, err := dockerServerBackend.GetImageInspect(ctx, c.image.fromImage.Name())
+	fromImageInspect, err := dockerServerBackend.GetImageInspect(ctx, fromImageRef)
 	if err != nil {
 		return nil, fmt.Errorf("unable to get image inspect: %w", err)
+	}
+	if fromImageInspect == nil {
+		return nil, fmt.Errorf("image %s is not available locally", fromImageRef)
 	}
 
 	if len(fromImageInspect.Config.Cmd) != 0 {
@@ -285,6 +283,16 @@ func (c *LegacyStageImageContainer) prepareInheritedCommitOptions(ctx context.Co
 
 func (c *LegacyStageImageContainer) run(ctx context.Context) error {
 	_ = c.image.ContainerBackend.(*DockerServerBackend)
+
+	if c.image.fromImage == nil {
+		panic(fmt.Sprintf("runtime error: FromImage should be (%s)", c.image.name))
+	}
+
+	inheritedCommitOptions, err := c.prepareInheritedCommitOptions(ctx, c.imageRef(c.image.fromImage))
+	if err != nil {
+		return err
+	}
+	c.inheritedCommitOptions = inheritedCommitOptions
 
 	runArgs, err := c.prepareRunArgs(ctx)
 	if err != nil {

pkg/container_backend/legacy_stage_image_commit_options_test.go
@@ -1,0 +1,34 @@
+package container_backend
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("LegacyStageImageContainer commit options", func() {
+	newContainer := func() *LegacyStageImageContainer {
+		return newLegacyStageImageContainer(NewLegacyStageImage(nil, "repo:tag", nil, ""))
+	}
+
+	It("fails when the base image config has not been read before the run", func() {
+		c := newContainer()
+
+		_, err := c.prepareCommitOptions()
+
+		Expect(err).To(MatchError(ContainSubstring("are not prepared")))
+	})
+
+	It("restores the base image config read before the run", func() {
+		c := newContainer()
+		c.inheritedCommitOptions = newLegacyStageContainerOptions()
+		c.inheritedCommitOptions.Entrypoint = `["/bin/base-entrypoint"]`
+		c.inheritedCommitOptions.User = "base-user"
+		c.commitChangeOptions.AddUser("stage-user")
+
+		opts, err := c.prepareCommitOptions()
+
+		Expect(err).To(Succeed())
+		Expect(opts.Entrypoint).To(Equal(`["/bin/base-entrypoint"]`))
+		Expect(opts.User).To(Equal("stage-user"))
+	})
+})